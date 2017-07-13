1bfacdfbee949c74e17298f7464362e81656c70e
Alberto Rasillo <bluesnatural@gmail.com>
2017-07-13 16:45:03 +0200

    Mako full functional in dummy version 2->from .py

--- som_generationkwh/report/report_amortization_gkwh.py
+++ som_generationkwh/report/report_amortization_gkwh.py
@@ -3,33 +3,29 @@ from osv import osv, fields
 import pooler
 
 class AccountInvoice(osv.osv):
-
-    #_name = 'amortization_gkwh_report'
-    #_inherit = 'amortization_gkwh_report'
     _name = 'account.invoice'
     _inherit = 'account.invoice'
 
 
-
-
-
-
-    #Report datas
-    def titleReport(self):
-        return 'Liquidació préstec Generation kWh'
-
-    def dateReceiptReport(self):
-        return '20-05-2017'
-
-    #Investment datas
-    def nameInvestmentFromInvoice(self, cursor, uid, invoice_id):
-        return '2GKWH001'
-
-    def ownerInvestmentFromInvoice(self, cursor, uid, invoice_id):
+    # Owner data
+    def ownerNameInvestmentFromInvoice(self, cursor, uid, invoice_id):
         return 'Pepito de Los Palotes'
 
+    def ownerNifInvestmentFromInvoice(self, cursor, uid, invoice_id):
+        return '87654321-A'
+
+    # Investment data
+    def nameInvestmentFromInvoice(self, cursor, uid, invoice_id):
+        return 'GKWH0000001'
+
     def amoutInitialInvestment(self,cursor, uid, investment_id):
-        return '1.000,00 €'
+        return '1.000,23'
+
+    def capitalPendingFromInvestment(self, cursor, uid, investment_id):
+        return '960,11'
+
+    def bankAccountFromInvoice(self, cursor, uid, invoice_id):
+        return 'ES25 0081 5273 6200 0103 ZZZZ'
 
     def datePurchaseInvestment(self, cursor, uid, investment_id):
         return '20-05-2015'
@@ -37,22 +33,21 @@ class AccountInvoice(osv.osv):
     def dateExpirationInvestment(seqlf, cursor, uid, investment_id):
         return '19-05-2040'
 
-    #Amortitzation datas
-    def amountAmortitzationFromInvoice(self, cursor, uid, invoice_id):
-        return '40,00 €'
+    # Amortization data
+    def amortizationNameFromInvoice(self, cursor, uid, invoice_id):
+        return "GKWH0000001-AMOR2017"
 
-    def dateEndAmortizationFromInvoice(self, cursor, uid, invoice_id):
+    def amortitzationAmountFromInvoice(self, cursor, uid, invoice_id):
+        return '40,02'
+
+    def amortizationEndDateFromInvoice(self, cursor, uid, invoice_id):
         return '20-05-2017'
 
-    def numPaymentFromInvoice(self, cursor, uid, invoice_id):
-        return '1 de 24'
+    def amortizationNumPaymentFromInvoice(self, cursor, uid, invoice_id):
+        return '7'
 
-    def capitalPendingFromInvestment(self, cursor, uid, investment_id):
-        return '960,00 €'
-
-    #AccountBank
-    def bankAccountFromInvoice(self, cursor, uid, invoice_id):
-        return 'ES25 0081 5273 6200 0103 9910'
+    def amortizationTotalPaymentFromInvoice(self, cursor, uid, invoice_id):
+        return '29'
 
 AccountInvoice()
 # vim: et ts=4 sw=4
