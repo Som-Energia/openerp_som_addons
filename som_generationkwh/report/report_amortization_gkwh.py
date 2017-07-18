83bd255c7d16a10e94efcf0bef054baa6d06bc18
Alberto Rasillo <bluesnatural@gmail.com>
2017-07-18 15:59:30 +0200

    Testing report filler

--- som_generationkwh/report/report_amortization_gkwh.py
+++ som_generationkwh/report/report_amortization_gkwh.py
@@ -9,57 +9,61 @@ class AccountInvoice(osv.osv):
     _name = 'account.invoice'
     _inherit = 'account.invoice'
 
-    def investmentAmortization_notificationData(self, cursor, uid, id):
+    def investmentAmortization_notificationData_asDict(self, cursor, uid, ids):
+        return dict(self.investmentAmortization_notificationData(cursor, uid, ids))
+
+    def investmentAmortization_notificationData(self, cursor, uid, ids):
         report = ns()
-
         pool = pooler.get_pool(cursor.dbname)
-        acc_inv = pool.get('account.invoice')
-        gkwh_inv = pool.get('generationkwh.investment')
+        Invoice = pool.get('account.invoice')
+        Investment = pool.get('generationkwh.investment')
+        Partner = pool.get('res.partner')
 
-        inversionId = acc_inv.search(cursor, uid, [
-            ('id', '=', id)
-        ])
+        investment_id = ids[0]
 
-        #Receipt Date
-        inversionDictReceDat = acc_inv.read(cursor, uid, inversionId, ['date_invoice'])
-        acc_inv_rec_date = [a['date_invoice'] for a in inversionDictReceDat]
-        report.receiptDate = acc_inv_rec_date[0]
+        investment = Invoice.read(cursor, uid, investment_id, [
+            'date_invoice',
+            'partner_id',
+            'name',
+            ])
+        report.receiptDate = investment['date_invoice']
 
+        #a partir de partner id
+        partner = Partner.read(cursor, uid, investment['partner_id'][0], ['vat'])
+        # TODO: non spanish vat not covered by tests
+        report.ownerNif = partner['vat'][2:] if partner['vat'][:2]=='ES' else partner['vat']
+        report.inversionName = investment['name']
 
-        report.ownerNif = '87654321-A'
+        return report
 
-        #Descripcio
-        inversionDictName = acc_inv.read(cursor, uid, inversionId, ['name'])
-        acc_inv_name = [a['name'] for a in inversionDictName]
-        report.inversionName = acc_inv_name[0]
 
         #Get gkwh Inversion name/id
-        gkwhInversionId = gkwh_inv.search(cursor, uid, [
+        gkwhInversionId = Investment.search(cursor, uid, [
             ('name', '=', report.inversionName)
         ])
 
         # Titular
-        gkwhInversionDictOwner = gkwh_inv.read(cursor, uid, gkwhInversionId, ['member_id'])
+        gkwhInversionDictOwner = Investment.read(cursor, uid, gkwhInversionId, ['member_id'])
         gkwh_inv_owner_name = [a['member_id'] for a in gkwhInversionDictOwner]
         report.ownerName = gkwh_inv_owner_name[0][1]
 
         #Inversion Initial
-        gkwhInversionDictInitAmou = gkwh_inv.read(cursor, uid, gkwhInversionId, ['nshares'])
+        gkwhInversionDictInitAmou = Investment.read(cursor, uid, gkwhInversionId, ['nshares'])
         gkwh_inv_nshares = [a['nshares'] for a in gkwhInversionDictInitAmou]
         report.inversionInitialAmount = gkwh_inv_nshares[0] * 100
 
         #Pending capital
-        gkwhInversionDictPendCapt = gkwh_inv.read(cursor, uid, gkwhInversionId, ['amortized_amount'])
+        gkwhInversionDictPendCapt = Investment.read(cursor, uid, gkwhInversionId, ['amortized_amount'])
         gkwh_inv_amor_amou = [a['amortized_amount'] for a in gkwhInversionDictPendCapt]
         report.inversionPendingCapital = report.inversionInitialAmount - gkwh_inv_amor_amou[0]
 
         #Bank Account
-        inversionDictBankAcco = acc_inv.read(cursor, uid, inversionId, ['partner_bank'])
+        inversionDictBankAcco = Invoice.read(cursor, uid, investment_id, ['partner_bank'])
         acc_inv_bank_acco = [a['partner_bank'] for a in inversionDictBankAcco]
         report.report.inversionBankAccount = acc_inv_bank_acco[0]
 
         #Purchase Date
-        gkwhInversionDictPurDat = gkwh_inv.read(cursor, uid, gkwhInversionId, ['purchase_date'])
+        gkwhInversionDictPurDat = Investment.read(cursor, uid, gkwhInversionId, ['purchase_date'])
         gkwh_inv_pur_date = [a['purchase_date'] for a in gkwhInversionDictPurDat]
         report.inversionPurchaseDate = gkwh_inv_pur_date[0]
 
@@ -67,12 +71,12 @@ class AccountInvoice(osv.osv):
         report.inversionExpirationDate = datetime.datetime.strptime(gkwh_inv_pur_date[0], "%Y-%m-%d").date() + relativedelta(years=+25)
 
         #Actual Amortization
-        inversionDictActualAmor = acc_inv.read(cursor, uid, inversionId, ['number'])
+        inversionDictActualAmor = Invoice.read(cursor, uid, investment_id, ['number'])
         acc_inv_actu_amor = [a['number'] for a in inversionDictActualAmor]
         report.amortizationName = acc_inv_actu_amor[0]
 
         #Amortization Amount
-        inversionDictAmount = acc_inv.read(cursor, uid, inversionId, ['amount_total'])
+        inversionDictAmount = Invoice.read(cursor, uid, investment_id, ['amount_total'])
         acc_inv_amount = [a['amount_total'] for a in inversionDictAmount]
         report.amortizationAmount = acc_inv_amount[0]
 
