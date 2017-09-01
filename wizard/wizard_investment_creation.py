4afe0d5f9e7d2452a9b75c7aba940fa86f6c5240
Francisco Cubero <Francisco.Cubero@somenergia.coop>
2017-09-01 18:04:21 +0200

    WIP Wizard inversion creation added more fields

--- wizard/wizard_investment_creation.py
+++ wizard/wizard_investment_creation.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from datetime import date
+from datetime import date,datetime,timedelta
 
 from osv import osv, fields
 from tools.translate import _
@@ -10,7 +10,7 @@ class WizardInvestmentCreation(osv.osv):
 
     _name = 'wizard.generationkwh.investment.creation'
 
-    def do_action(self, cursor, uid, ids, context=None):
+    def do_create(self, cursor, uid, ids, context=None):
         """ Do selected action"""
         if context is None:
             context = {}
@@ -19,7 +19,9 @@ class WizardInvestmentCreation(osv.osv):
         Member = self.pool.get('somenergia.soci')
 
         wiz = self.browse(cursor, uid, ids[0], context=context)
-        result = 'testing'
+
+        result = wiz.ip
+
         wiz.write({'info': result , 'state': 'done'}, context=context)
 
     def _default_info(self, cursor, uid, context=None):
@@ -30,11 +32,22 @@ class WizardInvestmentCreation(osv.osv):
     _columns = {
         'state': fields.char('State', size=16),
         'info': fields.text('Info'),
+        'amount_in_euros': fields.float('quantitat aportada'),
+        'ip': fields.char("ip d'origen",size=16),
+        'iban': fields.text('compte iban'),
+        'order_date': fields.date(
+            'data de comanda',
+            required=True
+        ),
     }
 
     _defaults = {
         'state': lambda *a: 'init',
         'info': _default_info,
+        'order_date': lambda *a: str(datetime.today()+timedelta(days=6)),
+        'amount_in_euros': lambda *a: 0.0,
+        'ip': lambda *a: "127.0.0.1",
+        'iban': lambda *A: ""
     }
 
 WizardInvestmentCreation()
