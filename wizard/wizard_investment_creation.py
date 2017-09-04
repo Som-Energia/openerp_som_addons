5c86738bb51e4d389ba541a00d44c23786255cbb
Francisco Cubero <Francisco.Cubero@somenergia.coop>
2017-09-04 12:15:49 +0200

    Wizard inversion creation uses create_from_form

--- wizard/wizard_investment_creation.py
+++ wizard/wizard_investment_creation.py
@@ -17,11 +17,23 @@ class WizardInvestmentCreation(osv.osv):
 
         Investment = self.pool.get('generationkwh.investment')
         Member = self.pool.get('somenergia.soci')
-
         wiz = self.browse(cursor, uid, ids[0], context=context)
 
-        result = wiz.ip
+        partner_id  = int(wiz.partner_id)
+        amount_in_e = float(wiz.amount_in_euros)
+        ip = str(wiz.ip)
+        iban = str(wiz.iban)
 
+        Investment.create_from_form(cursor, uid,
+            partner_id, wiz.order_date, amount_in_e, ip, iban,
+            context)
+
+        result = "Partner_id : \t{}\n".format(wiz.partner_id)
+        result += "Order date: \t{}\n".format(wiz.order_date)
+        result += "Amount: \t\t{}\n".format(wiz.amount_in_euros)
+        result += "IP \t\t\t\t{}\n".format(wiz.ip)
+        result += "IBAN : \t\t\t{}".format(wiz.iban)
+        result += "Inversió creada"
         wiz.write({'info': result , 'state': 'done'}, context=context)
 
     def _default_info(self, cursor, uid, context=None):
@@ -32,9 +44,10 @@ class WizardInvestmentCreation(osv.osv):
     _columns = {
         'state': fields.char('State', size=16),
         'info': fields.text('Info'),
+        'partner_id': fields.integer('partner_id'),
         'amount_in_euros': fields.float('quantitat aportada'),
         'ip': fields.char("ip d'origen",size=16),
-        'iban': fields.text('compte iban'),
+        'iban': fields.char('compte iban',size=35),
         'order_date': fields.date(
             'data de comanda',
             required=True
@@ -44,10 +57,11 @@ class WizardInvestmentCreation(osv.osv):
     _defaults = {
         'state': lambda *a: 'init',
         'info': _default_info,
+        'partner_id': lambda *a: 0,
         'order_date': lambda *a: str(datetime.today()+timedelta(days=6)),
         'amount_in_euros': lambda *a: 0.0,
         'ip': lambda *a: "127.0.0.1",
-        'iban': lambda *A: ""
+        'iban': lambda *a: ""
     }
 
 WizardInvestmentCreation()
