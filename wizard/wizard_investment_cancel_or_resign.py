18b9fd7d5001cd31f532a2f92a3e7dad634d4843
Francisco Cubero <Francisco.Cubero@somenergia.coop>
2017-10-16 12:35:05 +0200

    CancelOrResignWizard now can cancel,resing not yet

--- wizard/wizard_investment_cancel_or_resign.py
+++ wizard/wizard_investment_cancel_or_resign.py
@@ -20,30 +20,46 @@ class WizardInvestmentCancelOrResing(osv.osv):
         wiz = self.browse(cursor, uid, ids[0], context=context)
         result = ''
         investment_ids = context.get('active_ids', [])
+        Investment = self.pool.get('generationkwh.investment')
 
         for counter,investment_id in enumerate(investment_ids):
             # TODO: 
             # cancel or resign this investment
             # detect action to be executed [cancel] [resign] [error-none]
             # execute The action : call to cancel or resign functions
-            inv_names = self._get_investment_names(cursor,uid,investment_id,context)
-            result += "{0}/{1} Acció {2} realitzada per ( {3} , {4} , {5} )\n".format(
+            inv_data = self._get_investment_data(cursor,uid,investment_id,context)
+            if inv_data[2]:     #draft
+                try:
+                    Investment.cancel(cursor, uid, [investment_id], context)
+                    action = "ha estat cancel·lada"
+                except Exception as e:
+                    action = "ha generat error : " + str(e)
+                result += "{0}/{1} inversió ( {2} , {3} , {4} ) {5}\n".format(
                     counter+1,
                     len(investment_ids),
-                    random.choice(['cancel·lació','renúncia']),
                     investment_id,
-                    inv_names[0],
-                    inv_names[1]
+                    inv_data[0],
+                    inv_data[1],
+                    action
+                )
+            else:
+                result += "{0}/{1} inversió ( {2} , {3} , {4} ) no podem tractar\n".format(
+                    counter+1,
+                    len(investment_ids),
+                    investment_id,
+                    inv_data[0],
+                    inv_data[1]
                 )
 
         wiz.write({'info': result , 'state': 'done'}, context=context)
 
-    def _get_investment_names(self, cursor, uid, investment_id, context=None):
+    def _get_investment_data(self, cursor, uid, investment_id, context=None):
         Investment = self.pool.get('generationkwh.investment')
         inv = Investment.read(cursor, uid, investment_id , [])
         customer_name = inv['member_id'][1]
         investment_name = inv['name']
-        return (customer_name, investment_name)
+        investment_draft = inv['draft']
+        return (customer_name, investment_name, investment_draft)
 
     def _default_info(self, cursor, uid, context=None):
         if context is None:
@@ -52,14 +68,14 @@ class WizardInvestmentCancelOrResing(osv.osv):
         investment_ids = context.get('active_ids', [])
         result = 'Inversion seleccionades per a cancel·lació o renúncia:\n\n'
         for counter,investment_id in enumerate(investment_ids):
-            inv_names = self._get_investment_names(cursor,uid,investment_id,context)
+            inv_data = self._get_investment_data(cursor,uid,investment_id,context)
             result += "({0}/{1}) inversió ( {2} , {3} , {4} ) realitzarà l'acció {5}\n".format(
                     counter+1,
                     len(investment_ids),
                     investment_id,
-                    inv_names[0],
-                    inv_names[1],
-                    random.choice(['cancel·lació','renúncia','no cancelable']),
+                    inv_data[0],
+                    inv_data[1],
+                    'cancel·lació' if inv_data[2] else 'renúncia',
                 )
         return result
 
