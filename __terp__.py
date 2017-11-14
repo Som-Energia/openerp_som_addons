6b8c253df20bce8d33408cf0dc7a081dc8ba7a5b
Francisco Cubero <Francisco.Cubero@somenergia.coop>
2017-11-14 14:23:35 +0100

    fix xml order in terp

--- __terp__.py
+++ __terp__.py
@@ -22,15 +22,15 @@
     "wizard/wizard_investment_activation.xml",
     "wizard/wizard_investment_amortization.xml",
     "wizard/wizard_investment_payment.xml",
+    "wizard/wizard_investment_creation.xml",
+    "wizard/wizard_investment_cancel_or_resign.xml",
     "investment_view.xml",
     "assignment_view.xml",
     "somenergia_soci_view.xml",
     "somenergia_soci_data.xml",
     "security/som_generationkwh_security.xml",
     "security/ir.model.access.csv",
-    "amortization_report.xml",
-    "wizard/wizard_investment_creation.xml",
-    "wizard/wizard_investment_cancel_or_resign.xml"
+    "amortization_report.xml"
     ],
   "active": False,
   "installable": True
