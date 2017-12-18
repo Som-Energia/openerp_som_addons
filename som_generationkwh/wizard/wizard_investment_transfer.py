983e98f7e444f0b3141e9af88277088bb5d0f2a4
oriolpiera <oriol.piera@somenergia.coop>
2017-12-18 15:24:44 +0100

    Add wizard investment tranfer files

---
+++ som_generationkwh/wizard/wizard_investment_transfer.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+from datetime import date
+from osv import osv, fields
+from tools.translate import _
+import pickle
+
+class WizardInvestmentTransfer(osv.osv):
+
+    _name = 'wizard.generationkwh.investment.transfer'
+
+    _columns = {
+        'state': fields.char('State', size=16),
+        'info': fields.text('Info'),
+        'invoices': fields.text(
+            'test',
+        ),
+        'partner_id_alt': fields.many2one(
+            'res.partner',
+            'Titular',
+            domain=[('category_id','=',8)],
+            required=True,
+        ),
+        'iban': fields.many2one(
+            'res.partner.bank',
+            'IBAN',
+            required=True,
+        ),        
+    }
+
+    _defaults = {
+        'state': lambda *a: 'init',
+        'info': lambda *a: 'Aquesta acció transferirà la inversió.\n'
+                           'Condicions:\n'
+                           '  - Les inversions han d"estar pagades \n'
+    }
+
+    def do_transfer(self, cursor, uid, ids, context=None):
+        if context is None:
+            context = {}
+
+        Investment = self.pool.get('generationkwh.investment')
+        wiz = self.browse(cursor, uid, ids[0], context)
+        investment_ids = context.get('active_ids', [])
+        new_partner_id = int(wiz.partner_id_alt.id)
+        iban = wiz.iban.iban
+        order_date = date.today()
+
+        old_investment_id, new_investment_id = Investment.create_from_transfer(cursor, uid, investment_ids[0], new_partner_id, order_date, iban, context=None)
+
+        info = "RESULTAT: \n"
+        info += "================\n"
+        info += "Investment vell: %d" % old_investment_id
+        info += "\nInvestment nou: %d" % new_investment_id
+        wiz.write(dict(
+            info= info,
+            state = 'Done',
+            ))
+
+        return True
+
+
+WizardInvestmentTransfer()
+# vim: et ts=4 sw=4 
