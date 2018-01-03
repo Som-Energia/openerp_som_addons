e0430d650f895078361bdd3f30a8fa3546b3ebb7
Francisco Cubero <Francisco.Cubero@somenergia.coop>
2018-01-03 18:20:05 +0100

    www_generationkwh_assignments implemented

--- som_generationkwh/res_partner.py
+++ som_generationkwh/res_partner.py
@@ -17,7 +17,30 @@ class ResPartner(osv.osv):
         if not idmap: # Not a member
             return []
         return Investment.list(cursor, uid, idmap[id], context=context)
-    
+
+    def www_generationkwh_assignments(self, cursor, uid, id, context=None):
+        Dealer =self.pool.get('generationkwh.dealer')
+        idmap = dict(Dealer.get_members_by_partners(cursor, uid, [id]))
+        if not idmap: return [] # Not a member
+
+        Assignments =self.pool.get('generationkwh.assignment')
+        assignment_ids = Assignments.search(cursor, uid, [
+            ('member_id', '=', idmap[id]),
+            ])
+        def process(x):
+            x['contract_name'] = x['contract_id'][1]
+            x['contract_id'] = x['contract_id'][0]
+            x['member_name'] = x['member_id'][1]
+            x['member_id'] = x['member_id'][0]
+            return x
+
+        return sorted([
+            process(x)
+            for x in Assignments.read(cursor, uid, assignment_ids, [])
+        ], key=lambda x: (x['priority'],x['id']))
+
+
+
 
 ResPartner()
 
