4ecc30213c7db2dda6f9c2376ae74d69c13a6b73
David García Garzón <voki@canvoki.net>
2019-03-06 15:42:43 +0100

    effectiveItems -> items

--- som_generationkwh.py
+++ som_generationkwh.py
@@ -317,7 +317,7 @@ class GenerationkWhTestHelper(osv.osv):
         farePeriod = FarePeriodCurve(holidays)
 
         print 'remainders', remainders.lastRemainders()
-        print 'investment', investment.effectiveItems(
+        print 'investment', investment.items(
             start=isodate(start),
             end=isodate(stop),
             member=member)
