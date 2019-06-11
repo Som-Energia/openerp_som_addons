58c3a540962585e05cf75fc4a35140fc1daa0aa9
David García Garzón <voki@canvoki.net>
2019-06-11 15:08:18 +0200

    recomputeRightsOnPeriod

--- productionloader.py
+++ productionloader.py
@@ -8,8 +8,13 @@ from mongodb_backend.mongodb2 import mdbpool
 from generationkwh.productionloader import ProductionLoader
 from generationkwh.sharescurve import MixTotalSharesCurve
 from generationkwh.rightspershare import RightsPerShare
-from generationkwh.isodates import isodate
-
+from generationkwh.isodates import (
+    localisodate,
+    addDays,
+    isodate,
+    dateToLocal,
+    )
+from yamlns import namespace as ns
 from som_plantmeter.som_plantmeter import PlantShareProvider
 
 
@@ -33,7 +38,7 @@ class ProductionAggregatorProvider(ErpWrapper):
         return production.lastMeasurementDate(self.cursor, self.uid,
                 self.pid, context=self.context)
 
-
+# TODO: Rename it as GenerationkwhRightsCalculator
 class GenerationkWhProductionLoader(osv.osv):
 
     _name = 'generationkwh.production.loader'
@@ -60,6 +65,64 @@ class GenerationkWhProductionLoader(osv.osv):
         logger.notifyChannel('gkwh_productionLoader COMPUTE', netsvc.LOG_INFO,
                 'Compute available rights')
         return log
+
+    def recomputeRightsOnPeriod(self, cursor, uid, pid,
+            firstDateToRecompute,
+            lastDateToRecompute,
+            initialRemainders,
+            context=None,
+        ):
+        result=[]
+        logger = netsvc.Logger()
+        firstDateToRecompute = isodate(firstDateToRecompute)
+        lastDateToRecompute = isodate(lastDateToRecompute)
+        class RightsRecomputationRemainders:
+            def __init__(self, firstDate, sharesToWh):
+                self.firstDate = firstDate
+                self._input = sharesToWh
+                self._outremainders = {}
+
+            def lastRemainders(self):
+                return [
+                    (r.nshares, self.firstDate, r.remainder_wh)
+                    for r in (ns(d) for d in self._input)
+                    ]
+
+            def updateRemainders(self, newRemainders):
+                for nshares, nextDate, Wh in newRemainders:
+                    self._outremainders[nshares] = Wh
+
+            def getOutput(self):
+                return self._outremainders
+
+
+        production = ProductionAggregatorProvider(self, cursor, uid, pid, context)
+        plantsharesprovider = PlantShareProvider(self, cursor, uid, 'GenerationkWh')
+        plantsharecurver = MixTotalSharesCurve(plantsharesprovider)
+        rights = RightsPerShare(mdbpool.get_db())
+        remainders = RightsRecomputationRemainders(firstDateToRecompute, initialRemainders)
+
+        productionLoader = ProductionLoader(
+                productionAggregator=production,
+                plantShareCurver=plantsharecurver,
+                rightsPerShare=rights,
+                remainders=remainders)
+        productionLoader.computeAvailableRights(lastDateToRecompute)
+
+        logger.notifyChannel('gkwh_productionLoader COMPUTE', netsvc.LOG_INFO,
+                'Compute available rights')
+
+        result.append( dict(
+            nshares = 1,
+            first_date = str(firstDateToRecompute),
+            last_date = str(lastDateToRecompute),
+            next_date = str(addDays(dateToLocal(lastDateToRecompute),1).date()),
+            previous_remainder_wh = 0,
+            rights_kwh = 15000,
+            remainder_wh = 0,
+        ))
+        return result
+
 
 GenerationkWhProductionLoader()
 
