a0f0475f4862156673159193a1a8438c79be34a6
David García Garzón <voki@canvoki.net>
2019-07-18 21:32:38 +0200

    recomputeRightsOnPeriod: we took a different way

--- productionloader.py
+++ productionloader.py
@@ -79,65 +79,6 @@ class GenerationkWhProductionLoader(osv.osv):
         return log
 
 
-
-    def recomputeRightsOnPeriod(self, cursor, uid, pid,
-            firstDateToRecompute,
-            lastDateToRecompute,
-            initialRemainders,
-            context=None,
-        ):
-        logger = netsvc.Logger()
-        firstDateToRecompute = isodate(firstDateToRecompute)
-        lastDateToRecompute = isodate(lastDateToRecompute)
-        class RightsRecomputationRemainders:
-            def __init__(self, firstDate, sharesToWh):
-                self.firstDate = firstDate
-                self._input = sharesToWh
-                self._outremainders = {}
-
-            def lastRemainders(self):
-                return [
-                    (r.nshares, self.firstDate, r.remainder_wh)
-                    for r in (ns(d) for d in self._input)
-                    ]
-
-            def updateRemainders(self, newRemainders):
-                for nshares, nextDate, Wh in newRemainders:
-                    self._outremainders[nshares] = Wh
-
-            def getOutput(self):
-                return self._outremainders
-
-
-        production = ProductionAggregatorProvider(self, cursor, uid, pid, context)
-        plantsharesprovider = PlantShareProvider(self, cursor, uid, 'GenerationkWh')
-        plantsharecurver = MixTotalSharesCurve(plantsharesprovider)
-        rights = RightsPerShare(mdbpool.get_db())
-        remainders = RightsRecomputationRemainders(firstDateToRecompute, initialRemainders)
-
-        productionLoader = ProductionLoader(
-                productionAggregator=production,
-                plantShareCurver=plantsharecurver,
-                rightsPerShare=rights,
-                remainders=remainders)
-        result=[]
-        productionLoader.computeAvailableRights(lastDateToRecompute)
-
-        logger.notifyChannel('gkwh_productionLoader COMPUTE', netsvc.LOG_INFO,
-                'Compute available rights')
-
-        result.append( dict(
-            nshares = 1,
-            first_date = str(firstDateToRecompute),
-            last_date = str(lastDateToRecompute),
-            next_date = str(addDays(dateToLocal(lastDateToRecompute),1).date()),
-            previous_remainder_wh = 0,
-            rights_kwh = 15000,
-            remainder_wh = 0,
-        ))
-        return result
-
-
 GenerationkWhProductionLoader()
 
 
