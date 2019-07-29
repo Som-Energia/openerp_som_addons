f03a1cb4e817b933fd1e4594a0420597f3cc6c14
David García Garzón <voki@canvoki.net>
2019-07-29 11:35:42 +0200

    bump to 1.7.3

--- __terp__.py
+++ __terp__.py
@@ -2,7 +2,7 @@
 {
   "name": "Generationkwh production manager",
   "description": """Support for SomEnergia's plantmeter in GisceERP""",
-  "version": "1.7.2",
+  "version": "1.7.3",
   "author": "Som-Energia",
   "category": "Master",
   "depends": ['base'],
