a48a661afe6c7847219b19e36df7e07800b9cdd9
Oriol Piera <oriol.piera@somenergia.coop>
2019-12-03 15:01:11 +0100

    New test set_done and configure setUp and tearDown

--- tests/emission_tests.py
+++ tests/emission_tests.py
@@ -8,25 +8,46 @@ import random
 
 
 class EmissionTests(testing.OOTestCase):
-    def test_change_emission_state(self):
+    def setUp(self):
+        self.pool = self.openerp.pool
+        self.imd_obj = self.pool.get('ir.model.data')
+        self.emission_obj = self.pool.get('generationkwh.emission')
+        #self.txn = Transaction().start(self.database)
+
+    def tearDown(self):
+        #self.txn.stop()
+        pass
+
+    def test__action_open(self):
         """
         Checks if when state changed, everithing works
         :return:
         """
-        pool = self.openerp.pool
-        emission_obj = pool.get('generationkwh.emission')
-        imd_obj = pool.get('ir.model.data')
-
         with Transaction().start(self.database) as txn:
             cursor = txn.cursor
             uid = txn.user
-            em_id = imd_obj.get_object_reference(
+            em_id = self.imd_obj.get_object_reference(
                 cursor, uid, 'som_generationkwh', 'emissio_0001')[1]
-            em_0001 = emission_obj.browse(cursor, uid, em_id)
+            em_0001 = self.emission_obj.browse(cursor, uid, em_id)
 
-            emission_obj.action_open(cursor, uid, em_id)
+            self.emission_obj.action_open(cursor, uid, em_id)
 
             self.assertEquals(em_0001.state, "open")
 
+    def test__set_done(self):
+        """
+        Checks if when state changed, everithing works
+        :return:
+        """
+        with Transaction().start(self.database) as txn:
+            cursor = txn.cursor
+            uid = txn.user
+            em_id = self.imd_obj.get_object_reference(
+                cursor, uid, 'som_generationkwh', 'emissio_0001')[1]
+            em_0001 = self.emission_obj.browse(cursor, uid, em_id)
+
+            self.emission_obj.set_done(cursor, uid, em_id)
+
+            self.assertEquals(em_0001.state, "done")
 
 # vim: et ts=4 sw=4
