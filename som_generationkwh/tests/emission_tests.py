841b929963b68356361309e7cacd7fb6736c948d
Oriol Piera <oriol.piera@somenergia.coop>
2019-12-03 15:04:56 +0100

    New test cance and set_to_draft

--- som_generationkwh/tests/emission_tests.py
+++ som_generationkwh/tests/emission_tests.py
@@ -50,4 +50,36 @@ class EmissionTests(testing.OOTestCase):
 
             self.assertEquals(em_0001.state, "done")
 
+    def test__cancel(self):
+        """
+        Checks if when state changed, everithing works
+        :return:
+        """
+        with Transaction().start(self.database) as txn:
+            cursor = txn.cursor
+            uid = txn.user
+            em_id = self.imd_obj.get_object_reference(
+                cursor, uid, 'som_generationkwh', 'emissio_0001')[1]
+            em_0001 = self.emission_obj.browse(cursor, uid, em_id)
+
+            self.emission_obj.cancel(cursor, uid, em_id)
+
+            self.assertEquals(em_0001.state, "cancel")
+
+    def test__set_to_draft(self):
+        """
+        Checks if when state changed, everithing works
+        :return:
+        """
+        with Transaction().start(self.database) as txn:
+            cursor = txn.cursor
+            uid = txn.user
+            em_id = self.imd_obj.get_object_reference(
+                cursor, uid, 'som_generationkwh', 'emissio_0001')[1]
+            em_0001 = self.emission_obj.browse(cursor, uid, em_id)
+
+            self.emission_obj.set_to_draft(cursor, uid, em_id)
+
+            self.assertEquals(em_0001.state, "draft")
+
 # vim: et ts=4 sw=4
