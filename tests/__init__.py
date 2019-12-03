3f91cba9912b41cddc6f7248c8da39eab7a5baa9
Oriol Piera <oriol.piera@somenergia.coop>
2019-12-03 09:48:37 +0100

    First som_generationkwh destral test

---
+++ tests/__init__.py
@@ -0,0 +1,1 @@
+import investment_tests
---
+++ tests/investment_tests.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from destral import testing
+from destral.transaction import Transaction
+from destral.patch import PatchNewCursors
+import netsvc
+import time
+import random
+
+
+class InvestmentTests(testing.OOTestCase):
+    def test_mark_as_signed(self):
+        """
+        Checks if signed_date change
+        :return:
+        """
+        pool = self.openerp.pool
+        investment_obj = pool.get('generationkwh.investment')
+        imd_obj = pool.get('ir.model.data')
+
+        with Transaction().start(self.database) as txn:
+            cursor = txn.cursor
+            uid = txn.user
+            inv_id = imd_obj.get_object_reference(
+                        cursor, uid, 'som_generationkwh', 'genkwh_0001'
+                        )[1]
+            inv_0001 = investment_obj.browse(cursor, uid, inv_id)
+            self.assertEquals(inv_0001.signed_date, False)
+
+            investment_obj.mark_as_signed(cursor, uid, inv_id, '2017-01-06')
+
+            inv_0001 = investment_obj.browse(cursor, uid, inv_id)
+            self.assertEquals(inv_0001.signed_date, '2017-01-06')
+
+
