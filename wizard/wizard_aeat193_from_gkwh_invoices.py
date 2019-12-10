dc3ac78479c617b22e0ab25af72955f4434d4518
Emilio García <egarcia@gisce.net>
2019-12-10 09:46:26 +0100

    Added Create model 193 lines Wizard
    Added Create model 193 lines Wizard

--- wizard/wizard_aeat193_from_gkwh_invoices.py
+++ wizard/wizard_aeat193_from_gkwh_invoices.py
@@ -1,5 +1,6 @@
 # -*- encoding: utf-8 -*-
 from osv import osv, fields
+from tools import config
 
 
 class WizardComputeMod193Invoice(osv.osv_memory):
@@ -26,7 +27,7 @@ class WizardComputeMod193Invoice(osv.osv_memory):
             context = {}
 
         txt = u"Calculant la retenció de IRPF per a les factures amb " \
-              u"Generation kWh de l'any {name} ({start}->{end}"
+              u"Generation kWh de l'any {name} ({start} -> {end})"
 
         dates = self.get_year(cursor, uid, context)
 
@@ -42,6 +43,7 @@ class WizardComputeMod193Invoice(osv.osv_memory):
         wiz = self.browse(cursor, uid, wiz_id, context)
 
         report_obj = self.pool.get('l10n.es.aeat.mod193.report')
+        record_obj = self.pool.get('l10n.es.aeat.mod193.record')
         report_id = context.get('active_id')
 
         report = report_obj.browse(cursor, uid, report_id, context)
@@ -53,29 +55,57 @@ class WizardComputeMod193Invoice(osv.osv_memory):
               u"Buscarem les factures entre les dates " \
               u"{start} i {end}".format(**dates)
 
+        common_vals = {
+            'report_id': report.id,
+            'lr_vat': '',
+            'mediator': False,
+            'code_key': '1',
+            'code_value': report.company_vat,
+            'incoming_key': 'B',
+            'nature': '03',
+            'payment': '1',
+            'code_type': False,
+            'code_bank': '',
+            'pending': ' ',
+            'fiscal_year_id': report.fiscalyear_id.id,
+            'incoming_type': '1'
+        }
 
+        query_file = (u"%s/som_generationkwh/sql/aeat193_from_gkwh_invoices_query.sql" % config['addons_path'])
+        query = open(query_file).read()
+        cursor.execute(query, dict(start=dates['start'], end=dates['end']))
 
-        num_linies = 0
-        num_factures = 0
-        txt += u'Afegides {} línies de {} factures'.format(
-            num_linies, num_factures
-        )
+        total_linies = []
+        for data in cursor.fetchall():
+            vals = common_vals.copy()
+            amount = float(data[2])
+            vals.update({
+                'partner_id': data[0],
+                'partner_vat': data[1],
+                'amount': amount,
+                'amount_base': amount,
+                'amount_tax': amount * wiz.tax_id.name,
+                'tax_percent': wiz.tax_id.amount * 100
+            })
+            total_linies.append(record_obj.create(cursor, uid, vals))
 
+        txt += u'\nAfegides {} línies al model 193'.format(len(total_linies))
 
-
-        wiz.write(cursor, uid, wiz_id, {'info': txt})
-        return True
+        wiz.write({'info': txt, 'state': 'done'})
 
     _columns = {
         'tax_id': fields.many2one(
             'account.tax', 'Retenció a aplicar', required=True,
-            help='Retenció IRPF a aplicar', domain=[('name','like','IRPF')]
+            help='Retenció IRPF a aplicar', domain=[('name', 'like', 'IRPF')]
         ),
-        'info': fields.text('Info')
+        'info': fields.text('Info'),
+        'state': fields.char('State', size=16),
     }
 
     _defaults = {
         'info': _default_info,
+        'state': lambda *a: 'init',
     }
+
 
 WizardComputeMod193Invoice()
