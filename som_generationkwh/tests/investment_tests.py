5142193870a9ef5452cfd94e08ce00ae79ce03b5
Oriol Piera <oriol.piera@somenergia.coop>
2020-01-08 17:50:48 +0100

    New destral test create_from_form and some semantic ids

--- som_generationkwh/tests/investment_tests.py
+++ som_generationkwh/tests/investment_tests.py
@@ -6,7 +6,6 @@ import netsvc
 import time
 import random
 
-
 class InvestmentTests(testing.OOTestCase):
     def test_mark_as_signed(self):
         """
@@ -78,20 +77,25 @@ class InvestmentTests(testing.OOTestCase):
         :return:
         """
         pool = self.openerp.pool
-        investment_obj = pool.get('investment.aportacio')
+        investment_obj = pool.get('generationkwh.investment')
         imd_obj = pool.get('ir.model.data')
+        partner_obj = pool.get('res.partner')
+        mailmockup = pool.get('generationkwh.mailmockup')
         with Transaction().start(self.database) as txn:
             cursor = txn.cursor
             uid = txn.user
+            mailmockup.activate(cursor, uid)
+            partner_id = imd_obj.get_object_reference(
+                        cursor, uid, 'som_generationkwh', 'res_partner_inversor1'
+                        )[1]
 
             inv_id = investment_obj.create_from_form(cursor, uid,
-                    1,
+                    partner_id,
                     '2017-01-06',
                     4000,
                     '10.10.23.123',
                     'ES7712341234161234567890',
                     'emissio_apo')
-
             inv_0001 = investment_obj.read(cursor, uid, inv_id)
             inv_0001.pop('actions_log')
             inv_0001.pop('log')
@@ -103,14 +107,16 @@ class InvestmentTests(testing.OOTestCase):
                     'first_effective_date': False,
                     'move_line_id': False,
                     'last_effective_date': False,
-                    'nshares': 10,
-                    'signed_date': '2019-12-19',
+                    'nshares': 40,
+                    'signed_date': False,
                     'draft': True,
                     'purchase_date': False,
-                    'member_id': (1, u'Cognoms, Nom'),
+                    'member_id': (1, u'Gil, Pere'),
                     'active': True,
-                    'order_date': '2019-10-01',
+                    'order_date': '2017-01-06',
                     'amortized_amount': 0.0,
-                    'name': u'APO00001'
+                    'name': u'APO005000'
                 })
+            mailmockup.deactivate(cursor, uid)
+
 # vim: et ts=4 sw=4
