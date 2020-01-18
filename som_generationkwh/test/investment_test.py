fab04b5504a5c73413b459da11e9714bae37dbb2
David García Garzón <voki@canvoki.net>
2020-01-18 10:53:56 +0100

    updating fragile test on gis amortizations

--- som_generationkwh/test/investment_test.py
+++ som_generationkwh/test/investment_test.py
@@ -72,8 +72,6 @@ class Investment_OLD_Test(unittest.TestCase):
                 [1, '2017-09-21', False, -1],
                 [1, '2017-09-26', False, -1],
                 [1, '2017-09-28', False, -1],
-                #[1, '2016-05-19', False, -86], #Fiscal year closing
-                #[1, '2016-05-19', False, 86],  #Fiscal year closing
                 [1, '2017-12-01', False, -2],
                 [1, '2017-12-01', False, -2],
                 [1, '2018-08-24', False, -1],
@@ -84,6 +82,8 @@ class Investment_OLD_Test(unittest.TestCase):
                 [1, '2019-07-11', False, -1],
                 [1, '2019-07-11', False, -1],
                 [1, '2019-08-06', False, -1],
+                [1, '2019-12-03', False, -2],
+                [1, '2019-12-03', False, -2],
             ])
 
     def test__create_from_accounting__restrictingFirst(self):
