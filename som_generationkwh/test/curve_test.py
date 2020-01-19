9410bb7ea943f2d200d0c2b3c02438c226c4ebc1
David García Garzón <voki@canvoki.net>
2020-01-19 03:32:20 +0100

    proper import of destructiveTest

--- som_generationkwh/test/curve_test.py
+++ som_generationkwh/test/curve_test.py
@@ -12,7 +12,7 @@ import genkwh_curve
 from genkwh_curve import isodate
 from yamlns import namespace as ns
 import generationkwh.investmentmodel as gkwh
-from somutils.testutils import destructive_test
+from somutils.testutils import destructiveTest
 
 @destructiveTest
 class CurveExporter_Test(unittest.TestCase):
