fa1d8d1bc431178d6048651e40043a45324fe02c
david <davidsuarez92@gmail.com>
2020-04-24 02:43:07 +0200

    using sql query in order to solve timeout

---
+++ sql/genkwh_savings_between_dates_for_partner.sql
@@ -0,0 +1,7 @@
+SELECT gkwh_owner.owner_id AS partner_id, par.vat as vat, SUM(gkwh_owner.saving_gkw_amount) as amount
+FROM generationkwh_invoice_line_owner gkwh_owner
+INNER JOIN giscedata_facturacio_factura f ON f.id=gkwh_owner.factura_id
+INNER JOIN account_invoice i ON i.id=f.invoice_id
+INNER JOIN res_partner par ON par.id = gkwh_owner.owner_id
+WHERE partner_id = %(partner_id)s AND i.date_invoice BETWEEN %(start)s AND %(end)s
+GROUP BY gkwh_owner.owner_id, par.vat
