2554d5cf2260a69f2139dc409c0380400e014055
David Suárez <davidsuarez92@gmail.com>
2020-07-10 16:10:38 +0200

    wizard & send mail funct added

--- som_generationkwh/wizard/__init__.py
+++ som_generationkwh/wizard/__init__.py
@@ -9,3 +9,4 @@ import wizard_investment_transfer
 import wizard_aeat193_from_gkwh_invoices
 import wizard_send_retencio_estalvi_to_members
 import wizard_baixa_soci
+import wizard_contract_sign_signaturit
