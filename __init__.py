0ac8af04f474e37864b15242e8b065460b837af4
David Suárez <davidsuarez92@gmail.com>
2021-02-26 12:48:03 +0100

    added callbacks & fix

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
-import powersms_core
-import powersms_templates
-import powersms_smsbox
-import wizard
+from . import powersms_core
+from . import powersms_templates
+from . import powersms_smsbox
+from . import wizard
 import logging
 
 logger = logging.getLogger('openerp.powersms')
