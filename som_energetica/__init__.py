711c0c93aff10d6c608678bdfc35160cefa40db0
Oriol Piera <oriol.piera@somenergia.coop>
2021-03-19 16:20:20 +0100

    Add som_energetica module

---
+++ som_energetica/__init__.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+import giscedata_polissa
+import giscedata_facturacio
---
+++ som_energetica/__terp__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Adaptació Energética per Som Energia",
+    "description": """Aquest mòdul afegeix les següents funcionalitats:
+  * Menú Energética
+""",
+    "version": "0-dev",
+    "author": "GISCE",
+    "category": "Master",
+    "depends":[
+        "base",
+        "giscedata_facturacio_comer",
+        "som_polissa_soci",
+        "som_switching",
+    ],
+    "init_xml": [],
+    "demo_xml": [],
+    "update_xml":[
+        "som_energetica_view.xml",
+        "som_energetica_data.xml",
+        "giscedata_facturacio_data.xml"
+    ],
+    "active": False,
+    "installable": True
+}
