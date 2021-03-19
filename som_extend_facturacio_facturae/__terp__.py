8581f8bbb750da36de12f25f4d49d2525613cb39
Oriol Piera <oriol.piera@somenergia.coop>
2021-03-19 16:38:07 +0100

    Add som_extend_facturacio_facturae module

---
+++ som_extend_facturacio_facturae/__terp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Som Energia facturae extension",
+    "description": """SomEnergia facturae extension""",
+    "version": "0-dev",
+    "author": "Som Energia",
+    "category": "Master",
+    "depends":[
+        "base",
+        "giscedata_facturacio",
+        "giscedata_facturacio_facturae"
+    ],
+    "init_xml": [],
+    "demo_xml": [],
+    "update_xml":[
+        "giscedata_facturacio_view.xml"
+    ],
+    "active": False,
+    "installable": True
+}
