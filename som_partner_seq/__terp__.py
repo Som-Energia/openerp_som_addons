8df2026f1a6485e8edfbb75252bdd9f306c7cd50
Oriol Piera <oriol.piera@somenergia.coop>
2021-03-19 16:37:34 +0100

    Add som_partner_seq module

---
+++ som_partner_seq/__terp__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Seqüencies de partner per SomEnergia",
+    "description": """
+    This module provide :
+        * A sequence for partners SXXXXXX.
+    """,
+    "version": "0-dev",
+    "author": "GISCE",
+    "category": "SomEnergia",
+    "depends":[
+        "base"
+    ],
+    "init_xml": [],
+    "demo_xml": [],
+    "update_xml":[
+        "som_partner_seq_data.xml"
+    ],
+    "active": False,
+    "installable": True
+}
