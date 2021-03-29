7a7e018e6432bfde8f77fb5eab23933963e47c71
Oriol Piera <oriol.piera@somenergia.coop>
2021-03-29 12:15:59 +0200

    Add som_control_reports module

---
+++ som_webforms_helpers/__terp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Webforms Helpers for Som Energia",
+    "description": """Aquest mòdul afegeix funcions per donar suport a l'API de Webforms""",
+    "version": "0-dev",
+    "author": "SOMEnergia",
+    "category": "Master",
+    "depends":[
+        "base",
+        "giscedata_facturacio_comer",
+        "som_facturacio_switching",
+        "som_generationkwh"
+    ],
+    "init_xml": [],
+    "demo_xml": [],
+    "update_xml":[
+    ],
+    "active": False,
+    "installable": True
+}
