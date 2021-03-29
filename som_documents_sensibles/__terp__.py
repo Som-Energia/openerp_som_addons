689694c9f797e8c7c3029b7a5dc305649f42048e
Oriol Piera <oriol.piera@somenergia.coop>
2021-03-29 12:15:41 +0200

    Add som_control_reports module

---
+++ som_documents_sensibles/__terp__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Documents sensibles for Som Energia",
+    "description": """Aquest mòdul afegeix les següents funcionalitats:
+  * Afegeix el model de dades per a clients electrodependents
+""",
+    "version": "0-dev",
+    "author": "SOMEnergia",
+    "category": "Master",
+    "depends":[
+        "base",
+    ],
+    "init_xml": [],
+    "demo_xml": [],
+    "update_xml":[
+        'som_documents_sensibles_view.xml',
+        'som_documents_sensibles_data.xml',
+        'security/ir.model.access.csv',
+    ],
+    "active": False,
+    "installable": True
+}
