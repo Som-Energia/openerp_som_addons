ec6fb5ca19cef85f87a170e975f783a8ba4ac97d
Oriol Piera <oriol.piera@somenergia.coop>
2021-04-15 13:04:09 +0200

    Move module som_atc from main repo

---
+++ som_atc/__terp__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "som_atc",
+    "description": """Aquest mòdul afegeix les següents funcionalitats:
+    * Funcionalitats sobre casos d'atencio al client
+    """,
+    "version": "0-dev",
+    "author": "GISCE",
+    "category": "Master",
+    "depends": [
+        "giscedata_atc"
+    ],
+    "init_xml": [],
+    "demo_xml": [],
+    "update_xml": [],
+    "active": False,
+    "installable": True
+}
