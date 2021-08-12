e86f8582b9f9c55216a424ab8bb29702a5980585
AdriaOrellana <adria.orellana@somenergia.coop>
2021-08-12 16:34:01 +0200

    ADD C1 01 to informe

---
+++ som_informe/report/components/C101/C101.py
@@ -0,0 +1,33 @@
+from gestionatr.utils import get_description
+from ..component_utils import dateformat
+from ..ProcesC1 import ProcesC1
+
+class C101(ProcesC1.ProcesC1):
+    def __init__(self):
+        ProcesC1.ProcesC1.__init__(self)
+
+    def get_data(self, wiz, cursor, uid, step):
+        result = ProcesC1.ProcesC1.get_data(self, wiz, cursor, uid, step)
+        result['type'] = 'C101'
+        result['text'] = step.comentaris
+        if len(step.document_ids) == 0:
+            result['adjunts'] = False
+
+        """
+        swl_obj = step.pool.get('giscedata.switching.log')
+
+        search_params = [
+            ('request_code','=',step.sw_id.codi_sollicitud),
+            ('tipus','=','export'),
+            ('proces','=','C1'),
+            ('pas','=', '01'),
+            ('status', '=', 'correcte')
+        ]
+        swl_ids = swl_obj.search(cursor, uid, search_params)
+
+        if len(swl_ids) > 0:
+            swl = swl_obj.browse(cursor, uid, swl_ids[0])
+            result['day'] = dateformat(swl.case_date)
+        """
+
+        return result
---
+++ som_informe/report/components/ProcesC1/__init__.py
@@ -0,0 +1,1 @@
+import ProcesC1
