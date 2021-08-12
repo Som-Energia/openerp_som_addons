bc0917ebecbbeb866d3ae4125bc389caf701c5af
marite <marite.guerrieri@somenergia.coop>
2021-08-12 13:38:38 +0200

    Remove ES from NIF and change date format for data_alta

--- som_informe/wizard/wizard_create_technical_report.py
+++ som_informe/wizard/wizard_create_technical_report.py
@@ -149,10 +149,10 @@ class WizardCreateTechnicalReport(osv.osv_memory):
         return [{
             'type': 'header',
             'date': '1970-01-01',
-            'data_alta': pol_data.data_alta,
+            'data_alta': dateformat(pol_data.data_alta, False),
             'contract_number': pol_data.name,
             'titular_name': pol_data.titular.name,
-            'titular_nif': pol_data.titular_nif,
+            'titular_nif': pol_data.titular_nif[2:11],
             'distribuidora': pol_data.distribuidora.name,
             'distribuidora_contract_number': pol_data.ref_dist,
             'cups': pol_data.cups.name,
