56cc9f313bdf45f3d32c08d269034852aab4daa9
AdriaOrellana <adria.orellana@somenergia.coop>
2021-08-13 15:57:59 +0200

    ADD C1 08 to informe

---
+++ som_informe/report/components/C108/C108.py
@@ -0,0 +1,14 @@
+from gestionatr.utils import get_description
+from ..component_utils import dateformat
+from ..ProcesC1 import ProcesC1
+
+class C108(ProcesC1.ProcesC1):
+    def __init__(self):
+        ProcesC1.ProcesC1.__init__(self)
+
+    def get_data(self, wiz, cursor, uid, step):
+        result = ProcesC1.ProcesC1.get_data(self, wiz, cursor, uid, step)
+        result['type'] = 'C108'
+        result['data_creacio'] = step.date_created
+
+        return result
