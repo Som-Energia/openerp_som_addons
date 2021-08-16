e356c3cc82f29ecb49f450b4fcf37cc339d571f3
AdriaOrellana <adria.orellana@somenergia.coop>
2021-08-16 11:58:03 +0200

    ADD C2 02 to informe

---
+++ som_informe/report/components/C202/C202.py
@@ -0,0 +1,21 @@
+from gestionatr.utils import get_description
+from ..component_utils import dateformat
+from ..ProcesC2 import ProcesC2
+
+class C202(ProcesC2.ProcesC2):
+    def __init__(self):
+        ProcesC2.ProcesC2.__init__(self)
+
+    def get_data(self, wiz, cursor, uid, step):
+        result = ProcesC2.ProcesC2.get_data(self, wiz, cursor, uid, step)
+        result['type'] = 'C202'
+        result['rebuig'] = step.rebuig
+        result['rebutjos'] = []
+        for rebuig in step.rebuig_ids:
+            result['rebutjos'].append({
+                    'codi' : rebuig.motiu_rebuig.name,
+                    'descripcio' : rebuig.desc_rebuig
+                })
+        result['data_rebuig'] = step.data_rebuig
+
+        return result
