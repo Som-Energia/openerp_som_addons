5d1a092d7a741b36ba4111da7a3e09164c7889b4
AdriaOrellana <adria.orellana@somenergia.coop>
2021-08-16 14:26:35 +0200

    ADD C2 09 to informe

---
+++ som_informe/report/components/C209/C209.py
@@ -0,0 +1,36 @@
+from gestionatr.utils import get_description
+from ..component_utils import dateformat
+from ..ProcesC2 import ProcesC2
+
+class C209(ProcesC2.ProcesC2):
+    def __init__(self):
+        ProcesC2.ProcesC2.__init__(self)
+
+    def get_data(self, wiz, cursor, uid, step):
+        result = ProcesC2.ProcesC2.get_data(self, wiz, cursor, uid, step)
+        result['type'] = 'C209'
+        result['data_creacio'] = step.date_created
+        result['rebuig'] = step.rebuig
+        result['rebutjos'] = []
+        for rebuig in step.rebuig_ids:
+            result['rebutjos'].append({
+                    'descripcio' : rebuig.desc_rebuig
+                })
+        """
+        swl_obj = step.pool.get('giscedata.switching.log')
+
+        search_params = [
+            ('request_code','=',step.sw_id.codi_sollicitud),
+            ('tipus','=','export'),
+            ('proces','=','C2'),
+            ('pas','=', '09'),
+            ('status', '=', 'correcte')
+        ]
+        swl_ids = swl_obj.search(cursor, uid, search_params)
+
+        if len(swl_ids) > 0:
+            swl = swl_obj.browse(cursor, uid, swl_ids[0])
+            result['day'] = dateformat(swl.case_date)
+        """
+
+        return result
