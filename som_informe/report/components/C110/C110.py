2e3c79ba10474bc0d3d395e2d756b2ed6b46e238
AdriaOrellana <adria.orellana@somenergia.coop>
2021-08-16 16:15:04 +0200

    Remove comment from logs

--- som_informe/report/components/C110/C110.py
+++ som_informe/report/components/C110/C110.py
@@ -11,21 +11,5 @@ class C110(ProcesC1.ProcesC1):
         result['type'] = 'C110'
         result['data_creacio'] = step.date_created
         result['data_acceptacio'] = step.data_acceptacio
-        """
-        swl_obj = step.pool.get('giscedata.switching.log')
-
-        search_params = [
-            ('request_code','=',step.sw_id.codi_sollicitud),
-            ('tipus','=','export'),
-            ('proces','=','C1'),
-            ('pas','=', '01'),
-            ('status', '=', 'correcte')
-        ]
-        swl_ids = swl_obj.search(cursor, uid, search_params)
-
-        if len(swl_ids) > 0:
-            swl = swl_obj.browse(cursor, uid, swl_ids[0])
-            result['day'] = dateformat(swl.case_date)
-        """
 
         return result
