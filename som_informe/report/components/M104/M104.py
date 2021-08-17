bb8d0947bb6e43adb2a556dfac9336af3e1e4351
AdriaOrellana <adria.orellana@somenergia.coop>
2021-08-17 16:14:04 +0200

    ADD M1 04 to informe

---
+++ som_informe/report/components/M104/M104.py
@@ -0,0 +1,20 @@
+from gestionatr.utils import get_description
+from ..component_utils import dateformat
+from ..ProcesM1 import ProcesM1
+
+class M104(ProcesM1.ProcesM1):
+    def __init__(self):
+        ProcesM1.ProcesM1.__init__(self)
+
+    def get_data(self, wiz, cursor, uid, step):
+        result = ProcesM1.ProcesM1.get_data(self, wiz, cursor, uid, step)
+        result['type'] = 'M104'
+        result['data_rebuig'] = step.data_rebuig
+        result['rebutjos'] = []
+        for rebuig in step.rebuig_ids:
+            result['rebutjos'].append({
+                    'codi' : rebuig.motiu_rebuig.name,
+                    'descripcio' : rebuig.desc_rebuig
+                })
+
+        return result
