dbd5dded6858b6c4c754cdfa9ebbe0a82b47d66b
AdriaOrellana <adria.orellana@somenergia.coop>
2021-08-17 16:04:55 +0200

    ADD M1 05 to informe

---
+++ som_informe/report/components/M105/M105.mako
@@ -0,0 +1,22 @@
+<%page args="d" />
+<li>
+    ${_(u"El %s Som Energia presenta la sol·licitud següent a la distribuïdora (%s)") % (d.day, d.distribuidora) }<br/>
+    <br>
+    ${_(u"<b>Procediment:</b> M1 (Modificació contractual)")}<br/>
+    ${_(u"<b>Pas:</b> 05")}<br/>
+    ${_(u"<b>Codi de la sol·licitud:</b> %s") % (d.codi_solicitud)}<br/>
+
+    %if d.tipus_sol == 'N' or d.tipus_sol=='A':
+        ${_(u"<b>Tipus autoconsum:</b> %s") % (d.tipus_autoconsum)}<br/>
+        ${_(u"<b>Control de potència:</b> %s")% (d.control_potencia)}<br/>
+        ${_(u"<b>Potència: </b>")}
+        % for pot in d.potencies[:-1]:
+            ${pot['name']} : ${pot['potencia']},
+        % endfor
+        ${d.potencies[-1]['name']}: ${d.potencies[-1]['potencia']}<br/>
+        ${_(u"<b>Tarifa:</b> %s") % (d.tarifa)}<br/>
+        ${_(u"<b>Tensió sol·licitada:</b> %s")% (d.tensio_sol)}<br/>
+    %endif
+
+    ${_(u"<b>Data activació:</b> %s")% (d.data_activacio)}<br/>
+</li>
---
+++ som_informe/report/components/M105/M105.py
@@ -0,0 +1,30 @@
+from gestionatr.utils import get_description
+from ..component_utils import dateformat
+from ..ProcesM1 import ProcesM1
+
+class M105(ProcesM1.ProcesM1):
+    def __init__(self):
+        ProcesM1.ProcesM1.__init__(self)
+
+    def get_data(self, wiz, cursor, uid, step):
+        result = ProcesM1.ProcesM1.get_data(self, wiz, cursor, uid, step)
+        result['type'] = 'M105'
+        result['tipus_autoconsum'] =  get_description(step.tipus_autoconsum, "TABLA_113")
+        result['control_potencia'] =  get_description(step.control_potencia, "TABLA_51")
+        result['potencies'] = []
+        for pot in step.pot_ids:
+            result['potencies'].append({
+                    'name' : pot.name,
+                    'potencia' : pot.potencia
+                })
+        result['tarifa'] =  get_description(step.tarifaATR, "TABLA_17")
+        result['data_activacio'] = step.data_activacio
+
+        swl_obj = step.pool.get('giscedata.switching')
+
+        swl_ids = swl_obj.browse(cursor, uid, step.sw_id.id)
+        model_obj = step.pool.get(swl_ids.step_ids[0].pas_id.split(',')[0])
+
+        result['tipus_sol'] =  model_obj.read(cursor, uid, (swl_ids.step_ids[0].pas_id).split(',')[1])[0]['sollicitudadm']
+        result['tensio_sol'] = get_description(model_obj.read(cursor, uid, (swl_ids.step_ids[0].pas_id).split(',')[1])[0]['tensio_solicitada'], "TABLA_64")
+        return result
