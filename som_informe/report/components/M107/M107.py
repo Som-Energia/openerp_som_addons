7d7ed29a3466bc99128062881770b94c0978a23d
AdriaOrellana <adria.orellana@somenergia.coop>
2021-08-17 16:28:33 +0200

    ADD M1 07 to informe

---
+++ som_informe/report/components/M107/M107.py
@@ -0,0 +1,18 @@
+from ..ProcesM1 import ProcesM1
+
+class M107(ProcesM1.ProcesM1):
+    def __init__(self):
+        ProcesM1.ProcesM1.__init__(self)
+
+    def get_data(self, wiz, cursor, uid, step):
+        result = ProcesM1.ProcesM1.get_data(self, wiz, cursor, uid, step)
+        result['type'] = 'M107'
+        result['rebuig'] = step.rebuig
+        result['data_creacio'] = step.date_created
+        result['rebutjos'] = []
+        for rebuig in step.rebuig_ids:
+            result['rebutjos'].append({
+                    'descripcio' : rebuig.desc_rebuig
+                })
+
+        return result
