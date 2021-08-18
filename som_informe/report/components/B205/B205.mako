39fa69e748c012653e2ba2b10d205b507abb7acf
AdriaOrellana <adria.orellana@somenergia.coop>
2021-08-18 16:49:18 +0200

    ADD B2 05 to informe

---
+++ som_informe/report/components/B205/B205.mako
@@ -0,0 +1,11 @@
+<%page args="d" />
+<li>
+    ${_(u"El %s Som Energia presenta la sol·licitud següent a la distribuïdora ( %s ):") % (d.day, d.distribuidora) }<br/>
+    <br>
+    ${_(u"<b>Procediment:</b> B2 (Baixa de subministrament o suspensió del subministrament)")}<br/>
+    ${_(u"<b>Pas:</b> 05")}<br/>
+    ${_(u"<b>Codi de la sol·licitud:</b> %s") % (d.codi_solicitud)}<br/>
+    ${_(u"<b>Motiu de la baixa:</b> %s") % (d.motiu_baixa)}<br/>
+    ${_(u"<b>Data d'activació:</b> %s") % (d.data_activacio)}<br/>
+    <br/>
+</li>
---
+++ som_informe/report/components/B205/B205.py
@@ -0,0 +1,14 @@
+from gestionatr.utils import get_description
+from ..component_utils import dateformat
+from ..ProcesB2 import ProcesB2
+
+class B205(ProcesB2.ProcesB2):
+    def __init__(self):
+        ProcesB2.ProcesB2.__init__(self)
+
+    def get_data(self, wiz, cursor, uid, step):
+        result = ProcesB2.ProcesB2.get_data(self, wiz, cursor, uid, step)
+        result['type'] = 'B205'
+        result['data_activacio'] = step.data_activacio
+        result['motiu_baixa'] = get_description(step.motiu, "TABLA_120")
+        return result
