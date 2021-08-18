64aea59f64ca7525653b20e3455e06fd52f42f9b
AdriaOrellana <adria.orellana@somenergia.coop>
2021-08-18 15:35:56 +0200

    ADD B1 04 to informe

---
+++ som_informe/report/components/B104/B104.py
@@ -0,0 +1,13 @@
+from gestionatr.utils import get_description
+from ..component_utils import dateformat
+from ..ProcesB1 import ProcesB1
+
+class B104(ProcesB1.ProcesB1):
+    def __init__(self):
+        ProcesB1.ProcesB1.__init__(self)
+
+    def get_data(self, wiz, cursor, uid, step):
+        result = ProcesB1.ProcesB1.get_data(self, wiz, cursor, uid, step)
+        result['type'] = 'B104'
+        result['data_rebuig'] = step.data_rebuig
+        return result
