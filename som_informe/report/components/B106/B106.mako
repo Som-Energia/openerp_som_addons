715adb240e62bce9ed4918284ce52ce88d4a7ab7
AdriaOrellana <adria.orellana@somenergia.coop>
2021-08-18 12:56:50 +0200

    ADD B1 06 to informe

---
+++ som_informe/report/components/B106/B106.mako
@@ -0,0 +1,16 @@
+<%page args="d" />
+<li>
+    ${_(u"El %s la distribuïdora ( %s ) ens comunica la incidència següent:") % (d.day, d.distribuidora) }<br/>
+    <br>
+    ${_(u"<b>Procediment:</b> B1 (Alta de subministrament)")}<br/>
+    ${_(u"<b>Pas:</b> 06")}<br/>
+    ${_(u"<b>Codi de la sol·licitud:</b> %s") % (d.codi_solicitud)}<br/>
+    % for incidencia in d.incidencies:
+        ${_(u"<b>Tipus d'incidència:</b> %s") % (incidencia['tipus'])}<br/>
+        ${_(u"<b>Comentaris sobre la incidència:</b> %s") % (incidencia['comentari'])}<br/>
+        <br/>
+    % endfor
+    ${_(u"<b>Data d'incidència:</b> %s") % (d.data_incidencia)}<br/>
+    ${_(u"<b>Data prevista acció:</b> %s") % (d.data_prevista_accio)}<br/>
+    <br/>
+</li>
---
+++ som_informe/report/components/B106/B106.py
@@ -0,0 +1,20 @@
+from gestionatr.utils import get_description
+from ..component_utils import dateformat
+from ..ProcesB1 import ProcesB1
+
+class B106(ProcesB1.ProcesB1):
+    def __init__(self):
+        ProcesB1.ProcesB1.__init__(self)
+
+    def get_data(self, wiz, cursor, uid, step):
+        result = ProcesB1.ProcesB1.get_data(self, wiz, cursor, uid, step)
+        result['type'] = 'B106'
+        result['data_incidencia'] = step.data_incidencia
+        result['data_prevista_accio'] = step.data_prevista_accio
+        result['incidencies'] = []
+        for incidencia in step.incidencia_ids:
+            result['incidencies'].append({
+                    'tipus' : incidencia.motiu_incidencia,
+                    'comentari' : incidencia.desc_incidencia
+            })
+        return result
