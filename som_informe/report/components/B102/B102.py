11a78f41ff09fefe5a27b25766b88e2abaa97e18
AdriaOrellana <adria.orellana@somenergia.coop>
2021-08-18 12:29:54 +0200

    ADD B1 02 to informe

---
+++ som_informe/report/components/B102/B102.py
@@ -0,0 +1,21 @@
+from gestionatr.utils import get_description
+from ..component_utils import dateformat
+from ..ProcesB1 import ProcesB1
+
+class B102(ProcesB1.ProcesB1):
+    def __init__(self):
+        ProcesB1.ProcesB1.__init__(self)
+
+    def get_data(self, wiz, cursor, uid, step):
+        result = ProcesB1.ProcesB1.get_data(self, wiz, cursor, uid, step)
+        result['type'] = 'B102'
+        result['rebuig'] = step.rebuig
+        result['rebutjos'] = []
+        for rebuig in step.rebuig_ids:
+            result['rebutjos'].append({
+                    'codi' : rebuig.motiu_rebuig.name,
+                    'descripcio' : rebuig.desc_rebuig
+                })
+        result['data_rebuig'] = step.data_rebuig
+        result['data_activacio'] = step.data_ult_lect
+        return result
