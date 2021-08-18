3089c5a3f7af47a61ce78946290d68d4ea538f6f
AdriaOrellana <adria.orellana@somenergia.coop>
2021-08-18 12:14:54 +0200

    ADD B1 to informe

---
+++ som_informe/report/components/B101/B101.mako
@@ -0,0 +1,13 @@
+<%page args="d" />
+<li>
+    ${_(u"El %s Som Energia presenta la sol·licitud següent a la distribuïdora ( %s ):") % (d.day, d.distribuidora) }<br/>
+    <br>
+    ${_(u"<b>Procediment:</b> B1 (Alta de subministrament)")}<br/>
+    ${_(u"<b>Pas:</b> 01")}<br/>
+    ${_(u"<b>Codi de la sol·licitud:</b> %s") % (d.codi_solicitud)}<br/>
+    %if d.text:
+        ${_(u"<b>Comentaris:</b> %s") % (d.text)}<br/>
+    %endif
+    ${_(u"<b>Motiu de la baixa:</b> %s") % (d.motiu_baixa)}<br/>
+    <br/>
+</li>
---
+++ som_informe/report/components/B101/B101.py
@@ -0,0 +1,31 @@
+from gestionatr.utils import get_description
+from ..component_utils import dateformat
+from ..ProcesB1 import ProcesB1
+
+class B101(ProcesB1.ProcesB1):
+    def __init__(self):
+        ProcesB1.ProcesB1.__init__(self)
+
+    def get_data(self, wiz, cursor, uid, step):
+        result = ProcesB1.ProcesB1.get_data(self, wiz, cursor, uid, step)
+        result['type'] = 'B101'
+        result['text'] = step.comentaris
+        result['motiu_baixa'] = get_description(step.motiu, "TABLA_10")
+
+        """
+        swl_obj = step.pool.get('giscedata.switching.log')
+
+        search_params = [
+            ('request_code','=',step.sw_id.codi_sollicitud),
+            ('tipus','=','export'),
+            ('proces','=','B1'),
+            ('pas','=', '01'),
+            ('status', '=', 'correcte')
+        ]
+        swl_ids = swl_obj.search(cursor, uid, search_params)
+
+        if len(swl_ids) > 0:
+            swl = swl_obj.browse(cursor, uid, swl_ids[0])
+            result['day'] = dateformat(swl.case_date)
+        """
+        return result
