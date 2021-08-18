3ae63cf62b904b0f970dbb1c722ad447b025e7e0
AdriaOrellana <adria.orellana@somenergia.coop>
2021-08-18 16:49:18 +0200

    ADD B2 05 to informe

---
+++ som_informe/report/components/ProcesB2/ProcesB2.py
@@ -0,0 +1,17 @@
+from ..component_utils import dateformat
+
+class ProcesB2:
+
+    def __init__(self):
+        pass
+
+    def get_data(self, wiz, cursor, uid, step):
+        result = {}
+        result['date'] = step.date_created
+        result['day'] = dateformat(step.date_created)
+        result['create'] = dateformat(step.date_created, True)
+        result['pas'] = step.sw_id.step_id.name
+        result['codi_solicitud'] = step.sw_id.codi_sollicitud
+        result['titol'] = step.sw_id.proces_id.name + " - " + step.sw_id.step_id.name
+        result['distribuidora'] = step.sw_id.partner_id.name
+        return result
