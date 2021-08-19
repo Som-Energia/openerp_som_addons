d698ba83427d820afd86196b22e3ed6dcbcba2e2
AdriaOrellana <adria.orellana@somenergia.coop>
2021-08-19 14:49:14 +0200

    ADD E1 13 to informe

---
+++ som_informe/report/components/E113/E113.py
@@ -0,0 +1,22 @@
+from gestionatr.utils import get_description
+from ..ProcesE1 import ProcesE1
+
+class E113(ProcesE1.ProcesE1):
+    def __init__(self):
+        ProcesE1.ProcesE1.__init__(self)
+
+    def get_data(self, wiz, cursor, uid, step):
+        result = ProcesE1.ProcesE1.get_data(self, wiz, cursor, uid, step)
+        result['type'] = 'E113'
+        result['data_creacio'] = step.date_created
+        result['contestacio_incidencia'] = get_description(step.tipus_contestacio, "TABLA_121")
+        result['nom_contacte'] = step.nom_contacte
+        result['email_contacte'] = step.email
+        result['telefons'] = []
+        for telefon in step.telefons:
+            result['telefons'].append({
+                'numero' : telefon.numero
+            })
+
+
+        return result
