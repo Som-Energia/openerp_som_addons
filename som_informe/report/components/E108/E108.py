ffbd761f7418524683b42199a3808833b31d66df
AdriaOrellana <adria.orellana@somenergia.coop>
2021-08-19 14:47:42 +0200

    ADD E1 08 to informe

---
+++ som_informe/report/components/E108/E108.py
@@ -0,0 +1,29 @@
+from gestionatr.utils import get_description
+from ..component_utils import dateformat
+from ..ProcesE1 import ProcesE1
+
+class E108(ProcesE1.ProcesE1):
+    def __init__(self):
+        ProcesE1.ProcesE1.__init__(self)
+
+    def get_data(self, wiz, cursor, uid, step):
+        result = ProcesE1.ProcesE1.get_data(self, wiz, cursor, uid, step)
+        result['type'] = 'E108'
+
+        """"
+        swl_obj = step.pool.get('giscedata.switching.log')
+
+        search_params = [
+            ('request_code','=',step.sw_id.codi_sollicitud),
+            ('tipus','=','export'),
+            ('proces','=','E1'),
+            ('pas','=', '08'),
+            ('status', '=', 'correcte')
+        ]
+        swl_ids = swl_obj.search(cursor, uid, search_params)
+
+        if len(swl_ids) > 0:
+            swl = swl_obj.browse(cursor, uid, swl_ids[0])
+            result['day'] = dateformat(swl.case_date)
+        """
+        return result
