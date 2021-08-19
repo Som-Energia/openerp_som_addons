05b81c03d3d18a8b81d5c18e7e9e9d72cc6ba394
AdriaOrellana <adria.orellana@somenergia.coop>
2021-08-19 12:48:42 +0200

    ADD E1 05 to informe

---
+++ som_informe/report/components/E105/E105.py
@@ -0,0 +1,14 @@
+from gestionatr.utils import get_description
+from ..component_utils import dateformat
+from ..ProcesE1 import ProcesE1
+
+class E105(ProcesE1.ProcesE1):
+    def __init__(self):
+        ProcesE1.ProcesE1.__init__(self)
+
+    def get_data(self, wiz, cursor, uid, step):
+        result = ProcesE1.ProcesE1.get_data(self, wiz, cursor, uid, step)
+        result['type'] = 'E105'
+        result['data_activacio'] = step.data_activacio
+        result['resultat_activacio'] =  get_description(step.resultat_activacio, "TABLA_118")
+        return result
