248b01862b4ffd1cf4b6cb2a37c4c7fdcf5f0b37
AdriaOrellana <adria.orellana@somenergia.coop>
2021-08-19 15:04:54 +0200

    ADD E1 10 to informe

---
+++ som_informe/report/components/E110/E110.py
@@ -0,0 +1,14 @@
+from gestionatr.utils import get_description
+from ..component_utils import dateformat
+from ..ProcesE1 import ProcesE1
+
+class E110(ProcesE1.ProcesE1):
+    def __init__(self):
+        ProcesE1.ProcesE1.__init__(self)
+
+    def get_data(self, wiz, cursor, uid, step):
+        result = ProcesE1.ProcesE1.get_data(self, wiz, cursor, uid, step)
+        result['type'] = 'E110'
+        result['data_acceptacio'] = step.data_acceptacio
+
+        return result
