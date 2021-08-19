92efe2c94446bb1fc2188da3094f676fcea8b89b
AdriaOrellana <adria.orellana@somenergia.coop>
2021-08-19 15:07:44 +0200

    Remove comment from logs

--- som_informe/report/components/E101/E101.py
+++ som_informe/report/components/E101/E101.py
@@ -12,7 +12,7 @@ class E101(ProcesE1.ProcesE1):
         result['codi_subjacent'] =step.atr_subjacent.codi_sollicitud
         result['data_subjacent'] = step.atr_subjacent.data_sollicitud
 
-        """"
+        
         swl_obj = step.pool.get('giscedata.switching.log')
 
         search_params = [
@@ -27,6 +27,6 @@ class E101(ProcesE1.ProcesE1):
         if len(swl_ids) > 0:
             swl = swl_obj.browse(cursor, uid, swl_ids[0])
             result['day'] = dateformat(swl.case_date)
-        """
+        
 
         return result
