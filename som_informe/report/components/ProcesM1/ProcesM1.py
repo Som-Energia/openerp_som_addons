e957a6d74c55081b8f318203b6a6767d6c6da22c
Francisco Jose Cubero Sanchez <francisco.cubero@somenergia.coop>
2021-09-21 13:45:03 +0200

    fix get step 01 in M105 process

--- som_informe/report/components/ProcesM1/ProcesM1.py
+++ som_informe/report/components/ProcesM1/ProcesM1.py
@@ -5,6 +5,18 @@ class ProcesM1:
     def __init__(self):
         pass
 
+    def get_step_01(self, wiz, cursor, uid, step):
+        return self.get_step(wiz, cursor, uid, step, '01')
+
+    def get_step(self, wiz, cursor, uid, step, step_name):
+        for sub_step in step.sw_id.step_ids:
+            if sub_step.step_id.name == step_name:
+                r_model,r_id = sub_step.pas_id.split(',')
+                model_obj = wiz.pool.get(r_model)
+                return model_obj.browse(cursor, uid, int(r_id))
+        return None
+
+
     def get_data(self, wiz, cursor, uid, step):
         result = {}
         result['date'] = step.date_created
