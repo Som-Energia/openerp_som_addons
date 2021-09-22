2222753c443144c82f3508f181822ba5d70add93
Francisco Jose Cubero Sanchez <francisco.cubero@somenergia.coop>
2021-09-22 16:59:58 +0200

    name left for A3's

--- som_informe/report/components/ProcesA3/ProcesA3.py
+++ som_informe/report/components/ProcesA3/ProcesA3.py
@@ -6,6 +6,9 @@ class ProcesA3(ProcesATR.ProcesATR):
     def __init__(self):
         ProcesATR.ProcesATR.__init__(self)
 
+    def proces_name(self):
+        return 'A3'
+
     def get_data(self, wiz, cursor, uid, step):
         result = ProcesATR.ProcesATR.get_data(self, wiz, cursor, uid, step)
         result['date'] = step.date_created
