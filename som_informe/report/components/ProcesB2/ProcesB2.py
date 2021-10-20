eaa9535c2b50a5c5a9bbabc2eedfa56c7ab29777
Francisco Jose Cubero Sanchez <francisco.cubero@somenergia.coop>
2021-10-20 11:09:39 +0200

    simplification for the get_data function, moved to ProcesATR

--- som_informe/report/components/ProcesB2/ProcesB2.py
+++ som_informe/report/components/ProcesB2/ProcesB2.py
@@ -1,7 +1,7 @@
 from ..component_utils import dateformat
 from ..ProcesATR import ProcesATR
 
-class ProcesB2:
+class ProcesB2(ProcesATR.ProcesATR):
 
     def __init__(self):
         ProcesATR.ProcesATR.__init__(self)
@@ -11,11 +11,4 @@ class ProcesB2:
 
     def get_data(self, wiz, cursor, uid, step):
         result = ProcesATR.ProcesATR.get_data(self, wiz, cursor, uid, step)
-        result['date'] = step.date_created
-        result['day'] = dateformat(step.date_created)
-        result['create'] = dateformat(step.date_created, True)
-        result['pas'] = step.sw_id.step_id.name
-        result['codi_solicitud'] = step.sw_id.codi_sollicitud
-        result['titol'] = step.sw_id.proces_id.name + " - " + step.sw_id.step_id.name
-        result['distribuidora'] = step.sw_id.partner_id.name
         return result
