af67721ea87c50c1ed2880a942c507f97164744b
Oriol Piera <oriol.piera@somenergia.coop>
2021-11-22 14:20:31 +0100

    Change origin of our function

--- som_infoenergia/giscedata_cups.py
+++ som_infoenergia/giscedata_cups.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from osv import osv, fields
+from osv import osv
 from tools.translate import _
 
 class GiscedataCupsPs(osv.osv):
@@ -7,6 +7,22 @@ class GiscedataCupsPs(osv.osv):
 
     _name = 'giscedata.cups.ps'
     _inherit = 'giscedata.cups.ps'
+
+    NEW_ORIGENS_CONANY = [
+        ('consums', _(u'Historic consums')),
+    ]
+
+    def __init__(self, pool, cursor):
+        ''' Afegim els nou orígen'''
+        super(GiscedataCupsPs, self).__init__(pool, cursor)
+        origens = self.get_fonts_consums_anuals(cursor, 1)
+        for origen in origens:
+            current_sel = dict(self._columns['conany_origen'].selection).keys()
+            if origen['origen'] not in current_sel:
+                new_sel = (origen['origen'],
+                           dict(self._NEW_ORIGENS_CONANY).get(
+                               origen['origen'], origen['origen']))
+                self._columns['conany_origen'].selection.append(new_sel)
 
     def get_fonts_consums_anuals(self, cursor, uid, context=None):
         ''' Afegim consum_anual_consum_lectures com a font de consum anual
@@ -17,7 +33,7 @@ class GiscedataCupsPs(osv.osv):
         vals = {'priority': 5,
                 'model': 'giscedata.polissa',
                 'func': 'get_consum_anual_consum_lectures',
-                'origen': 'lectures'}
+                'origen': 'consums'}
         llista.append(vals)
 
         return llista
