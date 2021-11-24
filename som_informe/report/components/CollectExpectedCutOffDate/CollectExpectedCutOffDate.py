1e73c6d7c2efc2f7c66b19410a96a1bc75d1442f
Francisco Jose Cubero Sanchez <francisco.cubero@somenergia.coop>
2021-11-24 11:03:35 +0100

    cut off date component and invoices component options

---
+++ som_informe/report/components/CollectExpectedCutOffDate/CollectExpectedCutOffDate.py
@@ -0,0 +1,16 @@
+from ..component_utils import dateformat
+
+class CollectExpectedCutOffDate:
+    def __init__(self):
+        pass
+
+    def get_data(self, w, cursor, uid, wiz, context):
+        return {
+            'type': 'CollectExpectedCutOffDate',
+            'expected_cut_off_date' : 'TODO find the date',
+            'distri_name' : wiz.polissa.distribuidora.name,
+        }
+
+
+
+
