272b4e225d7e01262525e64908e7b03944fb865e
Francisco Jose Cubero Sanchez <francisco.cubero@somenergia.coop>
2021-11-26 12:42:42 +0100

    collect cut off date component generated

--- som_informe/report/components/component_utils.py
+++ som_informe/report/components/component_utils.py
@@ -1,6 +1,20 @@
 from gestionatr.utils import get_description as gestion_atr_get_description
 from datetime import datetime
 
+def to_date(str_date, hours = False):
+    if not str_date:
+        return None
+    if not hours:
+        return datetime.strptime(str_date[0:10],'%Y-%m-%d')
+    return datetime.strptime(str_date,'%Y-%m-%d %H:%M:%S')
+
+def to_string(obj_date, hours = False):
+    if not obj_date:
+        return ""
+    if not hours:
+        return obj_date.strftime('%d-%m-%Y')
+    return obj_date.strftime('%d-%m-%Y %H:%M:%S')
+
 def dateformat(str_date, hours = False):
     if not str_date:
         return ""
