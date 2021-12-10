090844dc427c5b9a3ca905676adb52deed4113f3
Oriol Piera <oriol.piera@somenergia.coop>
2021-12-10 12:49:25 +0100

    First test module som_informe

--- som_informe/report/components/InvoiceF1NG/InvoiceF1NG.py
+++ som_informe/report/components/InvoiceF1NG/InvoiceF1NG.py
@@ -4,7 +4,7 @@ class InvoiceF1NG:
     def __init__(self):
         pass
 
-    def get_data(self, cursor, uid, wiz, invoice, context):
+    def get_data(self, cursor, uid, wiz, invoice, context={}):
         result = {}
         import pudb; pu.db
         f1_obj = wiz.pool.get('giscedata.facturacio.importacio.linia')
@@ -14,7 +14,7 @@ class InvoiceF1NG:
         f1_id = f1_obj.search(cursor,uid,search_params)
         f1 = f1_obj.browse(cursor, uid, f1_id[0])
         #for linia in f1.importacio_lectures_ids:
-        result['numero_edm'] = f1.importacio_lectures_ids[0][0].comptador
+        result['numero_edm'] = f1.importacio_lectures_ids[0].comptador
 
         result['type'] = 'InvoiceF1NG'
         result['date'] = invoice.date_invoice
@@ -32,7 +32,7 @@ class InvoiceF1NG:
         fact_ob = fact.search([('type','=','in_invoice')],limit=1) id 13803452
         fact_3 = fact.browse(fact_ob[0])
         f1_obj = O_testing.model('giscedata.facturacio.importacio.linia')
-        id3 = f1_obj.search([('invoice_number_text','=',fact_3.origin)]
+        id3 = f1_obj.search([('invoice_number_text','=',fact_3.origin)] id 8228697
         f1_obj.browse(id3).importacio_lectures_ids[0].read()
         f1_obj.browse(id3).importacio_lectures_ids[0][0].comptador
 
