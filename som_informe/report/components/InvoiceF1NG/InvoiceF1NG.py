b90388c6d63374eb2ef3e00701b82e3a93ffb5d2
Francisco Jose Cubero Sanchez <francisco.cubero@somenergia.coop>
2022-01-03 16:14:20 +0100

    control if there is a associated f1

--- som_informe/report/components/InvoiceF1NG/InvoiceF1NG.py
+++ som_informe/report/components/InvoiceF1NG/InvoiceF1NG.py
@@ -13,15 +13,18 @@ class InvoiceF1NG:
             ('invoice_number_text', '=', invoice.origin),
         ]
         f1_id = f1_obj.search(cursor,uid,search_params)
-        f1 = f1_obj.browse(cursor, uid, f1_id[0])
+        if f1_id:
+            f1 = f1_obj.browse(cursor, uid, f1_id[0])
+        else:
+            f1 = None
 
-        result['numero_edm'] = f1.importacio_lectures_ids[0].comptador if f1.importacio_lectures_ids else ""
+        result['numero_edm'] = f1.importacio_lectures_ids[0].comptador if f1 and f1.importacio_lectures_ids else ""
 
         #camps obligats per estructura
         result['type'] = 'InvoiceF1NG'
         result['date'] = invoice.date_invoice
 
-        result['distribuidora'] = f1.distribuidora_id.name
+        result['distribuidora'] = f1.distribuidora_id.name if f1 else "Sense F1 relacionat"
         result['invoice_type'] = invoice.rectificative_type
         result['invoice_date'] = dateformat(invoice.date_invoice)
         result['invoice_number'] = invoice.origin
@@ -31,24 +34,25 @@ class InvoiceF1NG:
         #taula
 
         result['linies'] = []
-        for linia in f1.importacio_lectures_ids:
-            dict_linia={}
-            dict_linia['description_lectures'] = get_description(linia.origen_actual,"TABLA_44")
-            dict_linia['origen_lectures'] = linia.origen_actual
-            dict_linia['magnitud'] = linia.magnitud
-            dict_linia['periode'] = linia.periode
-            dict_linia['magnitud_desc'] = get_description(linia.magnitud, "TABLA_43")
-            dict_linia['periode_desc'] = get_description(linia.periode, "TABLA_42")
-            dict_linia['lectura_inicial'] = linia.lectura_desde
-            dict_linia['lectura_final'] = linia.lectura_actual
-            #el consum entre s'ha de calcular?
-            dict_linia['consum_entre'] = linia.lectura_actual - linia.lectura_desde
-            dict_linia['ajust'] = linia.ajust
-            i_line = get_invoice_line(invoice, linia.magnitud, linia.periode)
-            dict_linia['total_facturat'] = i_line.quantity if i_line else ""
-            dict_linia['unit'] = get_unit_magnitude(linia.magnitud)
+        if f1:
+            for linia in f1.importacio_lectures_ids:
+                dict_linia={}
+                dict_linia['description_lectures'] = get_description(linia.origen_actual,"TABLA_44")
+                dict_linia['origen_lectures'] = linia.origen_actual
+                dict_linia['magnitud'] = linia.magnitud
+                dict_linia['periode'] = linia.periode
+                dict_linia['magnitud_desc'] = get_description(linia.magnitud, "TABLA_43")
+                dict_linia['periode_desc'] = get_description(linia.periode, "TABLA_42")
+                dict_linia['lectura_inicial'] = linia.lectura_desde
+                dict_linia['lectura_final'] = linia.lectura_actual
+                #el consum entre s'ha de calcular?
+                dict_linia['consum_entre'] = linia.lectura_actual - linia.lectura_desde
+                dict_linia['ajust'] = linia.ajust
+                i_line = get_invoice_line(invoice, linia.magnitud, linia.periode)
+                dict_linia['total_facturat'] = i_line.quantity if i_line else ""
+                dict_linia['unit'] = get_unit_magnitude(linia.magnitud)
 
-            result['linies'].append(dict_linia)
+                result['linies'].append(dict_linia)
 
 
         result['invoiced_days'] = invoice.dies
