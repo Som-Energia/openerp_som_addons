597cf21d56a128e29e710e2eb915ecc2d0c70e06
Francisco Jose Cubero Sanchez <francisco.cubero@somenergia.coop>
2022-02-18 13:22:49 +0100

    last minute requests changes

--- som_infoenergia/report/components/first_page/first_page.mako
+++ som_infoenergia/report/components/first_page/first_page.mako
@@ -7,3 +7,5 @@
 </div>
 <h1>${_(u'Oferta tarifa indexada Som Energia')}</h1>
 <h2>${_(u'%s') % d.nom_titular}</h2>
+<p class="data-oferta">${_(u'%s') % d.data_oferta}</p>
+<p class="codi-oferta">${_(u'%s') % d.codi_oferta}</p>
--- som_infoenergia/report/components/first_page/first_page.py
+++ som_infoenergia/report/components/first_page/first_page.py
@@ -1,4 +1,10 @@
+from datetime import datetime
+
 class first_page():
 
     def get_data(self, cursor, uid, object, extra_text, context):
-        return {'nom_titular': object.partner_id.name}
+        return {
+            'nom_titular': object.partner_id.name,
+            'data_oferta': datetime.today().strftime("%d-%m-%Y"),
+            'codi_oferta': extra_text.get('codi_oferta', "ERROR EXTRA TEXT: sense codi oferta"),
+        }
