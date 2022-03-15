d98b33255ce3c755d8c9ac8db3e0b89338b41755
Mar Jene <mar.jene@somenergia.coop>
2022-03-15 10:25:11 +0100

    fix: multiple invoice print was broken

--- giscedata_facturacio_comer_som/report/giscedata_facturacio_comer_som.py
+++ giscedata_facturacio_comer_som/report/giscedata_facturacio_comer_som.py
@@ -45,6 +45,8 @@ class FacturaReportSomWebkitParserHTML(webkit_report.WebKitParser):
             context = {}
         pool = pooler.get_pool(cursor.dbname)
         factura_o = pool.get('giscedata.facturacio.factura')
+        polissa_o = pool.get('giscedata.polissa')
+        partner_o = pool.get('res.partner')
         imd_o = pool.get('ir.model.data')
 
         sign_rp_id = imd_o.get_object_reference(cursor, uid,
@@ -53,22 +55,34 @@ class FacturaReportSomWebkitParserHTML(webkit_report.WebKitParser):
         sign_gp_id = imd_o.get_object_reference(cursor, uid,
             'giscedata_facturacio_comer_som', 'cat_gp_factura_sign'
         )[1]
+        have_to_sign = False
+        fact_info = factura_o.read(cursor, uid, ids, ['polissa_id', 'partner_id'])
+        partner_ids = [x['partner_id'][0] for x in fact_info]
+        partner_category_info = partner_o.read(cursor, uid, partner_ids, ['category_id'])
+        partner_category_ids = set()
+        for x in partner_category_info:
+            partner_category_ids.update(x['category_id'])
+        have_to_sign = sign_rp_id in partner_category_ids
+        if not have_to_sign:
+            polissa_ids = [x['polissa_id'][0] for x in fact_info]
+            polissa_category_info = polissa_o.read(cursor, uid, polissa_ids, ['category_id'])
+            polissa_category_ids = set()
+            for x in polissa_category_info:
+                polissa_category_ids.update(x['category_id'])
 
-        for f_id in ids:
-            f = factura_o.browse(cursor, uid, f_id, context)
-            if sign_rp_id in [c.id for c in f.partner_id.category_id] \
-                or sign_gp_id in [c.id for c in f.polissa_id.category_id]:
+            have_to_sign = sign_gp_id in polissa_category_ids
 
-                ctx = context.copy()
-                ctx['webkit_signed_pdf'] = True
-                ctx['extra_commands'] = ['-V','--l2-text "Firma digital a {}"'.format(datetime.today().strftime('%d/%m/%Y')),'-llx 320','-lly 1030']
-                res = super(FacturaReportSomWebkitParserHTML, self).create(
-                    cursor, uid, [f_id], data, context=ctx
-                )
-            else:
-                res = super(FacturaReportSomWebkitParserHTML, self).create(
-                    cursor, uid, [f_id], data, context=context
-                )
+        if have_to_sign:
+            ctx = context.copy()
+            ctx['webkit_signed_pdf'] = True
+            ctx['extra_commands'] = ['-V','--l2-text "Firma digital a {}"'.format(datetime.today().strftime('%d/%m/%Y')),'-llx 320','-lly 1030']
+            res = super(FacturaReportSomWebkitParserHTML, self).create(
+                cursor, uid, ids, data, context=ctx
+            )
+        else:
+            res = super(FacturaReportSomWebkitParserHTML, self).create(
+                cursor, uid, ids, data, context=context
+            )
         return res
 
 FacturaReportSomWebkitParserHTML(
