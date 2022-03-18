9644352ab8fef4b2a890a929f4244b6ac92ff28d
Francisco Jose Cubero Sanchez <francisco.cubero@somenergia.coop>
2022-03-18 18:06:09 +0100

    actions control added

--- som_facturacio_calculada/giscedata_polissa.py
+++ som_facturacio_calculada/giscedata_polissa.py
@@ -38,25 +38,26 @@ class GiscedataPolissaCalculada(osv.osv):
         lc_origin = imd_o.get_object_reference(
             cursor, uid, 'som_facturacio_calculada', 'origen_lect_calculada'
         )[1]
+        msgs = []
         for _id in ids:
             mtr_ids = mtr_o.search(cursor, uid, [
                 ('polissa.id', '=', _id),
                 ('active', '=', True)
             ])
             if not mtr_ids:
-                print "No s'ha trobat comptador actiu per la pòlissa ID: {}".format(_id)
-                break
+                msgs.append("No s'ha trobat comptador actiu per la pòlissa ID: {}".format(_id))
+                continue
             if len(mtr_ids) != 1:
-                print "Multiples comptadors actius per la pòlissa ID: {}".format(_id)
-                break
+                msgs.append("Multiples comptadors actius per la pòlissa ID: {}".format(_id))
+                continue
             mtr_id = mtr_ids[0]
 
             pol_data = self.read(cursor, uid, _id, ['data_ultima_lectura','cups'])
             data_ultima_lect = pol_data['data_ultima_lectura']
 
             if not data_ultima_lect:
-                print "Pòlissa ID sense data ultima lectura: {}".format(_id)
-                break
+                msgs.append("Pòlissa ID sense data ultima lectura: {}".format(_id))
+                continue
             data_seguent_lect = (datetime.strptime(data_ultima_lect,'%Y-%m-%d') + timedelta(days=7)).strftime("%Y-%m-%d")
             start_date = (datetime.strptime(data_ultima_lect,'%Y-%m-%d') + timedelta(days=1)).strftime("%Y-%m-%d")
             cups_text = pol_data['cups'][1]
@@ -65,8 +66,8 @@ class GiscedataPolissaCalculada(osv.osv):
                 ('datetime', '>', data_seguent_lect),
             ], limit=1)
             if not tg_ids:
-                print "Pòlissa ID amb data última {} sense corbes en la data {}: {}".format(data_ultima_lect, data_seguent_lect, _id)
-                break
+                msgs.append("Pòlissa ID amb data última {} sense corbes en la data {}: {}".format(data_ultima_lect, data_seguent_lect, _id))
+                continue
             vals = {
                 'measure_origin': lc_origin,
                 'insert_reactive': False,
@@ -83,8 +84,9 @@ class GiscedataPolissaCalculada(osv.osv):
 
             wiz_measures_curve_o.load_measures(cursor, uid, [wiz_id], context=ctx)
             wiz_measures_curve_o.create_measures(cursor, uid, [wiz_id], context=ctx)
-
+            msgs.append("Lectures creades en data {} per la polissa {}".format(data_seguent_lect, _id))
         self.retrocedir_lot(cursor, uid, ids, context=context)
+        return msgs
 
     """
     @job(queue="facturacio_calculada")
