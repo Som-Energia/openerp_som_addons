c20a9f41db607ed029d52308b20c39eb2c7bdeb3
susu105 <davidsuarez92@gmail.com>
2022-04-20 17:26:18 +0200

    Added allow_grouped

--- account_invoice_som/wizard/wizard_payment_order_add_invoices.py
+++ account_invoice_som/wizard/wizard_payment_order_add_invoices.py
@@ -49,6 +49,9 @@ class WizardPaymentOrderAddInvoices(osv.osv_memory):
         for field in search_params_relation.keys():
             if getattr(wiz, field):
                 search_params += (search_params_relation[field])
+        if not wiz.allow_grouped:
+            search_params += [('group_move_id', '=', False)]
+
         res_ids = inv_obj.search(cursor, uid, search_params + [('payment_order_id','=',False)])
         values = {
             'len_result': 'La cerca ha trobat {} resultats'.format(len(res_ids)),
@@ -120,6 +123,8 @@ class WizardPaymentOrderAddInvoices(osv.osv_memory):
         'invoice_type': fields.selection([(False, '')]+ INVOICE_TYPES, _('Tipus')),
         'fiscal_position': fields.many2one('account.fiscal.position', 'Posició Fiscal'),
         'payment_type': fields.many2one('payment.type', 'Tipus de pagament'),
+        'allow_grouped': fields.boolean('Permetre agrupacions',
+            help='Activar aquesta opció admet factures agrupades')
     }
 
     _defaults = {
@@ -129,7 +134,8 @@ class WizardPaymentOrderAddInvoices(osv.osv_memory):
         'init_date': lambda *a: time.strftime('%Y-%m-%d'),
         'end_date': lambda *a: time.strftime('%Y-%m-%d'),
         'invoice_type': 'out_invoice',
-        'len_result': lambda *a: ''
+        'len_result': lambda *a: '',
+        'allow_grouped': False
     }
 
 WizardPaymentOrderAddInvoices()
