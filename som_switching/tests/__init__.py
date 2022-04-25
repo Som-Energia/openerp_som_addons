b47785ff56bc362da8ee1118cf21cc35c0dfc8a9
marite <marite.guerrieri@somenergia.coop>
2022-04-25 17:54:55 +0200

    Tests

--- som_switching/tests/__init__.py
+++ som_switching/tests/__init__.py
@@ -5,3 +5,4 @@ from tests_activacions_cn import *
 from tests_activacions_b1 import *
 from tests_activacions_b2 import *
 from tests_wizard_comment_to_F1 import *
+from tests_wizard_switching_b1 import *
