90f07f098b13e637e28386bbf0799c745c15be75
Oriol Piera <oriol.piera@somenergia.coop>
2022-05-02 16:48:18 +0200

    Move module www_som from gisce/erp repo

---
+++ www_som/__terp__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Integració WWW",
+    "description": """
+Mòdul per la integració de l'oficina virtual
+    """,
+    "version": "0-dev",
+    "author": "GISCE-TI, S.L.",
+    "category": "www",
+    "depends":[
+        "base",
+        "www_base",
+        "som_polissa_soci",
+        "giscedata_lectures_comer",
+        "giscedata_lectures_pool",
+        "giscedata_facturacio_impagat"
+    ],
+    "init_xml": [],
+    "demo_xml": [],
+    "update_xml":[
+        "www_som_data.xml",
+        "res_partner_view.xml"
+    ],
+    "active": False,
+    "installable": True
+}
