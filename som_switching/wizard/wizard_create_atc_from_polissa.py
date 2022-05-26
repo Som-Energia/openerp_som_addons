bb9d24ca108d5227fb4455127e7958c93214084e
pauboixsom <pau.boix@somenergia.coop>
2022-05-26 10:11:33 +0200

    fixed tag and functions wizard

--- som_switching/wizard/wizard_create_atc_from_polissa.py
+++ som_switching/wizard/wizard_create_atc_from_polissa.py
@@ -6,6 +6,30 @@ class WizardCreateAtc(osv.osv_memory):
 
     _inherit = "wizard.create.atc.from.polissa"
 
+    def create_atc_case(self, cursor, uid, ids, from_model, context=None):
+
+        if context is None:
+            context = {}
+        res = super(WizardCreateAtc, self).create_atc_case(cursor, uid, ids, from_model, context=context)
+        wizard = self.browse(cursor, uid, ids[0], context)
+        atc_obj = self.pool.get('giscedata.atc')
+        for atc_id in wizard.generated_cases:
+            if wizard.tag:
+                atc_obj.write(cursor, uid, atc_id, {'tag': wizard.tag.id})
+        return res
+
+    def onchange_subtipus(self, cursor, uid, ids, subtipus_id):
+
+        res = super(WizardCreateAtc, self).onchange_subtipus(cursor, uid, ids, subtipus_id)
+        section_id = res['value']['section_id']
+
+        section_obj = self.pool.get('crm.case.section')
+        seccio = section_obj.read(cursor, uid, section_id, ['code'])['code']
+        if seccio:
+            mostrar_tag = True if seccio == 'ATCF' else False
+        res['value']['mostrar_tag'] = mostrar_tag
+        return res
+
     def onchange_section(self, cursor, uid, ids, section_id):
         res = False
         section_obj = self.pool.get('crm.case.section')
