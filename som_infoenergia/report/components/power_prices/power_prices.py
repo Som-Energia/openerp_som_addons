3f9453a0ae5df2a03a7d6cfc618db453cea9e540
Marite Guerrieri <marite.guerrieri@somenergia.coop>
2022-06-09 09:52:21 +0200

    Modify formula and power prices only for 3XTD and 6XTD

--- som_infoenergia/report/components/power_prices/power_prices.py
+++ som_infoenergia/report/components/power_prices/power_prices.py
@@ -4,28 +4,27 @@ from datetime import datetime
 class power_prices():
 
     def get_data(self, cursor, uid, object, extra_text, context):
-        tariff = 'error'
         polissa = object.polissa_id
         preus = {
             'tariff': '',
-            'preu_p1': '',
-            'preu_p2': '',
-            'preu_p3': '',
-            'preu_p4': '',
-            'preu_p5': '',
-            'preu_p6': '',
+            'preu_p1': 0,
+            'preu_p2': 0,
+            'preu_p3': 0,
+            'preu_p4': 0,
+            'preu_p5': 0,
+            'preu_p6': 0,
         }
-        if polissa:
-            preus['tariff'] = polissa.tarifa_codi
-            ctx = {'date': False}
-            data_final = polissa.modcontractual_activa.data_final or ''
-            if data_final:
-                data_llista_preus = min(datetime.strptime(data_final, '%Y-%m-%d'), datetime.today())
-                ctx['date'] = data_llista_preus
+        if polissa and polissa.tarifa.codi_ocsum in ('020', '021', '022', '023', '025', '019', '024')
+                preus['tariff'] = polissa.tarifa_codi
+                ctx = {'date': False}
+                data_final = polissa.modcontractual_activa.data_final or ''
+                if data_final:
+                    data_llista_preus = min(datetime.strptime(data_final, '%Y-%m-%d'), datetime.today())
+                    ctx['date'] = data_llista_preus
 
-            periodes_potencia = sorted(polissa.tarifa.get_periodes('tp', context=ctx).keys())
+                periodes_potencia = sorted(polissa.tarifa.get_periodes('tp', context=ctx).keys())
 
-            for p in periodes_potencia:
-                preus['preu_{0}'.format(p)] = get_atr_price(cursor, uid, polissa, p, 'tp', ctx, with_taxes=True)[0]
+                for p in periodes_potencia:
+                    preus['preu_{0}'.format(p)] = get_atr_price(cursor, uid, polissa, p, 'tp', ctx, with_taxes=True)[0]
 
         return preus
