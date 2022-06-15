3a24177732150bac2ce23e74d024f1a1ee7fcc10
susu105 <davidsuarez92@gmail.com>
2022-06-15 12:43:02 +0200

    added res conf to dismiss draft invoices

--- giscedata_facturacio_som/__terp__.py
+++ giscedata_facturacio_som/__terp__.py
@@ -13,7 +13,8 @@
     "demo_xml": [],
     "update_xml":[
         "wizard/wizard_fraccionar_via_extralines_view.xml",
-        "wizard/wizard_model_list_from_file_data.xml"
+        "wizard/wizard_model_list_from_file_data.xml",
+        "giscedata_facturacio_data.xml",
     ],
     "active": False,
     "installable": True
