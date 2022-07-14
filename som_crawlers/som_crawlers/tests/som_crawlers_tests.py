f3248ec9ee184a93a0b2a2c25e60f8daa5d01a47
dalila jbilou <u1924054@campus.udg.edu>
2022-07-14 16:20:05 +0200

    canviar contrasenya fet + testos

--- som_crawlers/som_crawlers/tests/som_crawlers_tests.py
+++ som_crawlers/som_crawlers/tests/som_crawlers_tests.py
@@ -1,16 +1,48 @@
 from destral import testing
 from osv import osv
 import unittest
-
+from destral.transaction import Transaction
+from destral.patch import PatchNewCursors
 from datetime import datetime, timedelta
 
-class ConfiguracioTests(unittest.TestCase):
+class ConfiguracioTests(testing.OOTestCase):
 
     def setUp(self):
         self.pool = self.openerp.pool
-        self.Configuracio = self.pool.get('class.configuracio')
+        self.Configuracio = self.pool.get('classe.configuracio')
+        self.Data = self.pool.get('ir.model.data')
     
     def tearDown(self):
         pass
 
-    def test_canviarContrasenya_
+    def test_canviarContrasenya_contrasenya_nova_igual_contrasenya_antiga_resultat_exception(self):
+
+        with Transaction().start(self.database) as txn:
+            cursor = txn.cursor
+            uid = txn.user
+            #get_object_reference per llegir la id de un objecte
+    
+            crawler_config_id = self.Data.get_object_reference(cursor, uid, 'som_crawlers', 'demo_configuracio_1')[1]
+            #set values
+            password = 'Admin'
+            #check result
+            result = self.Configuracio.canviar_contrasenya(cursor,uid,crawler_config_id,password)
+            with self.assertRaises(osv.except_osv) as context:
+                self.Configuracio.canviar_contrasenya(cursor,uid,crawler_config_id,password)
+
+            self.assertTrue('Contrasenya identica a la anterior!','Torna a introduir una contrasenya diferent a la anterior' in context.exception)
+
+    def test_canviarContrasenya_nova_resultat_ok(self):
+
+            with Transaction().start(self.database) as txn:
+                cursor = txn.cursor
+                uid = txn.user
+                #get_object_reference per llegir la id de un objecte
+                crawler_config_id = self.Data.get_object_reference(cursor, uid, 'som_crawlers', 'demo_configuracio_1')[1]
+                #set values
+                password = 'Hola'
+                #try test
+                result = self.Configuracio.canviar_contrasenya(cursor,uid,crawler_config_id,password)
+                #check result
+                self.assertEqual(result, password)
+                #objecte.browse(... + id) per llegir el objecte al complet. 
