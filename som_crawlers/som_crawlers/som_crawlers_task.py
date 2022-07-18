1dcf0c3eed5459666d1bad7a622eff84d35f38c9
dalila jbilou <u1924054@campus.udg.edu>
2022-07-18 15:31:07 +0200

    com.grawlers.result funcionant

--- som_crawlers/som_crawlers/som_crawlers_task.py
+++ som_crawlers/som_crawlers/som_crawlers_task.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
+from datetime import datetime
 from osv import osv, fields
 from tools.translate import _
+import os
+
 class SomCrawlersTask(osv.osv):
     _name = 'som.crawlers.task'
     _columns = {
@@ -9,12 +12,11 @@ class SomCrawlersTask(osv.osv):
         'task_step_ids': fields.one2many('som.crawlers.task.step','task_id', string=_(u"Passos de la tasca")),
         'data_proxima_execucio':fields.datetime(_(u"Data proxima execució"),),
         'configuracio_id': fields.many2one('som.crawlers.config', 'Configuracio', help="Relacio de una configuracio amb la seva tasca",),
-        #'run_ids': fields.one2many('som.crawlers.config','',string="Llistat d'execucions", help="Llista de execucions que ha realitzat la tasca",),
+        'run_ids': fields.one2many('som.crawlers.result','task_id',string="Llistat d'execucions", help="Llista de execucions que ha realitzat la tasca",),
     }
     _defaults = {
         'active': lambda *a:False,
     }
-
 
 SomCrawlersTask()
 
@@ -23,7 +25,6 @@ class SomCrawlersTaskStep(osv.osv):
     _name = 'som.crawlers.task.step'
     _order = 'sequence'
 
-   
     _columns = {
         'name': fields.char(
             _(u"Nom"),
@@ -59,4 +60,30 @@ class SomCrawlersTaskStep(osv.osv):
         'name': lambda *a: 'nom_per_defecte',
     }
 
+
+
 SomCrawlersTaskStep()
+
+class SomCrawlersResult(osv.osv):
+    _name= 'som.crawlers.result'
+
+    _columns={
+
+        'task_id': fields.many2one(
+            'som.crawlers.task',
+            _('Tasca'),
+            help=_('Nom de la tasca'),
+            select=True,
+        ),
+        'data_i_hora_execucio': fields.datetime(
+            _(u"Data i hora de l'execució"),),
+        'resultat': fields.char(
+            _(u"Resultat"),
+            help=_("Resultat de l'execució"),
+            size=128,
+            required=True,)
+
+    }
+
+
+SomCrawlersResult()
