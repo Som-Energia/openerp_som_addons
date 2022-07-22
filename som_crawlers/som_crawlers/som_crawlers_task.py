4b6a23c12c0e6fcdea09d02bc433b88023ee0bbb
dalila jbilou <u1924054@campus.udg.edu>
2022-07-22 12:39:24 +0200

    merge

--- som_crawlers/som_crawlers/som_crawlers_task.py
+++ som_crawlers/som_crawlers/som_crawlers_task.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
+from ast import Param
+import subprocess
 from datetime import datetime
 from osv import osv, fields
 from tools.translate import _
 import os
+import json
 
 class SomCrawlersTask(osv.osv):
     _name = 'som.crawlers.task'
@@ -18,6 +21,53 @@ class SomCrawlersTask(osv.osv):
     _defaults = {
         'active': lambda *a:False,
     }
+
+    def executar_tasques(self, cursor, uid, ids, context=None):
+        #obtenim l'objecte tasca
+        import pudb; pu.db
+
+        if not context:
+            return False
+
+        if not isinstance(ids, (tuple, list)):
+            ids = [ids]
+
+        active_ids = context.get('active_ids',[])
+
+        for id in active_ids:
+            #obtenim una tasca
+            task_obj = self.browse(cursor, uid, id)
+            task_steps_list = task_obj.task_step_ids
+            task_steps_list.sort(key=lambda x: x.sequence)
+            for taskStep in task_steps_list:
+                function = getattr(self, taskStep.function)
+                date = function(cursor,uid,taskStep.id,context)
+                task_obj.write({'ultima_tasca_executada': str(taskStep.name)+ ' - ' + date})
+
+        return {'type': 'ir.actions.act_window_close'}
+
+    def executar_un_fitxer(self, cursor, uid,id, context=None):
+
+        classresult = self.pool.get('som.crawlers.result')
+        classTaskStep = self.pool.get('som.crawlers.task.step')
+        taskStep = classTaskStep.browse(cursor,uid,id)
+        taskStepParams = json.loads(taskStep.params)
+        if taskStepParams.has_key('nom_fitxer'):
+            output =os.system("python3 /home/somenergia/src/openerp_som_addons/som_crawlers/som_crawlers/Downloads/"
+             + taskStepParams['nom_fitxer'])
+            if output == 0:
+                output = 'ok'
+            elif output == 512:
+                output = 'File or directory doesn\'t exist'
+            else :
+                output = 'Error while executing'
+
+        else:
+            output = 'Falta especificar nom fitxer'
+        data_i_hora = datetime.now()
+        classresult.create(cursor,uid,{'task_id': taskStep.task_id.id, 'data_i_hora_execucio': data_i_hora, 'resultat':output})
+        return data_i_hora.strftime("%m/%d/%Y, %H:%M:%S")
+
 
 
 SomCrawlersTask()
--- som_crawlers/som_crawlers/tests/som_crawlers_tests.py
+++ som_crawlers/som_crawlers/tests/som_crawlers_tests.py
@@ -4,6 +4,7 @@ import unittest
 from destral.transaction import Transaction
 from destral.patch import PatchNewCursors
 from datetime import datetime, timedelta
+import wizard
 
 class ConfiguracioTests(testing.OOTestCase):
 
@@ -11,6 +12,10 @@ class ConfiguracioTests(testing.OOTestCase):
         self.pool = self.openerp.pool
         self.Configuracio = self.pool.get('som.crawlers.config')
         self.Data = self.pool.get('ir.model.data')
+        self.task = self.pool.get('som.crawlers.task')
+        self.taskStep = self.pool.get('som.crawlers.task.step')
+        self.result = self.pool.get('som.crawlers.result')
+        self.wiz= self.pool.get('wizard.executar.tasca')
 
     def tearDown(self):
         pass
@@ -45,4 +50,97 @@ class ConfiguracioTests(testing.OOTestCase):
                 result = self.Configuracio.canviar_contrasenya(cursor,uid,crawler_config_id,password)
                 #check result
                 self.assertEqual(result, password)
-                #objecte.browse(... + id) per llegir el objecte al complet. 
+                #objecte.browse(... + id) per llegir el objecte al complet.
+
+
+class WizardExecutarTascaTests(testing.OOTestCase):
+
+    def setUp(self):
+        self.pool = self.openerp.pool
+        self.Configuracio = self.pool.get('som.crawlers.config')
+        self.Data = self.pool.get('ir.model.data')
+        self.task = self.pool.get('som.crawlers.task')
+        self.taskStep = self.pool.get('som.crawlers.task.step')
+        self.result = self.pool.get('som.crawlers.result')
+        self.wiz= self.pool.get('wizard.executar.tasca')
+
+    def tearDown(self):
+        pass
+    
+    def test_executar_un_fitxer_buida_resultat_falta_especificar_nomFItxer(self): #no detecta l'id que toca??
+
+        with Transaction().start(self.database) as txn:
+                import pudb;pu.db
+                cursor = txn.cursor
+                uid = txn.user
+                crawler_task_id= self.Data.get_object_reference(cursor,uid,'som_crawlers','demo_accions_planificades_1')[1]
+                crawler_task_step_id= self.Data.get_object_reference(cursor,uid,'som_crawlers','demo_taskStep_3')[1]
+                ctx = {
+                    'active_id': crawler_task_id, 'active_ids': [crawler_task_id],
+                    'from_model':'som.crawlers.task.step'
+                 }
+                #set values
+                wiz_id = self.wiz.create(cursor, uid,{},context=ctx)
+                #try test
+                result = self.wiz.executar_un_fitxer(cursor, uid,crawler_task_step_id,context=ctx) 
+                #check result
+                self.assertEqual(result, 'Falta especificar nom fitxer')
+                #objecte.browse(... + id) per llegir el objecte al complet.
+
+
+    def test_executar_un_fitxer_entrada_hol_py_resultat_fitxer_invalid(self): #no detecta l'id que toca??
+
+        with Transaction().start(self.database) as txn:
+                import pudb;pu.db
+                cursor = txn.cursor
+                uid = txn.user
+                crawler_task_id= self.Data.get_object_reference(cursor,uid,'som_crawlers','demo_accions_planificades_1')[1]
+                crawler_task_step_id= self.Data.get_object_reference(cursor,uid,'som_crawlers','demo_tasca_1')[1]
+                ctx = {
+                    'active_id': crawler_task_id, 'active_ids': [crawler_task_id],
+                    'from_model':'som.crawlers.task.step'
+                 }
+                #set values
+                wiz_id = self.wiz.create(cursor, uid,{},context=ctx)
+                #try test
+                result = self.wiz.executar_un_fitxer(cursor, uid,crawler_task_step_id,context=ctx) 
+                #check result
+                self.assertEqual(result, 'File or directory doesn\'t exist')
+                #objecte.browse(... + id) per llegir el objecte al complet.
+
+    def test_executar_un_fitxer_hola_py_resultat_ok(self): 
+
+        with Transaction().start(self.database) as txn:
+                import pudb;pu.db
+                cursor = txn.cursor
+                uid = txn.user
+                crawler_task_id= self.Data.get_object_reference(cursor,uid,'som_crawlers','demo_accions_planificades_1')[1]
+                crawler_task_step_id= self.Data.get_object_reference(cursor,uid,'som_crawlers','demo_tasca_2')[1]
+                ctx = {
+                    'active_id': crawler_task_id, 'active_ids': [crawler_task_id],
+                    'from_model':'som.crawlers.task.step'
+                 }
+                #set values
+                wiz_id = self.wiz.create(cursor, uid,{},context=ctx)
+                #try test
+                result = self.wiz.executar_un_fitxer(cursor, uid,crawler_task_step_id,context=ctx) 
+                #check result
+                self.assertEqual(result, 'ok')
+                #objecte.browse(... + id) per llegir el objecte al complet.
+
+    def test_executar_una_tasca(self):
+
+         with Transaction().start(self.database) as txn:
+
+            cursor = txn.cursor
+            uid = txn.user
+            crawler_task_id= self.Data.get_object_reference(cursor,uid,'som_crawlers','demo_accions_planificades_1')[1]
+            ctx = {
+                    'active_id': crawler_task_id, 'active_ids': [crawler_task_id],
+            }
+            #set values
+            wiz_id = self.wiz.create(cursor, uid,{},context=ctx)
+            #try test
+            result = self.wiz.executar_tasca(cursor,uid,[wiz_id],context=ctx)
+            #check result
+            self.assertEqual(result,{'type': 'ir.actions.act_window_close'})
