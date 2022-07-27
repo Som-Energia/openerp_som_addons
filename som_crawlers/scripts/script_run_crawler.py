cd5b3c8eeef8b75d8f00da32154cc170aad121a8
dalila jbilou <u1924054@campus.udg.edu>
2022-07-27 09:20:34 +0200

    Revert "WIP Massive-importer funcionant passant 1 sol crawler des del script de som_crawlers"
    This reverts commit ca23b3b8309017798328b3d7ef04727bccecb9fb.

--- som_crawlers/scripts/script_run_crawler.py
+++ som_crawlers/scripts/script_run_crawler.py
@@ -1,11 +1,6 @@
 
 from massive_importer.crawlers.run_crawlers import WebCrawler
-from massive_importer.crawlers.crawlers.spiders.selenium_spiders import anselmo
-from massive_importer.crawlers.crawlers.spiders.selenium_spiders import PortalConfig
-
 print('Creant wc')
 wc = WebCrawler()
 print('cridant crawl')
-
-spider_instance = anselmo.Anselmo(wc.selenium_crawlers_conf['anselmo'])
-spider_instance.start_with_timeout(debug=True)
+wc.crawl()
