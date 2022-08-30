e58767a76ec079dc1818c3090d75c125717d82bd
dalila jbilou <u1924054@campus.udg.edu>
2022-08-30 09:13:45 +0200

    NEW call to system with str args

--- som_crawlers/som_crawlers_task.py
+++ som_crawlers/som_crawlers_task.py
@@ -186,7 +186,6 @@ class SomCrawlersTaskStep(osv.osv):
                 cfg_obj = self.pool.get('res.config')
                 path_python = cfg_obj.get(cursor, uid, 'som_crawlers_massive_importer_python_path', '~/.virtualenvs/massive/bin/python')
                 fileName = "output_" + config_obj.name + "_" + datetime.now().strftime("%Y-%m-%d_%H_%M") + ".txt"
-                import pudb;pu.db
                 args_str = self.createArgsForScript(cursor, uid, id, config_obj, taskStepParams, fileName)
                 ret_value = os.system("{} {} {}".format(path_python, filePath, args_str))
                 if ret_value != 0:
