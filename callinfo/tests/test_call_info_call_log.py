f2596072de26742b8d914a61cb7aee4686b6fc16
Fran Cubero <francisco.cubero@somenergia.coop>
2022-09-01 15:40:04 +0200

    test code: test inset call

--- callinfo/tests/test_call_info_call_log.py
+++ callinfo/tests/test_call_info_call_log.py
@@ -2,7 +2,7 @@
 from destral import testing
 from destral.transaction import Transaction
 from expects import *
-from datetime import datetime
+from datetime import datetime,date
 
 
 def today_str():
@@ -40,9 +40,6 @@ class CallInfoBaseTests(testing.OOTestCase):
 
 class CallInfoCallLogTest(CallInfoBaseTests):
 
-    def test_normalize_phone_dummy(self):
-        self.assertEqual(1,1)
-
     def test_normalize_phone_ok(self):
         cil_obj = self.get_model('call.info.call.log')
 
@@ -73,3 +70,122 @@ class CallInfoCallLogTest(CallInfoBaseTests):
         for sample in samples:
             result = cil_obj._normalize_phone_number(sample['raw'])
             self.assertEqual(result, sample['clean'])
+
+    def create_dummy_category(self):
+        sec_obj = self.get_model('crm.case.section')
+        cat_obj = self.get_model('crm.case.categ')
+        sec_id = sec_obj.create(self.cursor, self.uid, {'name':'dummy_section','code':'dummy'})
+        cat_id = cat_obj.create(self.cursor, self.uid, {'name':'dummy_section_cat','section_id': sec_id})
+        return sec_id, cat_id
+
+    def test_insert_call_log__basic(self):
+        cil_obj = self.get_model('call.info.call.log')
+        self.create_dummy_category()
+
+        call_data = {
+            'user_id': self.search_in('res.users',[]),
+            'categ_id': self.search_in('crm.case.categ',[]),
+            'notes': 'bla bla bla, complex explanation, ...',
+        }
+
+        call_id = cil_obj.insert_call_log(self.cursor, self.uid, call_data, {})
+        call = cil_obj.browse(self.cursor, self.uid, call_id)
+
+        self.assertEqual(call.comment, call_data['notes'])
+        self.assertEqual(call.call_date[:10], today_str())
+        self.assertEqual(call.user_id.id, call_data['user_id'])
+        self.assertEqual(call.categ_id.id, call_data['categ_id'])
+        self.assertEqual(call.phone, False)
+        self.assertEqual(call.polissa_id, False)
+        self.assertEqual(call.partner_id.id, False)
+
+    def test_insert_call_log__phone(self):
+        cil_obj = self.get_model('call.info.call.log')
+        self.create_dummy_category()
+
+        call_data = {
+            'user_id': self.search_in('res.users',[]),
+            'categ_id': self.search_in('crm.case.categ',[]),
+            'notes': 'bla bla bla, complex explanation, ...',
+            'phone': '0034900103605 som'
+        }
+
+        call_id = cil_obj.insert_call_log(self.cursor, self.uid, call_data, {})
+        call = cil_obj.browse(self.cursor, self.uid, call_id)
+
+        self.assertEqual(call.comment, call_data['notes'])
+        self.assertEqual(call.call_date[:10], today_str())
+        self.assertEqual(call.user_id.id, call_data['user_id'])
+        self.assertEqual(call.categ_id.id, call_data['categ_id'])
+        self.assertEqual(call.phone, '900103605')
+
+    def test_insert_call_log__contract(self):
+        cil_obj = self.get_model('call.info.call.log')
+        ir_obj = self.get_model('ir.model.data')
+        polissa_id = ir_obj.get_object_reference(self.cursor, self.uid, 'giscedata_polissa', 'polissa_0001')[1]
+        self.create_dummy_category()
+
+        call_data = {
+            'user_id': self.search_in('res.users',[]),
+            'categ_id': self.search_in('crm.case.categ',[]),
+            'notes': 'bla bla bla, complex explanation, ...',
+            'polissa_id': polissa_id,
+        }
+
+        call_id = cil_obj.insert_call_log(self.cursor, self.uid, call_data, {})
+        call = cil_obj.browse(self.cursor, self.uid, call_id)
+
+        self.assertEqual(call.comment, call_data['notes'])
+        self.assertEqual(call.call_date[:10], today_str())
+        self.assertEqual(call.user_id.id, call_data['user_id'])
+        self.assertEqual(call.categ_id.id, call_data['categ_id'])
+        self.assertEqual(call.contract_id.id, polissa_id)
+
+    def test_insert_call_log__partner(self):
+        cil_obj = self.get_model('call.info.call.log')
+        ir_obj = self.get_model('ir.model.data')
+        partner_id = ir_obj.get_object_reference(self.cursor, self.uid, 'base', 'main_partner')[1]
+        self.create_dummy_category()
+
+        call_data = {
+            'user_id': self.search_in('res.users',[]),
+            'categ_id': self.search_in('crm.case.categ',[]),
+            'notes': 'bla bla bla, complex explanation, ...',
+            'partner_id': partner_id,
+        }
+
+        call_id = cil_obj.insert_call_log(self.cursor, self.uid, call_data, {})
+        call = cil_obj.browse(self.cursor, self.uid, call_id)
+
+        self.assertEqual(call.comment, call_data['notes'])
+        self.assertEqual(call.call_date[:10], today_str())
+        self.assertEqual(call.user_id.id, call_data['user_id'])
+        self.assertEqual(call.categ_id.id, call_data['categ_id'])
+        self.assertEqual(call.partner_id.id, partner_id)
+
+    def test_insert_call_log__all_identifiers(self):
+        cil_obj = self.get_model('call.info.call.log')
+        ir_obj = self.get_model('ir.model.data')
+        polissa_id = ir_obj.get_object_reference(self.cursor, self.uid, 'giscedata_polissa', 'polissa_0001')[1]
+        partner_id = ir_obj.get_object_reference(self.cursor, self.uid, 'base', 'main_partner')[1]
+        self.create_dummy_category()
+
+        call_data = {
+            'user_id': self.search_in('res.users',[]),
+            'categ_id': self.search_in('crm.case.categ',[]),
+            'notes': 'bla bla bla, complex explanation, ...',
+            'partner_id': partner_id,
+            'polissa_id': polissa_id,
+            'phone': '(0034)900.103.605 som'
+        }
+
+        call_id = cil_obj.insert_call_log(self.cursor, self.uid, call_data, {})
+        call = cil_obj.browse(self.cursor, self.uid, call_id)
+
+        self.assertEqual(call.comment, call_data['notes'])
+        self.assertEqual(call.call_date[:10], today_str())
+        self.assertEqual(call.user_id.id, call_data['user_id'])
+        self.assertEqual(call.categ_id.id, call_data['categ_id'])
+        self.assertEqual(call.partner_id.id, partner_id)
+        self.assertEqual(call.phone, '900103605')
+        self.assertEqual(call.contract_id.id, polissa_id)
