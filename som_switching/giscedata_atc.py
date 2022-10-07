89b61354e347cac22ec2a3430c34c8dd53dd2fa5
Fran Cubero <francisco.cubero@somenergia.coop>
2022-10-07 10:32:18 +0200

    add case cancel exceptions

--- som_switching/giscedata_atc.py
+++ som_switching/giscedata_atc.py
@@ -43,7 +43,79 @@ class GiscedataAtc(osv.osv):
         return res
 
     def unlink(self, cursor, uid, ids, context=None):
-        super(GiscedataAtc, self).case_cancel(cursor, uid, ids, context)
+        return self.case_cancel(cursor, uid, ids, context)
+
+    def case_cancel(self, cursor, uid, ids, *args):
+        if not isinstance(ids, (list, tuple)):
+            ids = [ids]
+
+        cancel_ids = []
+        warning_info = []
+        for atc_id in ids:
+            atc = self.browse(cursor, uid, atc_id)
+            r1 = atc.ref
+            if r1:
+                model, index = r1.split(',')
+                m_obj = self.pool.get(model)
+                r1 = m_obj.browse(cursor, uid, int(index))
+
+            if atc.state == 'open' and atc.process_step == '01' and r1 and r1.enviament_pendent == False:
+                raise osv.except_osv(
+                        _(u"Warning"),
+                        _(u"Cas ATC {} no es pot cancel·lar: R1 01 enviat a distribuïdora, per cancel·lar cal anul·lar amb un 08 i esperar el 09").format(atc_id))
+
+            if atc.state == 'open'and atc.process_step == '02':
+                r1_finalitzat = self.has_r1_no_finalitzat(cursor, uid, atc_id)
+                if r1 and not r1_finalitzat:
+                    raise osv.except_osv(
+                            _(u"Warning"),
+                            _(u"Cas ATC {} no es pot cancel·lar: R1 02 cal esperar passos finalitzadors (per exemple 05) ").format(atc_id))
+
+                if r1 and r1_finalitzat:
+                    raise osv.except_osv(
+                            _(u"Warning"),
+                            _(u"Cas ATC {} no es pot cancel·lar: R1 02 amb passos finalitzats").format(atc_id))
+
+            if atc.state == 'open' and atc.process_step == '04':
+                raise osv.except_osv(
+                        _(u"Warning"),
+                        _(u"Cas ATC {} no es pot cancel·lar: R1 04 en estat Obert - ERROR MANUAL -").format(atc_id))
+
+            if atc.state == 'pending' and atc.process_step == '04':
+                raise osv.except_osv(
+                        _(u"Warning"),
+                        _(u"Cas ATC {} no es pot cancel·lar: R1 04 en estat Pendent").format(atc_id))
+
+            if atc.state == 'open' and atc.process_step == '05':
+                if r1 and r1.state != 'open':
+                    raise osv.except_osv(
+                            _(u"Warning"),
+                            _(u"Cas ATC {} no es pot cancel·lar: R1 05 en estat Pendent - ERROR MANUAL").format(atc_id))
+                else:
+                    raise osv.except_osv(
+                            _(u"Warning"),
+                            _(u"Cas ATC {} no es pot cancel·lar: R1 05 en estat obert s'ha de gestionar i tancar sefons tancament indicat (procedent/improcedent)").format(atc_id))
+
+            if atc.state == 'open' and atc.process_step == '08':
+                raise osv.except_osv(
+                        _(u"Warning"),
+                        _(u"Cas ATC {} no es pot cancel·lar: R1 08 no pots cancel·lar una cancel·lació, cal esperar a rebre pas 09 de distribuïdora").format(atc_id))
+
+            if atc.state == 'open' and atc.process_step == '09':
+                if r1 and r1.rebuig:
+                    raise osv.except_osv(
+                            _(u"Warning"),
+                            _(u"Cas ATC {} no es pot cancel·lar: R1 09 de rebuig no es pot cancel·lar ni tancar").format(atc_id))
+                else:
+                    raise osv.except_osv(
+                            _(u"Warning"),
+                            _(u"Cas ATC {} no es pot cancel·lar: R1 09 d'acceptació s'ha de tancar i no cancel·lar").format(atc_id))
+
+            cancel_ids.append(atc_id)
+
+        if cancel_ids:
+            return super(GiscedataAtc, self).case_cancel(self, cursor, uid, cancel_ids, *args)
+        return True
 
     _columns = {
         'tag': fields.many2one('giscedata.atc.tag', "Etiqueta"),
