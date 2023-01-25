b2fe6ed21f64f666f3797f185d51107d4228e751
Fran Cubero <francisco.cubero@somenergia.coop>
2023-01-25 15:38:44 +0100

    show the higher end date for each bloc in energy matrix

--- giscedata_facturacio_comer_som/giscedata_facturacio_report.py
+++ giscedata_facturacio_comer_som/giscedata_facturacio_report.py
@@ -1962,7 +1962,8 @@ class GiscedataFacturacioFacturaReport(osv.osv):
             lines_data[block]['data'] = data_desde
             lines_data[block]['days'] = (datetime.strptime(l.data_fins,'%Y-%m-%d') - datetime.strptime(l.data_desde,'%Y-%m-%d')).days + 1
             lines_data[block]['date_from'] = dateformat(l.data_desde)
-            lines_data[block]['date_to'] = dateformat(l.data_fins)
+            lines_data[block]['date_to_d'] = val(l.data_fins) if 'date_to_d' not in lines_data[block] or lines_data[block]['date_to_d'] < val(l.data_fins) else lines_data[block]['date_to_d']
+            lines_data[block]['date_to'] = dateformat(lines_data[block]['date_to_d'])
 
         lines_data = [lines_data[k] for k in sorted(lines_data.keys())]
         return lines_data
