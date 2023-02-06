6bfb772046850740f4b07dd6b231569521e2c7b7
Xavier Terés <xavier.teres@somenergia.coop>
2023-02-06 16:09:54 +0100

    Cancel related R101 when canceling CAC if it has not been sent

--- som_switching/giscedata_atc.py
+++ som_switching/giscedata_atc.py
@@ -50,6 +50,7 @@ class GiscedataAtc(osv.osv):
             ids = [ids]
 
         cancel_ids = []
+        cancel_r1_ids = []
         for atc_id in ids:
             atc = self.browse(cursor, uid, atc_id)
 
@@ -128,9 +129,15 @@ class GiscedataAtc(osv.osv):
                         _(u"Cas ATC {} no es pot cancel·lar: L'estat no és Pendent, Esborrany o Obert").format(atc_id))
 
             cancel_ids.append(atc_id)
+            if atc.process_step == '01' and r1 and r1.enviament_pendent == True:
+                cancel_r1_ids.append(r1.id)
 
         if cancel_ids:
+            if cancel_r1_ids:
+                r101_obj = self.pool.get("giscedata.switching")
+                r101_obj.case_cancel(cursor, uid, cancel_r1_ids, *args)
             return super(GiscedataAtc, self).case_cancel(cursor, uid, cancel_ids, *args)
+
         return True
 
     _columns = {
