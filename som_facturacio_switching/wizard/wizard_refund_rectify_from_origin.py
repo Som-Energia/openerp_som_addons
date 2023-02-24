dde4b9d5421df6187747ed346b66f890f945e1dc
Fran Cubero <francisco.cubero@somenergia.coop>
2023-02-24 11:22:39 +0100

    do not refund abonadores

--- som_facturacio_switching/wizard/wizard_refund_rectify_from_origin.py
+++ som_facturacio_switching/wizard/wizard_refund_rectify_from_origin.py
@@ -56,6 +56,7 @@ class WizardRefundRectifyFromOrigin(osv.osv_memory):
             ('polissa_id', '=', pol_id),
             ('type', 'in', ['out_invoice', 'out_refund']),
             ('refund_by_id', '=', False),
+            ('rectificative_type', '!=', 'B'),
             ('data_inici', '<', data_final),
             ('data_final', '>', data_inici)
             ], order='data_inici asc')
