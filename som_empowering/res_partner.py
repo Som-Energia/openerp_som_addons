5978c61a91e23bf34567da85787f27f5791c1f6c
David García Garzón <david.garcia@somenergia.coop>
2023-03-01 16:52:03 +0100

    related_contracts, first tests of generate_token

--- som_empowering/res_partner.py
+++ som_empowering/res_partner.py
@@ -12,10 +12,28 @@ class ResPartner(osv.osv):
     _name = 'res.partner'
     _inherit = 'res.partner'
 
+
+    def related_contracts(self, cursor, uid, id, relations, context=None):
+        relation_map=dict(
+            notifica='direccio_notificacio.partner_id',
+        )
+        polissa_obj = self.pool.get('giscedata.polissa')
+        domain = (['|'] if len(relations)>1 else []) + [
+            (relation_map.get(relation,relation), '=', id)
+            for relation in relations
+        ]
+        return polissa_obj.search(cursor, uid, domain)
+
     def assign_token(self, cursor, uid, ids, context=None):
         """Assign a new token to the partner
         """
-        polissa_obj = self.pool.get('giscedata.polissa')
+        for id in ids:
+            token = generate_token()
+            self.write(cursor, uid, id, {
+                'empowering_token': token
+            })
+        return True
+
         m = mdbpool.get_db()
         for partner in self.read(cursor, uid, ids, ['empowering_token']):
             m.tokens.remove({'token': partner['empowering_token']})
---
+++ som_empowering/tests/test_res_partner.py
@@ -0,0 +1,190 @@
+from destral import testing
+import unittest
+from mongodb_backend.mongodb2 import mdbpool
+import mock
+import pymongo
+from destral.transaction import Transaction
+from yamlns import ns
+
+class ResPartnerTest(testing.OOTestCase):
+
+
+    def get_reference(self, semantic_id):
+        return self.IrModelData.get_object_reference(
+            self.cursor, self.uid, 
+            *semantic_id.split('.')
+        )[1]
+
+
+    from yamlns.testutils import assertNsEqual
+
+    def setUp(self):
+        self.maxDiff = None
+        self.databasename = 'som_empowering_test'
+        self.collection = 'tokens'
+        c = pymongo.MongoClient()
+        c.drop_database(self.databasename)
+        self.db = c[self.databasename]
+
+        patcher = mock.patch('mongodb_backend.mongodb2.mdbpool.get_db')
+        self.get_db_mock = patcher.start()
+        self.get_db_mock.return_value = self.db
+        self.addCleanup(patcher.stop)
+
+        self.txn = Transaction().start(self.database)
+        self.cursor = self.txn.cursor
+        self.uid = self.txn.user
+        self.addCleanup(self.txn.stop)
+        self.pool = self.openerp.pool
+        self.IrModelData = self.pool.get('ir.model.data')
+        self.ResPartner = self.pool.get('res.partner')
+        self.ResPartnerAddress = self.pool.get('res.partner.address')
+        self.GiscedataPolissa = self.pool.get('giscedata.polissa')
+
+        self.contract1 = self.get_reference('giscedata_polissa.polissa_gisce')
+        self.contract2 = self.get_reference('giscedata_polissa.polissa_0001')
+        # owner of contract1, no other roles on contract 1, unrelated to contract2
+        self.owner1 = self.get_reference('base.res_partner_gisce')
+        self.notified1 =  self.get_reference('base.res_partner_9')
+        # payer of contract2, no other roles on contract 2, unrelated to contract1
+        self.owner2 = self.get_reference('base.res_partner_c2c')
+        self.payer2 = self.get_reference('base.res_partner_agrolait')
+        # giscedata_polissa.polissa_0001
+
+    def tearDown(self):
+        c = pymongo.MongoClient()
+        c.drop_database(self.databasename)
+
+    def related_contracts(self, partner_id, relations):
+        return self.ResPartner.related_contracts(self.cursor, self.uid, partner_id, relations)
+
+    def set_role(self, contract1, partner_id, role):
+        self.GiscedataPolissa.write(self.cursor, self.uid, contract1, {
+            role: partner_id,
+        })
+
+    def assertRelated(self, contract1, partner_id, *relations):
+        # Comparing the full list of related contracts would be fragile
+        self.assertIn(contract1, self.related_contracts(partner_id, relations))
+
+    def assertUnrelated(self, contract1, partner_id, *relations):
+        # Comparing the full list of related contracts would be fragile
+        self.assertNotIn(contract1, self.related_contracts(partner_id, relations))
+
+    def getRoles(self, contract):
+        def getid(foreignkey):
+            if not foreignkey: return None
+            return foreignkey[0]
+
+        polissa = self.GiscedataPolissa.read(self.cursor, self.uid, contract)
+        result = ns(
+            owner=getid(polissa['titular']),
+            payer=getid(polissa['pagador']),
+            #admin=getid(polissa['administradora']),
+            notified=None
+        )
+        notify_address = getid(polissa['direccio_notificacio'])
+
+        if notify_address:
+            address = self.ResPartnerAddress.read(
+                self.cursor, self.uid, notify_address)
+            result.notified=getid(address['partner_id'])
+        return result
+
+    def test_fixtures(self):
+        "This tests checks assumptions the other tests do on the fixtures"
+        self.assertNsEqual(
+            ns(
+                contract1 = self.getRoles(self.contract1),
+                contract2 = self.getRoles(self.contract2),
+            ),
+            ns(
+                contract1 = ns(
+                    owner = self.owner1,
+                    payer = self.owner1,
+                    notified = self.notified1,
+                ),
+                contract2 = ns(
+                    owner = self.owner2,
+                    payer = self.payer2,
+                    notified = self.payer2,
+                ),
+            )
+        )
+
+    def test_related_contracts__titular(self):
+        # Contract is just in the related of the owner
+        self.assertRelated(self.contract1, self.owner1, 'titular')
+        self.assertUnrelated(self.contract1, self.owner2, 'titular')
+        self.assertRelated(self.contract2, self.owner2, 'titular')
+        self.assertUnrelated(self.contract2, self.payer2, 'titular')
+
+    def test_related_contracts__pagador(self):
+        # owner2 is not payer of contract 2
+        self.assertUnrelated(self.contract2, self.owner2, 'pagador')
+        # payer2 is
+        self.assertRelated(self.contract2, self.payer2, 'pagador')
+        # but if we set owner2 as the payer of contract2
+        self.set_role(self.contract2, self.owner2, 'pagador')
+        # The situation is inverted
+        self.assertRelated(self.contract2, self.owner2, 'pagador')
+        self.assertUnrelated(self.contract2, self.payer2, 'pagador')
+
+    def test_related_contracts__notifica(self):
+        # payer2 is both payer but also notifier of contract2
+        self.assertRelated(self.contract2, self.payer2, 'notifica')
+        # but has no relation with contract1
+        self.assertUnrelated(self.contract1, self.payer2, 'notifica')
+        # owner1 is not notified of contrac1
+        self.assertUnrelated(self.contract1, self.owner1, 'notifica')
+        # notified1 is
+        self.assertRelated(self.contract1, self.notified1, 'notifica')
+
+    @unittest.skip("Missing dependency")
+    def test_related_contracts__administrador(self):
+        self.assertUnrelated(self.contract1, self.payer2, 'administradora')
+        self.set_role(self.contract1, self.payer2, 'administradora')
+        self.assertRelated(self.contract1, self.payer2, 'administradora')
+
+
+    def get_token(self, partner):
+        return self.ResPartner.read(self.cursor, self.uid, partner, ['empowering_token'])['empowering_token']
+
+    def token_info(self, token):
+        return [
+            ns(x,_id=str(x['_id']))
+            for x in self.db.tokens.find({'token': token})
+        ]
+
+    def test_assign_token__beforeAssigningOne(self):
+        token = self.get_token(self.owner1)
+        self.assertEqual(token, False)
+
+    def test_assign_token__afterAssignment(self):
+        self.ResPartner.assign_token(self.cursor, self.uid, [self.owner1])
+        token = self.get_token(self.owner1)
+        self.assertTrue(token)
+
+    def test_assign_token__simultaneous_assign_different_tokens(self):
+        self.ResPartner.assign_token(self.cursor, self.uid, [self.owner1, self.owner2])
+        token1 = self.get_token(self.owner1)
+        token2 = self.get_token(self.owner2)
+        self.assertNotEqual(token1, token2)
+
+    def _test_assign_token__twice_keepsSame(self):
+        self.ResPartner.assign_token(self.cursor, self.uid, [self.owner1])
+        token = self.get_token(self.owner1)
+        self.ResPartner.assign_token(self.cursor, self.uid, [self.owner1])
+        new_token = self.get_token(self.owner1)
+        self.assertEqual(new_token, token)
+
+    def _test_assign_token__setsContractList(self):
+        self.assertNsEqual(self.token_info(token), """
+            hola: tu
+        """)
+
+
+
+
+
+
