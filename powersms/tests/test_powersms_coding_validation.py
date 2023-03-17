dc7ae0b148b5256cd9faed69eb9a3e54249d5cfa
PSala <polsalalidon@gmail.com>
2023-03-17 11:23:52 +0100

    Test migration scripts

--- powersms/tests/test_powersms_coding_validation.py
+++ powersms/tests/test_powersms_coding_validation.py
@@ -29,4 +29,39 @@ class TestProviders(testing.OOTestCase):
                     provider_obj.search(cursor, uid, [('function_pattern_code', '=', _method.replace('send_sms_', ''))])
                 )
 
-
+    def test_migration_post_0001_set_lleidanet_provider_for_existing_acc(self):
+        import six
+        if six.PY2:
+            import imp
+            from addons import get_module_resource
+            script = get_module_resource(
+                'powersms', 'migrations', '5.0.23.1.0',
+                'post-0001-set_lleidanet_provider_for_existing_acc.py'
+            )
+            mod = imp.load_source('powersms', script)
+            with Transaction().start(self.database) as txn:
+                cursor, uid, pool = txn.cursor, txn.user, txn.pool
+                imd_obj = pool.get('ir.model.data')
+                acc_obj = pool.get('powersms.core_accounts')
+                cursor.execute(
+                    "ALTER TABLE powersms_core_accounts DROP COLUMN provider_id"
+                )
+                cursor.execute(
+                    "DROP TABLE powersms_provider"
+                )
+                mod.up(cursor, True)
+                provider_obj = pool.get('powersms.provider')
+                providers = provider_obj.search(
+                    cursor, uid, [], context={'active_test': False}
+                )
+                self.assertTrue(providers)
+                for p_id in providers:
+                    self.assertTrue(hasattr(provider_obj, provider_obj._get_provider_function(cursor, uid, p_id)))
+                account_id = imd_obj.get_object_reference(cursor, uid, 'powersms', 'sms_account_som')[1]
+                lleidanet_prov_id = imd_obj.get_object_reference(
+                    cursor, uid, 'powersms', 'powersms_provider_lleidanet'
+                )[1]
+                provider_on_account = acc_obj.read(cursor, uid, account_id, ['provider_id'])['provider_id']
+                self.assertTrue(provider_on_account)
+                provider_on_account = provider_on_account[0]
+                self.assertEqual(provider_on_account, lleidanet_prov_id)
