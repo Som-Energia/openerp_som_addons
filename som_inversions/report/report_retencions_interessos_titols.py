b8959656a6f31ec454cb5e4418d338ddfd9481ab
Xavier Terés <xavier.teres@somenergia.coop>
2023-04-27 14:35:31 +0200

    Add AEAT 193 record browse

--- som_inversions/report/report_retencions_interessos_titols.py
+++ som_inversions/report/report_retencions_interessos_titols.py
@@ -3,29 +3,40 @@ from yamlns import namespace as ns
 import pooler
 import datetime
 
+
 class ResPartner(osv.osv):
     _name = 'res.partner'
     _inherit = 'res.partner'
 
     def report_retencions_data(self, cursor, uid, ids):
+        aeat193_record_obj = self.pool.get('l10n.es.aeat.mod193.record')
         partner = self.browse(cursor, uid, ids[0])
         data = ns()
         data.year = str(datetime.date.today().year - 1)
-        import pudb;pu.db
-        aeat193_record_obj = self.pool.get('l10n.es.aeat.mod193.record')
+        import pudb; pu.db
+
         search_params = [
             ('partner_id', '=', ids[0]),
             ('report_id.company_name', '=', 'TITOLS'),
             ('fiscal_year_id.name', '=', data.year),
         ]
-        aeat193_record = aeat193_record_obj.search(
-            cursor, uid, search_params, limit=1)[0]
+        aeat193_record_ids = aeat193_record_obj.search(
+            cursor,
+            uid,
+            search_params,
+            limit=1,
+        )
+        aeat193_record = aeat193_record_obj.browse(
+            cursor,
+            uid,
+            aeat193_record_ids,
+        )
         data.lang = partner.lang
         data.somenergia = get_somenergia_partner_info(cursor, uid)
         data.partner_name = partner.name
         data.partner_vat = partner.vat[2:]
-        data.amount_untaxed = aeat193_record.amount_untaxed
-        data.amount_tax = abs(aeat193_record.amount_tax)
+        data.amount_untaxed = aeat193_record[0].amount_untaxed if aeat193_record else 0
+        data.amount_tax = abs(aeat193_record[0].amount_tax) if aeat193_record else 0
         data.date_last_date_previous_year = '{}-12-31'.format(data.year)
         data.balance = 0
 
