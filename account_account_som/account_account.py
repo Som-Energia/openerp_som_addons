0f6cfb04d69a51e57d0a6d87f9172e4780212d11
pauboixsom <pau.boix@somenergia.coop>
2023-05-24 11:41:34 +0200

    Add black formatting

--- account_account_som/account_account.py
+++ account_account_som/account_account.py
@@ -5,15 +5,14 @@ from osv import fields, osv
 
 class AccountAccount(osv.osv):
     """Modifiquem account_account per poder mostrat-lo o no al llistat a voluntat"""
-    _name = 'account.account'
-    _inherit = 'account.account'
 
-    _columns = {
-        'ocultar': fields.boolean('Ocultar compte')
-    }
+    _name = "account.account"
+    _inherit = "account.account"
+
+    _columns = {"ocultar": fields.boolean("Ocultar compte")}
 
     _defaults = {
-        'ocultar': lambda *a: False,
+        "ocultar": lambda *a: False,
     }
 
 
