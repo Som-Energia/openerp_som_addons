eb720881ebaa5927dde260d70fe9b1705769235c
Alex <acifuentes@gisce.net>
2023-05-24 14:52:07 +0200

    custom validacions som

--- som_facturacio_comer/wizard/wizard_afegir_contracte_bateria_virtual.py
+++ som_facturacio_comer/wizard/wizard_afegir_contracte_bateria_virtual.py
@@ -4,6 +4,11 @@ from osv import fields, osv
 from tools.translate import _
 from datetime import datetime
 
+STATES_GESTIO_DESCOMPTES = [
+    ('no_aplicar', 'No aplicar descomptes'),
+    ('aplicar', 'Aplicar descomptes disponibles')
+]
+
 class WizardAfegirContracteBateriaVirtual(osv.osv_memory):
     """Wizard per afegir contractes a un bateria virtual
     """
@@ -11,10 +16,52 @@ class WizardAfegirContracteBateriaVirtual(osv.osv_memory):
     _inherit = 'wizard.afegir.contracte.bateria.virtual'
 
     def action_assignar_bateria_virtual(self, cursor, uid, ids, context=None):
-        # validar origens
-        
-        # cridar al super
+        """
+        Acció per assignar el contracte a la bateria virutal
+        Es valida que una bateria virtual no pugui tenir més d'un origen
+        """
+        if context is None:
+            context = {}
 
+        bat_obj = self.pool.get('giscedata.bateria.virtual')
+        polissa_ids = context['active_ids']
+
+        self_fields = [
+            'bateria_id', 'es_receptor_descomptes',
+            'es_origen_descomptes', 'data_inici', 'pes',
+            'gestio_descomptes', 'crear_bateria_automaticament'
+        ]
+        self_vals = self.read(
+            cursor, uid, ids, self_fields, context=context
+        )[0]
+
+        # validacions
+        if self_vals['bateria_id'] and self_vals['es_origen_descomptes']:
+            # Intentem afegir més d'una polissa com a origen a una bateria virtual
+            if len(polissa_ids) > 1:
+                raise osv.except_osv(
+                    _("Error"),
+                    _("Estas intentant afegir més d'una pólissa a la bateria virtual com a origen")
+                )
+
+            polissa_descompte_ids = bat_obj.read(cursor, uid, self_vals['bateria_id'],
+                                                 ['polissa_descompte_ids'])['polissa_descompte_ids']
+            # Intentem afegir una polissa com a origen a una bateria virtual que ja te origen
+            if polissa_descompte_ids:
+                raise osv.except_osv(
+                    _("Error"),
+                    _("La bateria virtual seleccionada ja te origen.")
+                )
+
+        return super(WizardAfegirContracteBateriaVirtual, self).action_assignar_bateria_virtual(
+            cursor, uid, ids, context=context
+        )
+
+    _columns = {
+        'gestio_descomptes': fields.selection(
+            STATES_GESTIO_DESCOMPTES, 'Gestió dels descomptes'
+        ),
+    }
 
 
 WizardAfegirContracteBateriaVirtual()
