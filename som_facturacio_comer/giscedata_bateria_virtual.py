a8dc3f0e059799c85f86f13fd9ec22abd333c3c5
Alex <acifuentes@gisce.net>
2023-05-31 16:50:31 +0200

    correccions descomptes

--- som_facturacio_comer/giscedata_bateria_virtual.py
+++ som_facturacio_comer/giscedata_bateria_virtual.py
@@ -29,27 +29,30 @@ class GiscedataBateriaVirtualOrigen(osv.osv):
             else:
                 # aplicar el percentatge corresponent segons data, sobre el descompte total
                 for descompte_total in descomptes_totals:
-                    descompte_percentatge = self.get_descompte_amb_percentatge_acumulacio(cursor, uid, descompte_total)
+                    descompte_percentatge = self.get_descompte_amb_percentatge_acumulacio(cursor, uid, id, descompte_total)
                     descomptes.append(descompte_percentatge)
                 return descomptes
 
-    def get_descompte_amb_percentatge_acumulacio(self, cursor, uid, descompte_total):
+    def get_descompte_amb_percentatge_acumulacio(self, cursor, uid, id, descompte_total):
         # [descompte_date, price, 'giscedata.facturacio.factura, factura.id']
         # [05-05-2023, 25.3, 'giscedata.facturacio.factura, 3642']
         percentatge_acum_obj = self.pool.get('giscedata.bateria.virtual.percentatges.acumulacio')
 
         descompte_data = descompte_total[0]
         descompte_preu = descompte_total[1]
+
+        # todo: crec que es fa en una pr que no esta a testing
         descompte_ref = descompte_total[2]
 
-        percetatge_acum_id = self.get_percentatge_acumulacio_from_date(cursor, uid, descompte_data)
+        percetatge_acum_id = self.get_percentatge_acumulacio_from_date(cursor, uid, id, descompte_data)
         percentatge = percentatge_acum_obj.read(cursor, uid, percetatge_acum_id[0], ['percentatge'])['percentatge']
         amount = descompte_preu * (percentatge/100)
         return (descompte_data, amount, descompte_ref)
 
-    def get_percentatge_acumulacio_from_date(self, cursor, uid, descompte_date):
+    def get_percentatge_acumulacio_from_date(self, cursor, uid, id, descompte_date):
         percentatge_acum_obj = self.pool.get('giscedata.bateria.virtual.percentatges.acumulacio')
         percetatge_acum_id = percentatge_acum_obj.search(cursor, uid, [
+            ('origen_id', '=', id),
             ('data_inici', '<=', descompte_date),
             ('data_fi', '>=', descompte_date),
         ])
