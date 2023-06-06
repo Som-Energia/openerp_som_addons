c686a716063123bb99dd648c5bd04266a9d5607f
Alex <acifuentes@gisce.net>
2023-06-06 10:15:53 +0200

    modul som_facturacio_flux_solar

---
+++ som_facturacio_flux_solar/__init__.py
@@ -0,0 +1,1 @@
+import giscedata_bateria_virtual
---
+++ som_facturacio_flux_solar/__terp__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Funcions de suport a bateria virtual comer per SOM",
+    "description": """
+    This module provide :
+        * Validació propia a giscedata_facturacio_bateria_virtual
+    """,
+    "version": "0-dev",
+    "author": "GISCE",
+    "category": "SomEnergia",
+    "depends":[
+        "giscedata_facturacio_bateria_virtual",
+    ],
+    "init_xml": [],
+    "demo_xml": [],
+    "update_xml": [
+        "giscedata_bateria_virtual.xml",
+    ],
+    "active": False,
+    "installable": True
+}
