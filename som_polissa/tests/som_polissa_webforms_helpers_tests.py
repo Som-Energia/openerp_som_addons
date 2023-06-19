c8d4f08d6cd6b7e1b2e238b50327aa0e6a3e211a
destanyol <daniel.estanyol@somenergia.coop>
2023-06-19 15:49:54 +0200

    FIX: Tests and add demo data

--- som_polissa/tests/som_polissa_webforms_helpers_tests.py
+++ som_polissa/tests/som_polissa_webforms_helpers_tests.py
@@ -35,7 +35,7 @@ class TestSomWebformsHelpersPolissa(testing.OOTestCase):
     def test_www_get_iban(self):
         polissa_id = self._open_polissa('polissa_domestica_0100')
         result = self.helper_obj.www_get_iban(self.cursor, self.uid, polissa_id)
-        self.assertEqual(result, '**** **** **** **** **** 7890')
+        self.assertEqual(result, '**** **** **** **** **** 9397')
 
     def test_www_check_iban(self):
         correct_iban = 'ES3120170806126133002095'
@@ -47,7 +47,7 @@ class TestSomWebformsHelpersPolissa(testing.OOTestCase):
 
     def test_www_set_iban(self):
         polissa_id = self._open_polissa('polissa_domestica_0100')
-        new_iban = 'ES3120170806126133002095'
+        new_iban = 'ES31 2017 0806 1261 3300 2095'
         result = self.helper_obj.www_set_iban(
             self.cursor, self.uid, polissa_id, new_iban
         )
