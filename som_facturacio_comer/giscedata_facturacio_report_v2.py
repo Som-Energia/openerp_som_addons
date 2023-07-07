60e1d79bb3f476b20a8754e77821d6fd57f5a1f6
destanyol <daniel.estanyol@somenergia.coop>
2023-07-07 11:50:19 +0200

    FIX: Invoice CNMC QR code GkWh parameters
    Co-authored-by: Francisco Cubero <francisco.cubero@somenergia.coop>

---
+++ som_facturacio_comer/giscedata_facturacio_report_v2.py
@@ -0,0 +1,16 @@
+from osv import osv
+
+class GiscedataFacturacioFacturaReportV2(osv.osv):
+    _inherit = 'giscedata.facturacio.factura.report.v2'
+
+    def get_parametres_energia(self, data):
+        parametres_energia = super(GiscedataFacturacioFacturaReportV2, self).get_parametres_energia(
+            cursor, uid, ids, default, context
+        )
+        parametres_energia_sense_gwkh = []
+        for parametre in parametres_energia:
+            if parametre[0] != 'prEh':
+                parametres_energia_sense_gwkh.append(parametre)
+        return parametres_energia_sense_gwkh
+
+GiscedataFacturacioFacturaReportV2()
