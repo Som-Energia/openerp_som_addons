939163abcb760a1274951e8ae5f57568d08fa117
destanyol <daniel.estanyol@somenergia.coop>
2023-07-07 13:40:22 +0200

    FIX: Parameters of copy pasta

--- som_facturacio_comer/giscedata_facturacio_report_v2.py
+++ som_facturacio_comer/giscedata_facturacio_report_v2.py
@@ -4,9 +4,7 @@ class GiscedataFacturacioFacturaReportV2(osv.osv):
     _inherit = 'giscedata.facturacio.factura.report.v2'
 
     def get_parametres_energia(self, data):
-        parametres_energia = super(GiscedataFacturacioFacturaReportV2, self).get_parametres_energia(
-            cursor, uid, ids, default, context
-        )
+        parametres_energia = super(GiscedataFacturacioFacturaReportV2, self).get_parametres_energia(data)
         parametres_energia_sense_gwkh = []
         for parametre in parametres_energia:
             if parametre[0] != 'prEh':
