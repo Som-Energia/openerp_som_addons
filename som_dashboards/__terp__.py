16ac985b66be08bafddf2df128682cb4e3c1fe5a
destanyol <daniel.estanyol@somenergia.coop>
2023-08-04 15:32:00 +0200

    INITIAL: Som dashboard implemenation
    Co-authored-by: Pau Boix <pau.boix@somenergia.coop>
    Co-authored-by: Xavier Terés <xavier.teres@somenergia.coop>

---
+++ som_dashboards/__terp__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Dashboards personalitzats de SOM",
+    "description": """
+    """,
+    "version": "0-dev",
+    "author": "SOM Energia - ERP Team",
+    "category": "Dashboards",
+    "depends":[
+        "giscedata_polissa"
+    ],
+    "init_xml": [],
+    "demo_xml": [],
+    "update_xml":[
+        "som_dashboards_contractes.xml",
+    ],
+    "active": False,
+    "installable": True
+}
