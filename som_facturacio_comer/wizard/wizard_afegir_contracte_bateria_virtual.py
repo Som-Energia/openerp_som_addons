ce987bd35c502e58d805413bbf5d4a66adba4469
Emili García <egarcia@gisce.net>
2023-08-10 16:02:45 +0200

    Changes on origin retrieving

--- som_facturacio_comer/wizard/wizard_afegir_contracte_bateria_virtual.py
+++ som_facturacio_comer/wizard/wizard_afegir_contracte_bateria_virtual.py
@@ -24,6 +24,8 @@ class WizardAfegirContracteBateriaVirtual(osv.osv_memory):
             context = {}
 
         bat_obj = self.pool.get('giscedata.bateria.virtual')
+        origen_obj = self.pool.get('giscedata.bateria.virtual.origen')
+        pol_obj = self.pool.get('giscedata.polissa')
         polissa_ids = context['active_ids']
 
         self_fields = [
@@ -47,11 +49,28 @@ class WizardAfegirContracteBateriaVirtual(osv.osv_memory):
             origen_ids = bat_obj.read(cursor, uid, self_vals['bateria_id'],
                                                  ['origen_ids'])['origen_ids']
             # Intentem afegir una polissa com a origen a una bateria virtual que ja te origen
+            origin_cups = []
+            pol_cups = []
             if origen_ids:
-                raise osv.except_osv(
-                    _("Error"),
-                    _("La bateria virtual seleccionada ja te origen.")
-                )
+                for origen_id in origen_ids:
+                    orig_pol_id = origen_obj.read(
+                        cursor, uid, origen_id, ['origen_ref'], context=context
+                        )['origen_ref'].split(',')[1]
+                    pol_cups_id = pol_obj.read(cursor, uid, orig_pol_id, ['cups'], context=context)['cups'][0]
+                    origin_cups.append(pol_cups_id)
+                for pol_id in context.get('active_ids', []):
+                    cups_pol_id = pol_obj.read(cursor, uid, pol_id, ['cups'], context=context)['cups'][0]
+                    if cups_pol_id not in origin_cups:
+                        raise osv.except_osv(
+                            _("Error"),
+                            _("La bateria virtual seleccionada ja té un origen amb un CUPS diferent.")
+                        )
+                    if pol_cups and (cups_pol_id not in pol_cups):
+                        raise osv.except_osv(
+                            _("Error"),
+                            _("S'estan intentant assignar varies pòlisses amb diferent CUPS com a orígens de la bateria virtual.")
+                        )
+                    pol_cups.append(cups_pol_id)
 
         return super(WizardAfegirContracteBateriaVirtual, self).action_assignar_bateria_virtual(
             cursor, uid, ids, context=context
