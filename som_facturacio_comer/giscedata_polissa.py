0fef1eb3e7a0b8a50bc413fdf792087e42ef058f
Alex <acifuentes@gisce.net>
2023-08-17 15:41:12 +0200

    fix search factures

--- som_facturacio_comer/giscedata_polissa.py
+++ som_facturacio_comer/giscedata_polissa.py
@@ -10,6 +10,20 @@ class GiscedataPolissa(osv.osv):
     _name = 'giscedata.polissa'
     _inherit = 'giscedata.polissa'
 
+    def search_factura(self, cursor, uid, ids, data_inici, data_final, context=None):
+        factura_obj = self.pool.get('giscedata.facturacio.factura')
+        factura_ids = factura_obj.search(cursor, uid, [
+            ('polissa_id', '=', ids[0]),
+            ('data_inici', '>=', data_inici),
+            ('state', 'in', ('paid', 'open')),
+            ('type', 'in', ('out_invoice', 'out_refund')),
+            '|',
+            ('data_final', '>', data_final),
+            ('data_final', '=', False)
+        ], context=context
+                                         )
+        return factura_ids
+
     _columns = {
         'teoric_maximum_consume_gc': fields.float(
             digits=(8,2),
