d2e69175fc16d6e64469cc52117de16f24d05052
destanyol <daniel.estanyol@somenergia.coop>
2023-08-21 11:33:55 +0200

    🐛 webform interface: proper decoration
    Using a decorator factory instead of py3 parameter passing
    
    Co-authored-by: David García <david.garcia@somenergia.coop>

--- som_indexada/som_indexada_webforms_helpers.py
+++ som_indexada/som_indexada_webforms_helpers.py
@@ -4,40 +4,42 @@ from som_indexada.exceptions import indexada_exceptions
 from datetime import datetime
 from decorator import decorator
 
-@decorator
-def www_entry_point(f, self, cursor *args, expected_exceptions=tuple(), **kwds):
-    """
-    Wrapps an erp method so that it can be called safely from the web.
+def www_entry_point(expected_exceptions=tuple()):
 
-    - Establishes a database savepoint to return at if an exception happens.
-    - Translates any expected_exceptions to a error dictionary.
-    - Any other exception will be also translated but with code 'Unexpected'
-    - To any exception, expected or unexpected,
-      it adds the backtrace as attribute of the dictionary.
-    """
-    def traceback_info(exception):
-        import traceback
-        import sys
-        exc_type, exc_value, exc_tb = sys.exc_info()
-        return traceback.format_exception(exc_type, exc_value, exc_tb)
+    def wrapper(f, self, cursor, *args, **kwds):
+        """
+        Wrapps an erp method so that it can be called safely from the web.
 
-    savepoint = 'change_pricelist_{}'.format(id(cursor))
-    cursor.savepoint(savepoint)
-    try:
-        return f(self, cursor, *args, **kwds)
-    except expected_exceptions as e:
-        cursor.rollback(savepoint)
-        return dict(
-            e.to_dict(),
-            trace=self.traceback_info(e),
-        )
-    except Exception as e:
-        cursor.rollback(savepoint)
-        return dict(
-            error=str(e),
-            code="Unexpected",
-            trace=self.traceback_info(e),
-        )
+        - Establishes a database savepoint to return at if an exception happens.
+        - Translates any expected_exceptions to a error dictionary.
+        - Any other exception will be also translated but with code 'Unexpected'
+        - To any exception, expected or unexpected,
+          it adds the backtrace as attribute of the dictionary.
+        """
+        def traceback_info(exception):
+            import traceback
+            import sys
+            exc_type, exc_value, exc_tb = sys.exc_info()
+            return traceback.format_exception(exc_type, exc_value, exc_tb)
+
+        savepoint = 'change_pricelist_{}'.format(id(cursor))
+        cursor.savepoint(savepoint)
+        try:
+            return f(self, cursor, *args, **kwds)
+        except expected_exceptions as e:
+            cursor.rollback(savepoint)
+            return dict(
+                e.to_dict(),
+                trace=traceback_info(e),
+            )
+        except Exception as e:
+            cursor.rollback(savepoint)
+            return dict(
+                error=str(e),
+                code="Unexpected",
+                trace=traceback_info(e),
+            )
+    return decorator(wrapper)
 
 class SomIndexadaWebformsHelpers(osv.osv_memory):
 
