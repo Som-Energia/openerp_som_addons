4433acf81963e6a5a60d77429f4b0b3e4e39313e
pauboixsom <pau.boix@somenergia.coop>
2023-08-22 15:47:05 +0200

    fix exces v2

--- som_informe/report/components/InvoiceF1NG/InvoiceF1NG.py
+++ som_informe/report/components/InvoiceF1NG/InvoiceF1NG.py
@@ -67,7 +67,10 @@ class InvoiceF1NG:
                     dict_linia['consum_entre'] = linia.lectura_actual - linia.lectura_desde
                     dict_linia['ajust'] = linia.ajust
                     i_line = get_invoice_line(invoice, linia.magnitud, linia.periode)
-                    dict_linia['total_facturat'] = i_line.quantity if i_line else ""
+                    if dict_linia['magnitud_desc'] == 'Excesos de potencia':
+                        dict_linia['total_facturat'] = i_line.price_unit if i_line else 0
+                    else:
+                        dict_linia['total_facturat'] = i_line.quantity if i_line else 0
                     dict_linia['unit'] = get_unit_magnitude(linia.magnitud)
                     result['linies'].append(dict_linia)
             elif f1.tipo_factura_f1 == 'otros':
