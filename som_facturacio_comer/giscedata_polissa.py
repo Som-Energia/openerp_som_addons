66c87d01105a5fef055685f3f5605c70fbc8078f
aorellana <aorellana@gisce.net>
2023-08-24 10:58:41 +0200

    Fix calcular descomptes

--- som_facturacio_comer/giscedata_polissa.py
+++ som_facturacio_comer/giscedata_polissa.py
@@ -11,10 +11,24 @@ class GiscedataPolissa(osv.osv):
     _inherit = 'giscedata.polissa'
 
     def search_factura(self, cursor, uid, ids, data_inici, data_final, context=None):
+        if context is None:
+            context = {}
         factura_obj = self.pool.get('giscedata.facturacio.factura')
+        bat_polissa_obj = self.pool.get("giscedata.bateria.virtual.polissa")
+        dmn = [
+            ('polissa_id', '=', ids[0]),
+            ('es_origen', '=', True)
+        ]
+        bat_id = bat_polissa_obj.q(cursor, uid).read(
+            ['bateria_id']
+        ).where(dmn)
+        bat_id = bat_id[0]['bateria_id']
+        data_inici_bat_pol = bat_polissa_obj.read(cursor, uid, bat_id, ['data_inici'], context=context)['data_inici']
+
         factura_ids = factura_obj.search(cursor, uid, [
             ('polissa_id', '=', ids[0]),
-            ('data_inici', '>=', data_inici),
+            ('date_invoice', '>=', data_inici),
+            ('data_inici', '>=', data_inici_bat_pol),
             ('state', 'in', ('paid', 'open')),
             ('type', 'in', ('out_invoice', 'out_refund')),
             '|',
