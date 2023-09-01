56e1f79b12076b19965388231c6ebaae2a60f507
Somenergia IT <itcrowd@somenergia.coop>
2023-09-01 12:19:06 +0200

    Fix super call on switching validation

--- som_facturacio_switching/giscedata_facturacio_validation.py
+++ som_facturacio_switching/giscedata_facturacio_validation.py
@@ -12,7 +12,7 @@ class GiscedataFacturacioValidationValidator(osv.osv):
         for bat in fact.polissa_id.bateria_ids:
             if (datetime.strptime(bat.data_inici, '%Y-%m-%d') < datetime.today()
                     and (not bat.data_final or datetime.strptime(bat.data_final, '%Y-%m-%d') > datetime.today())):
-                return super(GiscedataFacturacioValidationValidator).check_factura_amb_expedient(cursor, uid, fact, parameters)
+                return super(GiscedataFacturacioValidationValidator, self).check_factura_amb_expedient(cursor, uid, fact, parameters)
 
         return None
 
