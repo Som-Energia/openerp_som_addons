f62dd59a7bff77acf0ea7a388a5dfc5c0228383d
Marc Bellavista <marcbellavistafabrega@gmail.com>
2023-09-01 11:48:50 +0200

    fix check if bateria virtual with dates

--- som_facturacio_switching/giscedata_facturacio_validation.py
+++ som_facturacio_switching/giscedata_facturacio_validation.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+from datetime import datetime
+
 from osv import osv
 
 
@@ -7,8 +9,10 @@ class GiscedataFacturacioValidationValidator(osv.osv):
     _name = 'giscedata.facturacio.validation.validator'
 
     def check_factura_amb_expedient(self, cursor, uid, fact, parameters):
-        if fact.polissa_id.bateria_ids:
-            return super(GiscedataFacturacioValidationValidator).check_factura_amb_expedient(cursor, uid, fact, parameters)
+        for bat in fact.polissa_id.bateria_ids:
+            if (datetime.strptime(bat.data_inici, '%Y-%m-%d') < datetime.today()
+                    and (not bat.data_final or datetime.strptime(bat.data_final, '%Y-%m-%d') > datetime.today())):
+                return super(GiscedataFacturacioValidationValidator).check_factura_amb_expedient(cursor, uid, fact, parameters)
 
         return None
 
