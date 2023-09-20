f2705bdbcb4c6870f08be6da1cd6c1ac7f8bcedb
xavier <xavier.teres@somenergia.coop>
2023-09-20 10:57:14 +0200

    Add black formatting

--- som_extend_facturacio_comer/giscedata_facturacio_validation.py
+++ som_extend_facturacio_comer/giscedata_facturacio_validation.py
@@ -6,25 +6,21 @@ from dateutil.relativedelta import relativedelta
 
 
 class GiscedataFacturacioValidationValidator(osv.osv):
-    _name = 'giscedata.facturacio.validation.validator'
-    _inherit = 'giscedata.facturacio.validation.validator'
-    _desc = 'Validador de factures'
+    _name = "giscedata.facturacio.validation.validator"
+    _inherit = "giscedata.facturacio.validation.validator"
+    _desc = "Validador de factures"
 
     def check_missing_days(self, cursor, uid, fact, parameters):
-        modcontract_obj = self.pool.get('giscedata.polissa.modcontractual')
-        fact_obj = self.pool.get('giscedata.facturacio.factura')
+        modcontract_obj = self.pool.get("giscedata.polissa.modcontractual")
+        fact_obj = self.pool.get("giscedata.facturacio.factura")
 
-        trad_months_fact = {
-            1: 'n_days_mensual',
-            2: 'n_days_bimensual',
-            12: 'n_days_anual'
-        }
+        trad_months_fact = {1: "n_days_mensual", 2: "n_days_bimensual", 12: "n_days_anual"}
 
         months_fact = fact.polissa_id.facturacio
         increment = relativedelta(months=months_fact, days=-1)
 
-        start_date = datetime.strptime(fact.data_inici, '%Y-%m-%d')
-        end_date = datetime.strptime(fact.data_final, '%Y-%m-%d')
+        start_date = datetime.strptime(fact.data_inici, "%Y-%m-%d")
+        end_date = datetime.strptime(fact.data_final, "%Y-%m-%d")
         expected_final_date = start_date + increment
 
         expected = (expected_final_date - start_date).days
@@ -36,26 +32,30 @@ class GiscedataFacturacioValidationValidator(osv.osv):
             # Hi ha una modcon amb data: fact.data_final + 1 dies
             data_final = end_date + timedelta(days=1)
             polissa_id = fact.polissa_id.id
-            mod = modcontract_obj.search(cursor, uid, [
-                ('polissa_id', '=', polissa_id),
-                ('data_inici', '=', data_final.strftime('%Y-%m-%d'))]
+            mod = modcontract_obj.search(
+                cursor,
+                uid,
+                [
+                    ("polissa_id", "=", polissa_id),
+                    ("data_inici", "=", data_final.strftime("%Y-%m-%d")),
+                ],
             )
 
             # Hi ha una factura posterior
-            posterior = fact_obj.search(cursor, uid, [
-                ('data_inici', '>', fact.data_inici),
-                ('id', '!=', fact.id),
-                ('polissa_id', '=', polissa_id)
-            ])
+            posterior = fact_obj.search(
+                cursor,
+                uid,
+                [
+                    ("data_inici", ">", fact.data_inici),
+                    ("id", "!=", fact.id),
+                    ("polissa_id", "=", polissa_id),
+                ],
+            )
 
             if mod and not posterior:
-                return {
-                    'margin': margin,
-                    'actual_days': actual,
-                    'expected_days': expected
-                }
+                return {"margin": margin, "actual_days": actual, "expected_days": expected}
 
         return None
 
-GiscedataFacturacioValidationValidator()
 
+GiscedataFacturacioValidationValidator()
--- som_extend_facturacio_comer/payment_order.py
+++ som_extend_facturacio_comer/payment_order.py
@@ -9,7 +9,9 @@ class payment_order(osv.osv):
     _name = "payment.order"
     _inherit = "payment.order"
 
-    def add_invoices_to_payment_order(self, cursor, uid, mode_pagament_id, data_venciment, context=None):
+    def add_invoices_to_payment_order(
+        self, cursor, uid, mode_pagament_id, data_venciment, context=None
+    ):
         """
         Afegeix factures a la remesa amb mode de pagament `mode_pagament` que
         estigui en estat 'obert' o en 'esborrany'. Si no hi ha cap remesa que
@@ -33,31 +35,34 @@ class payment_order(osv.osv):
         """
         if context is None:
             context = {}
-        factura_o = self.pool.get('giscedata.facturacio.factura')
-        wiz_afegir_a_remesa_o = self.pool.get('wizard.afegir.factures.remesa')
-        imd_obj = self.pool.get('ir.model.data')
+        factura_o = self.pool.get("giscedata.facturacio.factura")
+        wiz_afegir_a_remesa_o = self.pool.get("wizard.afegir.factures.remesa")
+        imd_obj = self.pool.get("ir.model.data")
 
         df_pending_state_id = imd_obj.get_object_reference(
-            cursor, uid, 'account_invoice_pending', 'default_invoice_pending_state'
+            cursor, uid, "account_invoice_pending", "default_invoice_pending_state"
         )[1]
 
         bs_pending_state_id = imd_obj.get_object_reference(
-            cursor, uid, 'giscedata_facturacio_comer_bono_social', 'correct_bono_social_pending_state'
+            cursor,
+            uid,
+            "giscedata_facturacio_comer_bono_social",
+            "correct_bono_social_pending_state",
         )[1]
 
         correct_ids = [df_pending_state_id, bs_pending_state_id]
         from_date_due = datetime.strptime(data_venciment, "%Y-%m-%d") - timedelta(days=7)
         from_date_due = from_date_due.strftime("%Y-%m-%d")
         dmn = [
-            ('date_due', '<=', data_venciment),
-            ('date_due', '>=', from_date_due),
-            ('payment_order_id', '=', False),
-            ('pending_state', 'in', correct_ids),
-            ('type', 'like', 'out_%'),
-            ('invoice_id.payment_type.code', '=', 'RECIBO_CSB'),
-            ('group_move_id', '=', False),
-            ('rectificative_type', 'in', ('N', 'C', 'G')),
-            ('state', 'in', ('draft', 'open'))
+            ("date_due", "<=", data_venciment),
+            ("date_due", ">=", from_date_due),
+            ("payment_order_id", "=", False),
+            ("pending_state", "in", correct_ids),
+            ("type", "like", "out_%"),
+            ("invoice_id.payment_type.code", "=", "RECIBO_CSB"),
+            ("group_move_id", "=", False),
+            ("rectificative_type", "in", ("N", "C", "G")),
+            ("state", "in", ("draft", "open")),
         ]
         factura_ids = factura_o.search(cursor, uid, dmn, context=context)
         remesa_created = False
@@ -65,9 +70,9 @@ class payment_order(osv.osv):
 
         if factura_ids:
             dmn = [
-                ('mode', '=', mode_pagament_id),
-                ('state', 'in', ('draft', 'open')),
-                ('type', '=', 'receivable')
+                ("mode", "=", mode_pagament_id),
+                ("state", "in", ("draft", "open")),
+                ("type", "=", "receivable"),
             ]
             remesa_id = self.search(cursor, uid, dmn, context=context)
 
@@ -78,19 +83,19 @@ class payment_order(osv.osv):
                     raise NotImplementedError
             else:
                 remesa_cv = {
-                    'mode': mode_pagament_id,
-                    'date_prefered': 'fixed',
-                    'type': 'receivable'
+                    "mode": mode_pagament_id,
+                    "date_prefered": "fixed",
+                    "type": "receivable",
                 }
                 ctx = context.copy()
-                ctx['type'] = 'receivable'
+                ctx["type"] = "receivable"
                 remesa_id = self.create(cursor, uid, remesa_cv, context=ctx)
                 remesa_created = True
 
             ctx = context.copy()
-            ctx['model'] = 'giscedata.facturacio.factura'
-            ctx['active_ids'] = factura_ids
-            wiz_afegir_a_remesa_cv = {'order': remesa_id}
+            ctx["model"] = "giscedata.facturacio.factura"
+            ctx["active_ids"] = factura_ids
+            wiz_afegir_a_remesa_cv = {"order": remesa_id}
             wiz_afegir_a_remesa_id = wiz_afegir_a_remesa_o.create(
                 cursor, uid, wiz_afegir_a_remesa_cv, context=ctx
             )
@@ -115,22 +120,21 @@ class payment_order(osv.osv):
         :type context: dict
         :return: True si no hi ha hagut cap problema.
         """
-        irmd_o = self.pool.get('ir.model.data')
+        irmd_o = self.pool.get("ir.model.data")
 
-        logger = logging.getLogger('openerp.payment.order.remesa_automatica')
-        tomorrow = (date.today() + timedelta(days=1)).strftime('%Y-%m-%d')
+        logger = logging.getLogger("openerp.payment.order.remesa_automatica")
+        tomorrow = (date.today() + timedelta(days=1)).strftime("%Y-%m-%d")
 
         mode_pagament_id = irmd_o.get_object_reference(
-            cursor, uid, 'som_extend_facturacio_comer',
-            'payment_mode_remesa_automatica'
+            cursor, uid, "som_extend_facturacio_comer", "payment_mode_remesa_automatica"
         )[1]
         res = self.add_invoices_to_payment_order(
             cursor, uid, mode_pagament_id, tomorrow, context=context
         )
-        payment_order_created_txt = 'Existing'
+        payment_order_created_txt = "Existing"
 
         if res[2]:
-            payment_order_created_txt = 'New'
+            payment_order_created_txt = "New"
 
         message_to_log = "{} facturas added. Affected payment order: {} ({})".format(
             res[0], res[1], payment_order_created_txt
