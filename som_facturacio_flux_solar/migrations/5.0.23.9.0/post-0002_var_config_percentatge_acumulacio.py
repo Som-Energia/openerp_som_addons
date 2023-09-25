8924a3f81ad39eeae2cb7995e9a0cf9e1db58dd0
Alex <acifuentes@gisce.net>
2023-09-25 15:30:25 +0200

    fic var config percentatges

---
+++ som_facturacio_flux_solar/migrations/5.0.23.9.0/post-0002_var_config_percentatge_acumulacio.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+import logging
+import pooler
+from oopgrade.oopgrade import load_data, load_data_records
+
+
+def up(cursor, installed_version):
+    if not installed_version:
+        return
+
+    logger = logging.getLogger('openerp.migration')
+
+    logger.info("Creating pooler")
+    pool = pooler.get_pool(cursor.dbname)
+
+    ##UPDATAR UNA PART DE L'XML (POSAR LA ID)##
+    logger.info("Updating XMLs")
+    list_of_records = [
+        "percentatge_acumulacio"
+    ]
+    load_data_records(
+        cursor, 'som_facturacio_flux_solar', 'giscedata_bateria_virtual_percentatges_acumulacio_data.xml', list_of_records, mode='update'
+    )
+    logger.info("XMLs succesfully updated.")
+
+    logger.info("Updating access CSV")
+    load_data(
+        cursor, 'som_facturacio_flux_solar', 'security/ir.model.access.csv', idref=None, mode='update'
+    )
+    logger.info("CSV succesfully updated.")
+
+
+def down(cursor, installed_version):
+    pass
+
+
+migrate = up
