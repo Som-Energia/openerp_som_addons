1d8ae94109ea1d844097dc2a3e1bd80dfa05889d
Somenergia IT <itcrowd@somenergia.coop>
2023-10-11 15:45:33 +0200

    manual apply 457

---
+++ som_facturacio_flux_solar/giscedata_polissa.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from datetime import datetime
+
+from osv import osv, fields
+
+
+class GiscedataPolissa(osv.osv):
+    """Pòlissa per afegir el camp de soci.
+    """
+    _name = 'giscedata.polissa'
+    _inherit = 'giscedata.polissa'
+
+    @staticmethod
+    def is_autoconsum_amb_excedents(cursor, uid, autoconsumo, reverse=False, context=None):
+        if context is None:
+            context = {}
+        if autoconsumo in ('41', '42', '43'):
+            return True
+        else:
+            return False
+
+
+GiscedataPolissa()
