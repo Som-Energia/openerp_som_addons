9448dc8a11cb817ba40aa1282f841fbed7319919
Somenergia IT <itcrowd@somenergia.coop>
2023-10-11 17:52:12 +0200

    fix init file

--- som_facturacio_flux_solar/__init__.py
+++ som_facturacio_flux_solar/__init__.py
@@ -2,3 +2,4 @@ import giscedata_bateria_virtual_percentatges_acumulacio
 import giscedata_bateria_virtual_origen
 import giscedata_bateria_virtual
 import giscedata_bateria_virtual_polissa
+from giscedata_polissa import *
