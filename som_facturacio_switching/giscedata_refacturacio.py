0d3effff7a616b057280af3f42f7cc8129b913c0
Marc Bellavista <marcbellavistafabrega@gmail.com>
2023-10-23 10:09:31 +0200

    fix create for refacturacio

--- som_facturacio_switching/giscedata_refacturacio.py
+++ som_facturacio_switching/giscedata_refacturacio.py
@@ -6,16 +6,15 @@ class GiscedataRefacturacio(osv.osv):
     _name = 'giscedata.refacturacio'
     _inherit = 'giscedata.refacturacio'
 
+    def create(self, cursor, uid, values, context=None):
+        if context is None:
+            context = {}
 
-def create(self, cursor, uid, values, context=None):
-    if context is None:
-        context = {}
+        res = super(GiscedataRefacturacio, self).create(cursor, uid, values, context=context)
 
-    res = super(GiscedataRefacturacio, self).create(cursor, uid, values, context=context)
+        type_factura = self._get_tipo_origen(cursor, uid, res, context=context).get(res)
 
-    type_factura = self._get_tipo_origen(cursor, uid, res, context=context).get(res)
+        if type_factura == 'G':
+            self.write(cursor, uid, res, {'refacturat': True}, context=context)
 
-    if type_factura == 'G':
-        self.write(cursor, uid, res, {'refacturat': True}, context=context)
-
-    return res
+        return res
