8b027e653299e435094e3af018795ff10563c223
pedropote <pere.montagud@somenergia.coop>
2023-10-31 11:33:58 +0100

    Change the field definitions to allow 6 decimals
    Co-authored-by: Fran Cubero <francisco.cubero@somenergia.coop>

--- som_leads_polissa/giscedata_crm_lead.py
+++ som_leads_polissa/giscedata_crm_lead.py
@@ -63,20 +63,19 @@ class GiscedataCrmLead(osv.OsvInherits):
 
     _columns = {
         'tipus_tarifa_lead': fields.selection(
-            _tipus_tarifes_lead, 'Tipus de tarifa del contracte'
-        ),
-        'preu_fix_energia_p1': fields.float('Preu Fix Energia P1', digits=(16, 4)),
-        'preu_fix_energia_p2': fields.float('Preu Fix Energia P2', digits=(16, 4)),
-        'preu_fix_energia_p3': fields.float('Preu Fix Energia P3', digits=(16, 4)),
-        'preu_fix_energia_p4': fields.float('Preu Fix Energia P4', digits=(16, 4)),
-        'preu_fix_energia_p5': fields.float('Preu Fix Energia P5', digits=(16, 4)),
-        'preu_fix_energia_p6': fields.float('Preu Fix Energia P6', digits=(16, 4)),
-        'preu_fix_potencia_p1': fields.float('Preu Fix Potència P1', digits=(16, 4)),
-        'preu_fix_potencia_p2': fields.float('Preu Fix Potència P2', digits=(16, 4)),
-        'preu_fix_potencia_p3': fields.float('Preu Fix Potència P3', digits=(16, 4)),
-        'preu_fix_potencia_p4': fields.float('Preu Fix Potència P4', digits=(16, 4)),
-        'preu_fix_potencia_p5': fields.float('Preu Fix Potència P5', digits=(16, 4)),
-        'preu_fix_potencia_p6': fields.float('Preu Fix Potència P6', digits=(16, 4)),
+            _tipus_tarifes_lead, 'Tipus de tarifa del contracte'),
+        'preu_fix_energia_p1': fields.float('Preu Fix Energia P1', digits=(16, 6)),
+        'preu_fix_energia_p2': fields.float('Preu Fix Energia P2', digits=(16, 6)),
+        'preu_fix_energia_p3': fields.float('Preu Fix Energia P3', digits=(16, 6)),
+        'preu_fix_energia_p4': fields.float('Preu Fix Energia P4', digits=(16, 6)),
+        'preu_fix_energia_p5': fields.float('Preu Fix Energia P5', digits=(16, 6)),
+        'preu_fix_energia_p6': fields.float('Preu Fix Energia P6', digits=(16, 6)),
+        'preu_fix_potencia_p1': fields.float('Preu Fix Potència P1', digits=(16, 6)),
+        'preu_fix_potencia_p2': fields.float('Preu Fix Potència P2', digits=(16, 6)),
+        'preu_fix_potencia_p3': fields.float('Preu Fix Potència P3', digits=(16, 6)),
+        'preu_fix_potencia_p4': fields.float('Preu Fix Potència P4', digits=(16, 6)),
+        'preu_fix_potencia_p5': fields.float('Preu Fix Potència P5', digits=(16, 6)),
+        'preu_fix_potencia_p6': fields.float('Preu Fix Potència P6', digits=(16, 6)),
     }
 
     _defaults = {
