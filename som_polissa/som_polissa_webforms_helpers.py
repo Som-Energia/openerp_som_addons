97caeeea0aee1bd06b6b6e4ca735c92294c65e56
xavier <xavier.teres@somenergia.coop>
2023-11-03 13:22:19 +0100

    Update payment order lines

--- som_polissa/som_polissa_webforms_helpers.py
+++ som_polissa/som_polissa_webforms_helpers.py
@@ -31,6 +31,37 @@ class SomPolissaWebformsHelpers(osv.osv_memory):
 
         return result
 
+    def _update_payment_lines(self, cursor, uid, polissa_id, context):
+            fact_obj = self.pool.get('giscedata.facturacio.factura')
+            order_obj = self.pool.get('payment.order')
+            line_obj = self.pool.get('payment.line')
+
+            search_vals = [
+                ('polissa_id', '=', polissa_id),
+                ('type', 'in', ['out_invoice', 'out_refund']),
+                ('state', 'in', ['draft', 'open']),
+            ]
+            factura_ids = fact_obj.search(cursor, uid, search_vals, context=context)
+            factures = fact_obj.browse(factures_ids)
+
+            draft_order_ids = []
+            for factura in factures:
+                if factura.payment_order_id and factura.payment_order_id.state == 'draft':
+                    draft_order_ids.append(factura.payment_order_id.id)
+                else:
+                    continue
+
+            if draft_order_ids:
+                bank_id = pol_obj.read(
+                    cursor, uid, polissa_id, ['bank'], context=context)['bank']
+                partner_id = pol_obj.read(
+                    cursor, uid, polissa_id, ['titular'], context=context)['titular']
+                line_ids = line_obj.search(cursor, uid, [
+                    ('order_id', 'in', draft_order_ids),
+                    ('partner_id', '=', partner_id),
+                ])
+                line_obj.write(cursor, uid, line_ids, {'bank_id': bank_id})
+
     def www_set_iban(self, cursor, uid, ids, iban, context=None):
         if context is None:
             context = {}
@@ -59,6 +90,7 @@ class SomPolissaWebformsHelpers(osv.osv_memory):
 
         result = False
         if wizard_result == {}:
+            self._update_payment_lines(cursor, uid, polissa_id, context=context)
             result = True
         return result
 
