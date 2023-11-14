fbad853374a1c722cad62240de3b9a027f5665cf
Oriol Piera <oriol.piera@somenergia.coop>
2023-11-14 08:17:51 +0100

    Add cnmc estimation method and refactor to reuse code to call gisce method

--- som_polissa/giscedata_cups.py
+++ som_polissa/giscedata_cups.py
@@ -30,8 +30,8 @@ class GiscedataCupsPs(osv.osv):
                                origen['origen'], origen['origen']))
                 self._columns['conany_origen'].selection.append(new_sel)
 
-    def get_consum_anual_backend_gisce(self, cursor, uid, polissa_id, context=None):
-        """ Consum anual segons query de del backend de la factura de GISCE """
+    def get_desdes_consum_anual_historic_backend_gisce(self, cursor, uid, polissa_id, context):
+        """ Obtenir dades consum anual segons query de del backend de la factura de GISCE """
         if context is None:
             context = {}
         if isinstance(polissa_id, (tuple, list)):
@@ -46,8 +46,12 @@ class GiscedataCupsPs(osv.osv):
             cursor, uid, search_params, order="data_inici desc", context=context
         )[0]
 
-        historic = factura_backend_obj.get_grafica_historic_consum_14_mesos(
-            cursor, uid, last_inv, context=context)
+        return factura_backend_obj.get_grafica_historic_consum_14_mesos(cursor, uid, last_inv, context=context)
+
+    def get_consum_anual_backend_gisce(self, cursor, uid, polissa_id, context=None):
+        """ Consum anual segons query de del backend de la factura de GISCE """
+        historic = self.get_desdes_consum_anual_historic_backend_gisce(
+            cursor, uid, polissa_id, context)
 
         consums = historic['historic_js']
         if len(consums) < 12:
@@ -55,13 +59,73 @@ class GiscedataCupsPs(osv.osv):
 
         consum_periodes = {'P1': 0, 'P2': 0, 'P3': 0, 'P4': 0, 'P5': 0, 'P6': 0}
         for invoice in consums:
-            for k, _ in invoice.items():
+            for k in invoice.keys():
                 if k in consum_periodes:
                     consum_periodes[k] += float(invoice[k].replace(',', '.'))
 
         days_of_consume = int(historic['historic']['days'])
-        for k, _ in consum_periodes.items():
+        for k in consum_periodes.keys():
             consum_periodes[k] = int(consum_periodes[k] * 365 / days_of_consume)
+        return consum_periodes
+
+    def get_consum_prorrageig_cnmc(self, cursor, uid, polissa_id, context=None):
+        """ Consum anual estimat perfilant mesos segons fòrmula CNMC """
+        pol_obj = self.pool.get('giscedata.polissa')
+        historic = self.get_desdes_consum_anual_historic_backend_gisce(
+            cursor, uid, polissa_id, context)
+        consums = historic['historic_js']
+        if len(consums) < 3:
+            return False
+
+        cnmc_formula_20TD = {
+            '1': 0.0940,
+            '2': 0.0791,
+            '3': 0.0785,
+            '4': 0.0723,
+            '5': 0.0688,
+            '6': 0.0713,
+            '7': 0.0854,
+            '8': 0.0853,
+            '9': 0.0762,
+            '10': 0.0806,
+            '11': 0.0961,
+            '12': 0.1124,
+        }
+        cnmc_formula_30TD = {
+            '1': 0.0862,
+            '2': 0.0761,
+            '3': 0.0794,
+            '4': 0.0744,
+            '5': 0.0794,
+            '6': 0.0843,
+            '7': 0.0983,
+            '8': 0.0924,
+            '9': 0.0848,
+            '10': 0.0831,
+            '11': 0.0801,
+            '12': 0.0812,
+        }
+
+        tarifa_pol = pol_obj.read(cursor, uid, polissa_id, ['tarifa_codi'])['tarifa_codi']
+        if tarifa_pol == '6.0TD':
+            return False
+        cnmc_formula = cnmc_formula_20TD if tarifa_pol == '2.0TD' else cnmc_formula_30TD
+        # Agafem les dades que tenim
+        consum_periodes = {'P1': 0, 'P2': 0, 'P3': 0, 'P4': 0, 'P5': 0, 'P6': 0}
+        mesos_amb_factures = []
+        for invoice in consums:
+            for k in invoice.keys():
+                if k in consum_periodes:
+                    consum_periodes[k] += float(invoice[k].replace(',', '.'))
+            mesos_amb_factures.append(str(int(invoice['mes'].split('/')[1])))
+        # Sumem percent dels mesos que tenim
+        percent_acumulat = 0
+        for mes in mesos_amb_factures:
+            percent_acumulat += cnmc_formula[mes]
+        # Calculem totals anuals
+        for k in consum_periodes.keys():
+            consum_periodes[k] = int(consum_periodes[k] / percent_acumulat)
+
         return consum_periodes
 
     def get_fonts_consums_anuals(self, cursor, uid, context=None):
--- som_polissa/tests/giscedata_cups_tests.py
+++ som_polissa/tests/giscedata_cups_tests.py
@@ -48,7 +48,7 @@ class TestGisceDataCups(testing.OOTestCase):
                                           {u'P1': u'183,00', u'P2': u'99,00', u'P3': u'256,00', u'mes': u'2023/08'}]}
 
         result = self.cups_obj.get_consum_anual_backend_gisce(
-            self.cursor, self.uid, self.contract1_id, {'lang': 'en_US', 'backend_test': ['all']})
+            self.cursor, self.uid, self.contract1_id)
 
         self.assertEqual(result, False)
 
@@ -94,6 +94,60 @@ class TestGisceDataCups(testing.OOTestCase):
                                                        {u'P1': u'183,00', u'P2': u'99,00', u'P3': u'256,00', u'mes': u'2023/08'}]}
 
         result = self.cups_obj.get_consum_anual_backend_gisce(
-            self.cursor, self.uid, self.contract1_id, {'lang': 'en_US', 'backend_test': ['all']})
+            self.cursor, self.uid, self.contract1_id)
 
         self.assertEqual(result, {'P2': 2033, 'P3': 4235, 'P1': 2254, 'P6': 0, 'P4': 0, 'P5': 0})
+
+    @mock.patch("giscedata_facturacio_comer.giscedata_facturacio_report_v2.GiscedataFacturacioFacturaReportV2.get_grafica_historic_consum_14_mesos")
+    def test__get_consum_prorrageig_cnmc__lessThan2(self, mock_function):
+        mock_function.return_value = {u'historic': {u'average_consumption': u'23,35',
+                                                    u'average_cost': u'4,95',
+                                                    u'consumption': u'9.947,00',
+                                                    u'days': 426,
+                                                    u'end_date': u'15/08/2023',
+                                                    u'init_date': u'16/06/2022',
+                                                    u'invoiced': u'2.109,66',
+                                                    u'months': 14,
+                                                    u'year_consumption': u'9.432,00'},
+                                      u'historic_js': [
+                                          {u'P1': u'132,00', u'P2': u'92,00',
+                                              u'P3': u'194,00', u'mes': u'2023/07'},
+                                          {u'P1': u'183,00', u'P2': u'99,00', u'P3': u'256,00', u'mes': u'2023/08'}]}
+
+        result = self.cups_obj.get_consum_prorrageig_cnmc(
+            self.cursor, self.uid, self.contract1_id)
+
+        self.assertEqual(result, False)
+
+    @mock.patch("giscedata_facturacio_comer.giscedata_facturacio_report_v2.GiscedataFacturacioFacturaReportV2.get_grafica_historic_consum_14_mesos")
+    def test__get_consum_prorrageig_cnmc__moreThan2(self, mock_function):
+        mock_function.return_value = {u'historic': {u'average_consumption': u'23,35',
+                                                    u'average_cost': u'4,95',
+                                                    u'consumption': u'9.947,00',
+                                                    u'days': 426,
+                                                    u'end_date': u'15/08/2023',
+                                                    u'init_date': u'16/06/2022',
+                                                    u'invoiced': u'2.109,66',
+                                                    u'months': 14,
+                                                    u'year_consumption': u'9.432,00'},
+                                      u'historic_js': [{u'P1': u'176,00',
+                                                        u'P2': u'122,00',
+                                                        u'P3': u'217,00',
+                                                        u'mes': u'2022/07'},
+                                                       {u'P1': u'203,00', u'P2': u'179,00',
+                                                           u'P3': u'411,00', u'mes': u'2022/08'},
+                                                       {u'P1': u'414,00', u'P2': u'430,00',
+                                                           u'P3': u'564,00', u'mes': u'2023/02'},
+                                                       {u'P1': u'235,00', u'P2': u'220,00',
+                                                           u'P3': u'474,00', u'mes': u'2023/03'},
+                                                       {u'P1': u'145,00', u'P2': u'114,00',
+                                                           u'P3': u'327,00', u'mes': u'2023/04'},
+                                                       {u'P1': u'93,00', u'P2': u'67,00',
+                                                           u'P3': u'234,00', u'mes': u'2023/05'},
+                                                       {u'P1': u'88,00', u'P2': u'66,00',
+                                                           u'P3': u'172,00', u'mes': u'2023/06'}]}
+
+        result = self.cups_obj.get_consum_prorrageig_cnmc(
+            self.cursor, self.uid, self.contract1_id)
+
+        self.assertEqual(result, {'P2': 2050, 'P3': 4105, 'P1': 2317, 'P6': 0, 'P4': 0, 'P5': 0})
