e3f844ffa31115e078f375ce39eb6c4d5c88ca70
Somenergia IT <itcrowd@somenergia.coop>
2023-11-16 10:48:59 +0100

    Support separtation grups in numbers

--- som_polissa/giscedata_polissa.py
+++ som_polissa/giscedata_polissa.py
@@ -686,7 +686,7 @@ class GiscedataPolissa(osv.osv):
         for invoice in consums:
             for k in invoice.keys():
                 if k in consum_periodes:
-                    consum_periodes[k] += float(invoice[k].replace(',', '.'))
+                    consum_periodes[k] += float(invoice[k].replace('.','').replace(',', '.'))
 
         days_of_consume = int(historic['historic']['days'])
         for k in consum_periodes.keys():
@@ -742,7 +742,7 @@ class GiscedataPolissa(osv.osv):
         for invoice in consums:
             for k in invoice.keys():
                 if k in consum_periodes:
-                    consum_periodes[k] += float(invoice[k].replace(',', '.'))
+                    consum_periodes[k] += float(invoice[k].replace('.','').replace(',', '.'))
             mesos_amb_factures.append(str(int(invoice['mes'].split('/')[1])))
         # Sumem percent dels mesos que tenim
         percent_acumulat = 0
