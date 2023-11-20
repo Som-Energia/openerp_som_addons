7684fddb7d9f344fa306643795cc8d4047ff93f2
Xavier Terés <66311413+xavierteres@users.noreply.github.com>
2023-11-20 13:49:09 +0100

    Actualitzar IBAN de les factures no pagades (#472)
    * Update unpaid invoices
    
    * Update payment order lines
    
    * Fix tests
    
    * Add missing pol_obj
    
    * Fix read was returning tuple instead of id
    
    ---------
    
    Co-authored-by: Oriol Piera <oriol.piera@somenergia.coop>

--- som_polissa/som_polissa_webforms_helpers.py
+++ som_polissa/som_polissa_webforms_helpers.py
@@ -1,5 +1,6 @@
 from osv import osv
 
+
 class SomPolissaWebformsHelpers(osv.osv_memory):
 
     _name = 'som.polissa.webforms.helpers'
@@ -31,6 +32,37 @@ class SomPolissaWebformsHelpers(osv.osv_memory):
 
         return result
 
+    def _update_payment_lines(self, cursor, uid, polissa_id, context):
+        fact_obj = self.pool.get('giscedata.facturacio.factura')
+        pol_obj = self.pool.get('giscedata.polissa')
+        line_obj = self.pool.get('payment.line')
+
+        search_vals = [
+            ('polissa_id', '=', polissa_id),
+            ('type', 'in', ['out_invoice', 'out_refund']),
+            ('state', 'in', ['draft', 'open']),
+        ]
+        factura_ids = fact_obj.search(cursor, uid, search_vals, context=context)
+        factures = fact_obj.browse(cursor, uid, factura_ids)
+
+        draft_order_ids = []
+        for factura in factures:
+            if factura.payment_order_id and factura.payment_order_id.state == 'draft':
+                draft_order_ids.append(factura.payment_order_id.id)
+            else:
+                continue
+
+        if draft_order_ids:
+            bank_id = pol_obj.read(
+                cursor, uid, polissa_id, ['bank'], context=context)['bank'][0]
+            partner_id = pol_obj.read(
+                cursor, uid, polissa_id, ['titular'], context=context)['titular'][0]
+            line_ids = line_obj.search(cursor, uid, [
+                ('order_id', 'in', draft_order_ids),
+                ('partner_id', '=', partner_id),
+            ])
+            line_obj.write(cursor, uid, line_ids, {'bank_id': bank_id})
+
     def www_set_iban(self, cursor, uid, ids, iban, context=None):
         if context is None:
             context = {}
@@ -43,6 +75,7 @@ class SomPolissaWebformsHelpers(osv.osv_memory):
 
         vals = {
             'account_iban': iban,
+            'update_fact_no_pagades': True,
             'print_mandate': False,
             'state': 'end'
         }
@@ -58,6 +91,7 @@ class SomPolissaWebformsHelpers(osv.osv_memory):
 
         result = False
         if wizard_result == {}:
+            self._update_payment_lines(cursor, uid, polissa_id, context=context)
             result = True
         return result
 
