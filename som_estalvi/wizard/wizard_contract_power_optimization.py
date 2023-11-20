3343b6bcd56ddf47b4b7389e63dc31434eafda07
destanyol <daniel.estanyol@somenergia.coop>
2023-11-20 13:08:33 +0100

    🚧 (estalvi): Fight with dates and datetimes
    Co-authored-by: Pau Boix <pau.boix@somenergia.coop>

--- som_estalvi/wizard/wizard_contract_power_optimization.py
+++ som_estalvi/wizard/wizard_contract_power_optimization.py
@@ -2,21 +2,32 @@
 from osv import osv, fields
 from datetime import datetime, timedelta
 from giscedata_facturacio.report.utils import get_atr_price
+from dateutil.relativedelta import relativedelta
 
 
 class WizardContractPowerOptimization(osv.osv_memory):
     _name = "wizard.contract.power.optimization"
 
-    def get_optimization_required_data(self, cursor, uid, wiz_id, polissa_id, context=None):
+    def get_potencies_maximetres(self, cursor, uid, wiz_id, polissa_id, context=None):
         if context is None:
             context = {}
-        if not context.get('date'):
-            last_year = (datetime.now() - timedelta(year=1)).year
-            context['date'] = '{}-01-01'.format(last_year)
+
+        pol_obj = self.pool.get('giscedata.polissa')
+
+        # Primer hem de dectar si es hem de mirar quins comptadors hem de mirar
+        polissa = pol_obj.browse(cursor, uid, polissa_id, context=context)
+        start_date
+
+        comptadors = []
+        for comptador in polissa.comptadors:
+            if comptador.data_alta
+
+    def get_periods_power(self, cursor, uid, wiz_id, polissa_id, context=None):
+        if context is None:
+            context = {}
 
         pol_obj = self.pool.get('giscedata.polissa')
         polissa = pol_obj.browse(cursor, uid, polissa_id, context=context)
-
         wizard = self.browse(cursor, uid, wiz_id, context=context)
 
         wiz.potencia_p1 = polissa.potencies_periode[0].potencia
@@ -25,6 +36,14 @@ class WizardContractPowerOptimization(osv.osv_memory):
         wiz.potencia_p4 = polissa.potencies_periode[3].potencia
         wiz.potencia_p5 = polissa.potencies_periode[4].potencia
         wiz.potencia_p6 = polissa.potencies_periode[5].potencia
+
+    def get_periods_power_price(self, cursor, uid, wiz_id, polissa_id, context=None):
+        if context is None:
+            context = {}
+
+        pol_obj = self.pool.get('giscedata.polissa')
+        polissa = pol_obj.browse(cursor, uid, polissa_id, context=context)
+        wizard = self.browse(cursor, uid, wiz_id, context=context)
 
         wiz.preu_potencia_p1 = get_atr_price(
             cursor, uid, polissa_id, 'P1', 'tp', context=context
@@ -45,9 +64,26 @@ class WizardContractPowerOptimization(osv.osv_memory):
             cursor, uid, polissa_id, 'P6', 'tp', context=context
         )
 
+    def get_excess_price(self, cursor, uid, wiz_id, polissa_id, context=None):
+        if context is None:
+            context = {}
+
+        pol_obj = self.pool.get('giscedata.polissa')
+        polissa = pol_obj.browse(cursor, uid, polissa_id, context=context)
+        wizard = self.browse(cursor, uid, wiz_id, context=context)
+
         wiz.preu_exces = get_atr_price(
             cursor, uid, polissa_id, 'P1', 'epm', context=context
         )
+
+    def get_optimization_required_data(self, cursor, uid, wiz_id, polissa_id, context=None):
+        if context is None:
+            context = {}
+
+        # We fill the wizard data
+        self.get_periods_power(cursor, uid, wiz_id, polissa_id, context=context)
+        self.get_periods_power_price(cursor, uid, wiz_id, polissa_id, context=context)
+        self.get_excess_price(cursor, uid, wiz_id, polissa_id, context=context)
 
     def pass_maximeter_validation(self, cursor, uid, ids, context=None):
         pass
@@ -81,6 +117,22 @@ class WizardContractPowerOptimization(osv.osv_memory):
                 state = 'multiple'
         return state
 
+    def _compute_end_date(self, cr, uid, ids, field_name, arg, context={}):
+        result = {}
+        for wiz in self.browse(cr, uid, ids, context):
+            end_date = wiz.start_date + relativedelta(years=+1)
+            result[wiz.id] = end_ate
+        return result
+
+    def _default_start_date(self, cursor, uid, context=None):
+        if not context:
+            context = {}
+
+        last_year = (datetime.now() - timedelta(year=1)).year
+        start_date = '{}-01-01'.format(last_year)
+        return start_date
+
+
     _columns = {
         'state': fields.selection(
             [
@@ -91,6 +143,9 @@ class WizardContractPowerOptimization(osv.osv_memory):
         'State'),
 
         'preu_exces': fields.float('Preu excés maxímetre'),
+
+        'start_date': fields.datetime('Data d\'inici'),
+        'end_date': fields.function(_compute_end_date, method=True, string='Data final', type='date'),
 
         'potencia_p1': fields.integer('Potència P1'),
         'potencia_p2': fields.integer('Potència P2'),
@@ -110,7 +165,8 @@ class WizardContractPowerOptimization(osv.osv_memory):
     }
 
     _defaults = {
-        'state': _default_state
+        'state': _default_state,
+        'start_date': _default_start_date
     }
 
 
