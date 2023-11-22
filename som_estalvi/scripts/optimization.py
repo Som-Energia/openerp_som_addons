ba5b7287de5ff7c1ae9954b1c9e741203306ed0a
destanyol <daniel.estanyol@somenergia.coop>
2023-11-22 12:46:39 +0100

    🐛 (estalvi): Delete async
    Co-authored-by: Pau Boix <pau.boix@somenergia.coop>

--- som_estalvi/scripts/optimization.py
+++ som_estalvi/scripts/optimization.py
@@ -6,29 +6,17 @@ def optimize(dades):
     model_path = './optimization.mzn'
     minizinc.Model(model_path)
 
-    solvers = ["chuffed", "coin-bc"]
-    tasks = set()
-    for solver_name in self.solvers:
-        # Create an instance of the model for every solver
-        solver = minizinc.Solver.lookup(solver_name)
-        inst = minizinc.Instance(solver, self.model)
+    solver_name = "coin-bc"
+    # Create an instance of the model for every solver
+    solver = minizinc.Solver.lookup(solver_name)
+    inst = minizinc.Instance(solver, self.model)
 
-        for k,v in dades.items():
-            inst[k] = v
+    for k,v in dades.items():
+        inst[k] = v
 
-        task = asyncio.create_task(inst.solve_async)
-        task.solver = solver.name
-        tasks.add(task)
+    inst.solve()
 
-    # Wait on the first task to finish and cancel the other tasks
-    done, pending = await asyncio.wait(tasks, return_when=asyncio.FIRST_COMPLETED)
-    if done:
-        result = done.pop().result()
 
-    # Clean pending tasks
-    for task in pending:
-        task.cancel()
-        await asyncio.sleep(0.1)
 
 
     pass
