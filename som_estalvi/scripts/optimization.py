4ba2ee76f10798445416dfe8d584786f6baa51c7
destanyol <daniel.estanyol@somenergia.coop>
2023-11-28 12:56:58 +0100

    ✨ (estalvi): Improve minizinc launcher script
    Co-authored-by: Pau Boix <pau.boix@somenergia.coop>

--- som_estalvi/scripts/optimization.py
+++ som_estalvi/scripts/optimization.py
@@ -1,28 +1,22 @@
 import sys, json
 import minizinc
 
-def optimize(dades):
-    # exectuar minizinc
-    model_path = './optimization.mzn'
-    minizinc.Model(model_path)
+def optimize(dades, mzn_path):
+    model = minizinc.Model(mzn_path)
 
     solver_name = "coin-bc"
-    # Create an instance of the model for every solver
     solver = minizinc.Solver.lookup(solver_name)
     inst = minizinc.Instance(solver, self.model)
 
     for k,v in dades.items():
         inst[k] = v
 
-    inst.solve()
-
-
-
-
-    pass
+    res = inst.solve()
+    print (res.solution)
 
 # Main program optimize
 if __name__ == "__main__":
     standard_input = sys.stdin.read()
     dades = json.loads(standard_input)
-    optimize(dades)
+    mzn_path = dades.pop("mzn_path")
+    optimize(dades, mzn_path)
