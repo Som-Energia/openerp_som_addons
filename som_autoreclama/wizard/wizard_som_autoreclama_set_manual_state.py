e6d2e3e14ad887f86eb816e8e8d3231686c5c834
Fran Cubero <francisco.cubero@somenergia.coop>
2023-12-15 12:09:53 +0100

    modify set_manual_state wizard to filter from the workflow
    Co-authored-by: Xavier Terés <xavier.teres@somenergia.coop>

--- som_autoreclama/wizard/wizard_som_autoreclama_set_manual_state.py
+++ som_autoreclama/wizard/wizard_som_autoreclama_set_manual_state.py
@@ -7,23 +7,31 @@ from tools.translate import _
 class WizardSomAutoreclamaSetManualState(osv.osv_memory):
     _name = "wizard.som.autoreclama.set.manual.state"
 
-    def assign_state(self, cursor, uid, ids, context=None):
-        h_obj = self.pool.get("som.autoreclama.state.history.atc")
+    def _get_workflow_id(self, cursor, uid, context=None):
+        namespace = context.get("namespace", "atc")
+        ir_obj = self.pool.get("ir.model.data")
+        return ir_obj.get_object_reference(
+            cursor, uid, "som_autoreclama", "workflow_" + namespace
+        )[1]
 
-        atc_ids = context.get("active_ids", [])
+    def assign_state(self, cursor, uid, ids, context=None):
+        namespace = context.get("namespace", "atc")
+        h_obj = self.pool.get("som.autoreclama.state.history." + namespace)
+
+        item_ids = context.get("active_ids", [])
 
         wiz = self.browse(cursor, uid, ids[0], context)
 
         info = ""
-        for atc_id in atc_ids:
+        for item_id in item_ids:
             try:
-                h_obj.historize(cursor, uid, atc_id, wiz.next_state_id.id, None, False, context)
-                info += _("Cas {} estat canviat manualment a '{}'\n").format(
-                    atc_id, wiz.next_state_id.name
+                h_obj.historize(cursor, uid, item_id, wiz.next_state_id.id, None, False, context)
+                info += _("{} {} estat canviat manualment a '{}'\n").format(
+                    namespace.capitalize(), item_id, wiz.next_state_id.name
                 )
             except Exception as e:
-                info += _("Cas {} error al canviar manualment a '{}' : {}\n").format(
-                    atc_id, wiz.next_state_id.name, e.message
+                info += _("{} {} error al canviar manualment a '{}' : {}\n").format(
+                    namespace.capitalize(), item_id, wiz.next_state_id.name, e.message
                 )
 
         self.write(cursor, uid, ids, {"state": "end", "info": info})
@@ -32,12 +40,14 @@ class WizardSomAutoreclamaSetManualState(osv.osv_memory):
     _columns = {
         "state": fields.selection([("init", "Init"), ("end", "End")], "State"),
         "info": fields.text("Informació", readonly=True),
-        "next_state_id": fields.many2one("som.autoreclama.state", "Estat", required=True),
+        "workflow_id": fields.many2one("som.autoreclama.state.workflow", "Fluxe"),
+        "next_state_id": fields.many2one("som.autoreclama.state", "Estat", required=True, domain="[('workflow_id', '=', workflow_id)]"),
     }
 
     _defaults = {
         "state": lambda *a: "init",
         "info": lambda *a: "",
+        "workflow_id": _get_workflow_id,
     }
 
 
