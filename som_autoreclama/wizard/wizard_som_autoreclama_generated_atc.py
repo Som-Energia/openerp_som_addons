ae4ccfff79f6d250844b1d9bdb724a11cda2b907
Fran Cubero <francisco.cubero@somenergia.coop>
2023-12-15 12:47:13 +0100

    modify autoreclama_generated_atc list wizard with namespace
    Co-authored-by: Xavier Terés <xavier.teres@somenergia.coop>

--- som_autoreclama/wizard/wizard_som_autoreclama_generated_atc.py
+++ som_autoreclama/wizard/wizard_som_autoreclama_generated_atc.py
@@ -8,17 +8,19 @@ class WizardSomAutoreclamaGeneratedAtc(osv.osv_memory):
     _name = "wizard.som.autoreclama.generated.atc"
 
     def view_generated_atc(self, cursor, uid, ids, context=None):
-        atc_obj = self.pool.get("giscedata.atc")
-        h_obj = self.pool.get("som.autoreclama.state.history.atc")
-
         if not context:
             context = {}
 
-        atc_ids = context.get("active_ids", [])
+        namespace = context.get("namespace", "atc")
+        h_obj = self.pool.get("som.autoreclama.state.history." + namespace)
+
+        model_obj = self.pool.get("giscedata." + namespace)
+
+        item_ids = context.get("active_ids", [])
         generated_atc_ids = []
 
-        for atc_id in atc_ids:
-            h_ids = atc_obj.read(cursor, uid, atc_id, ["autoreclama_history_ids"], context=context)[
+        for item_id in item_ids:
+            h_ids = model_obj.read(cursor, uid, item_id, ["autoreclama_history_ids"], context=context)[
                 "autoreclama_history_ids"
             ]
             for h_id in h_ids:
