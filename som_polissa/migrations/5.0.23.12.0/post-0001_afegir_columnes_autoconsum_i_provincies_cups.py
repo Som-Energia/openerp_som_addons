299dd723c8b64da4cb86ee773054f1917eb31697
Iker Paz Bustins <ikertaliga@gmail.com>
2023-12-19 14:27:32 +0100

    Nueva columna en polizas

---
+++ som_polissa/migrations/5.0.23.12.0/post-0001_afegir_columnes_autoconsum_i_provincies_cups.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+import logging
+from oopgrade.oopgrade import load_data, load_data_records
+
+
+def up(cursor, installed_version):
+    if not installed_version:
+        return
+
+    logger = logging.getLogger('openerp.migration')
+
+    ##UPDATAR UNA PART DE L'XML (POSAR LA ID)##
+    logger.info("Updating XMLs")
+    list_of_records = [
+        "view_giscedata_polissa_tree_inherit"
+    ]
+    load_data_records(
+        cursor, 'som_polissa', 'giscedata_polissa_view.xml', list_of_records, mode='update'
+    )
+    logger.info("XMLs succesfully updated.")
+
+
+def down(cursor, installed_version):
+    pass
+
+
+migrate = up
