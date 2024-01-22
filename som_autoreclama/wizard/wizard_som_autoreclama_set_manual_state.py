96887b0f72116501f08e84401b3ae398025795c5
Fran Cubero <francisco.cubero@somenergia.coop>
2024-01-22 17:29:22 +0100

    alow select only atc related to the polissa

--- som_autoreclama/wizard/wizard_som_autoreclama_set_manual_state.py
+++ som_autoreclama/wizard/wizard_som_autoreclama_set_manual_state.py
@@ -14,6 +14,18 @@ class WizardSomAutoreclamaSetManualState(osv.osv_memory):
             cursor, uid, "som_autoreclama", "workflow_" + namespace
         )[1]
 
+    def _get_associated_polissa_id(self, cursor, uid, context=None):
+        namespace = context.get("namespace", "atc")
+        if namespace == "atc":
+            item_ids = context.get("active_ids", [])
+            atc_obj = self.pool.get("giscedata.atc")
+            datas = atc_obj.read(cursor, uid, item_ids, ['polissa_id'])
+            return list(set([data['polissa_id'][0] for data in datas]))[0]
+        elif namespace == "polissa":
+            return context["active_ids"][0]
+        return None
+
+
     def assign_state(self, cursor, uid, ids, context=None):
         namespace = context.get("namespace", "atc")
         h_obj = self.pool.get("som.autoreclama.state.history." + namespace)
@@ -47,6 +59,7 @@ class WizardSomAutoreclamaSetManualState(osv.osv_memory):
     _columns = {
         "state": fields.selection([("init", "Init"), ("end", "End")], "State"),
         "info": fields.text("Informació", readonly=True),
+        "as_polissa_id": fields.many2one("giscedata.polissa"),
         "workflow_id": fields.many2one("som.autoreclama.state.workflow", "Fluxe"),
         "next_state_id": fields.many2one(
             "som.autoreclama.state",
@@ -57,6 +70,7 @@ class WizardSomAutoreclamaSetManualState(osv.osv_memory):
         "attached_atc": fields.many2one(
             "giscedata.atc",
             "Cas ATC associat",
+            domain="[('polissa_id', '=', as_polissa_id)]",
         ),
         "change_date": fields.date("Canvi de data"),
     }
@@ -65,6 +79,7 @@ class WizardSomAutoreclamaSetManualState(osv.osv_memory):
         "state": lambda *a: "init",
         "info": lambda *a: "",
         "workflow_id": _get_workflow_id,
+        "as_polissa_id": _get_associated_polissa_id,
     }
 
 
