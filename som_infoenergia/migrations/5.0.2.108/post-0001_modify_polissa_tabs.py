3534bdc2f8e8c6d87bf82cbb2faf878da701a12b
Oriol Piera <oriol.piera@somenergia.coop>
2024-01-29 13:15:21 +0100

    Add migration scripts

---
+++ som_infoenergia/migrations/5.0.2.108/post-0001_modify_polissa_tabs.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+import logging
+from oopgrade.oopgrade import load_data_records
+
+
+def up(cursor, installed_version):
+    if not installed_version:
+        return
+
+    logger = logging.getLogger('openerp.migration')
+    logger.info("Creating pooler")
+
+    ##UPATAR UN XML SENCER##
+    logger.info("Updating XML giscedata_polissa_view.xml")
+    load_data(
+        cursor, 'som_infoenergia', 'giscedata_polissa_view.xml', idref=None,
+        mode='update'
+    )
+    logger.info("XMLs succesfully updated.")
+
+
+def down(cursor, installed_version):
+    pass
+
+
+migrate = up
