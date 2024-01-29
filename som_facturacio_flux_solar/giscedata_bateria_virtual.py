76eb4c1d91510516963e0c7abfe08d5b06124d4d
Somenergia IT <itcrowd@somenergia.coop>
2024-01-29 14:56:01 +0100

    Commit PR 524

--- som_facturacio_flux_solar/giscedata_bateria_virtual.py
+++ som_facturacio_flux_solar/giscedata_bateria_virtual.py
@@ -1,5 +1,6 @@
 # -*- encoding: utf-8 -*-
 from osv import osv, fields
+from datetime import datetime
 
 
 class GiscedataBateriaVirtual(osv.osv):
@@ -47,7 +48,7 @@ class GiscedataBateriaVirtual(osv.osv):
         return res
 
     def _ff_data_app_descomptes(self, cursor, uid, bat_ids, name, args, context=None):
-        res = dict.fromkeys(bat_ids, "")
+        res = dict.fromkeys(bat_ids, {'data_inici_app_descomptes': 0, 'data_final_app_descomptes': 0})
         sql = ("SELECT bat.id, bat_pol.data_inici, bat_pol.data_final "
                "FROM giscedata_bateria_virtual_polissa bat_pol "
                "JOIN giscedata_bateria_virtual bat ON bat.id = bat_pol.bateria_id "
@@ -57,17 +58,46 @@ class GiscedataBateriaVirtual(osv.osv):
         res.update(aux)
         return res
 
+    def _ff_bateria_activa(self, cursor, uid, bat_ids, name, args, context=None):
+        if context is None:
+            context = {}
+        res = dict.fromkeys(bat_ids, False)
+        avui = datetime.today().strftime("%Y-%m-%d")
+        read_vals = ['data_inici_app_descomptes', 'data_final_app_descomptes']
+        for bat_id in bat_ids:
+            dates = self.read(cursor, uid, bat_id, read_vals, context=context)
+            if (dates['data_inici_app_descomptes'] <= avui and
+            (not dates['data_final_app_descomptes'] or dates['data_final_app_descomptes'] >= avui)):
+                res['bat_id'] = True
+        return res
+
+    def _activa_search(self, cursor, uid, obj, name, args, context=None):
+        if context is None:
+            context = {}
+
+        bat_pol_obj = self.pool.get('giscedata.bateria.virtual.polissa')
+
+        op = args[0][1]
+        value = args[0][2]
+        avui = datetime.today().strftime("%Y-%m-%d")
+        search_vals = [('data_inici', '<=', avui), '|', ('data_final', '=', False), ('data_final', '>=', avui)]
+        if (op == '=' and value) or (op == '!=' and not value):
+            operator = 'in'
+        else:
+            operator = 'not in'
+
+        bat_ids_aux = bat_pol_obj.q(cursor, uid).read(['bateria_id'], only_active=False).where(search_vals)
+        bat_ids = [x['bateria_id'] for x in bat_ids_aux]
+
+        return [('id', operator, bat_ids)]
+
     _columns = {
         'origen_info': fields.function(_ff_origen, type="text", method=True, string='Origen'),
         'receptor_info': fields.function(_ff_receptor, type="text", method=True, string='Receptor (pes)'),
         'data_inici_descomptes': fields.function(_ff_data_inici_descomptes, type="text", method=True, string='Data inici generació descomptes'),
-        'activa': fields.boolean('Activa'),
+        'activa': fields.function(_ff_bateria_activa, type="boolean", fnct_search=_activa_search, method=True, string='Activa'),  
         'data_inici_app_descomptes': fields.function(_ff_data_app_descomptes, type="text", method=True, string='Data inici aplicació descomptes', multi='data_app'),
         'data_final_app_descomptes': fields.function(_ff_data_app_descomptes, type="text", method=True, string='Data final aplicació descomptes', multi='data_app'),
-    }
-
-    _defaults = {
-        'activa': lambda *a: True
     }
 
 
