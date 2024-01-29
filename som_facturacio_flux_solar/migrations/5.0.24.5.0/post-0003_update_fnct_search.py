883f8a2f5660467d667cea25bffbedf5eb944eb8
aorellana <aorellana@gisce.net>
2024-01-29 15:00:06 +0100

    Add field to views

--- som_facturacio_flux_solar/migrations/5.0.24.5.0/post-0003_update_fnct_search.py
+++ som_facturacio_flux_solar/migrations/5.0.24.5.0/post-0003_update_fnct_search.py
@@ -18,6 +18,16 @@ def up(cursor, installed_version):
     pool.get("giscedata.bateria.virtual")._auto_init(cursor, context={'module': 'som_facturacio_flux_solar'})
     logger.info("Table created succesfully.")
 
+    ##UPDATAR UNA PART DE L'XML (POSAR LA ID)##
+    logger.info("Updating XMLs")
+    list_of_records = [
+        "view_som_bateria_virtual_form",
+    ]
+    load_data_records(
+        cursor, 'som_facturacio_flux_solar', 'giscedata_bateria_virtual.xml', list_of_records, mode='update'
+    )
+    logger.info("XMLs succesfully updated.")
+
 
 def down(cursor, installed_version):
     pass
