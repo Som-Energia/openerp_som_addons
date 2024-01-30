eb841d034b3c5cacb93c531e1a71f8b2a436e3a0
xavier <xavier.teres@somenergia.coop>
2024-01-30 14:46:26 +0100

    ✨🚧 (GURB): Add betas to som.gurb.cups

--- som_gurb/som_gurb_cups.py
+++ som_gurb/som_gurb_cups.py
@@ -11,6 +11,23 @@ class SomGurbCups(osv.osv):
     _name = "som.gurb.cups"
     _description = _('CUPS en grup de generació urbana')
 
+    def _ff_get_beta_percentage(self, cursor, uid, ids, field_name, arg, context=None):
+        gurb_obj = self.pool.get('som.gurb')
+        res = dict.fromkeys(ids, False)
+        for gurb_cups_vals in self.read(cursor, uid, ids, ['gurb_id', 'beta_kw']):
+            gurb_id = gurb_cups_vals.get('gurb_id', False)
+            if gurb_id:
+                generation_power = gurb_obj.read(
+                    cursor, uid, gurb_id[0], ['generation_power']
+                )['generation_power']
+
+                if generation_power:
+                    beta_kw = gurb_cups_vals.get('beta_kw', 0)
+                    res[gurb_cups_vals['id']] = beta_kw * 100 / generation_power
+                else:
+                    res[gurb_cups_vals['id']] = 0
+        return res
+
     # TODO: pensar
     def _ff_is_model_active(self, cursor, uid, ids, field_name, arg, context=None):
         if context is None:
@@ -34,7 +51,15 @@ class SomGurbCups(osv.osv):
         'start_date': fields.date(u"Data entrada GURB", required=True),
         'end_date': fields.date(u"Data sortida GURB",),
         'gurb_id': fields.many2one("som.gurb", "GURB", required=True),
-        'cups_id': fields.many2one('giscedata.cups.ps', 'CUPS'),
+        'cups_id': fields.many2one('giscedata.cups.ps', 'CUPS', required=True),
+        'beta_kw': fields.float('Beta (kW)', digits=(10, 3), required=True),
+        'beta_percentage': fields.function(
+            _ff_get_beta_percentage,
+            type='float',
+            digits=(10, 3),
+            string='Beta (%)',
+            method=True,
+        )
     }
 
     _defaults = {
