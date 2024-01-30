a99597874d9bfd1bffcef368ad0a039a97252901
xavier <xavier.teres@somenergia.coop>
2024-01-30 13:03:02 +0100

    ✨ (GURB): Add GURB Pilot contract category
    Co-authored-by: Daniel Estanyol i Torres <daniel.estanyol@somenergia.coop>

---
+++ som_gurb/__terp__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Generació urbana",
+    "description": "Mòdul per gestionar els grups de generació urbana",
+    "version": "0-dev",
+    "author": "SomEnergia",
+    "category": "SomEnergia",
+    "depends": [
+        "giscedata_polissa",
+    ],
+    "init_xml": [],
+    "demo_xml": [
+    ],
+    "update_xml": [
+        "som_gurb_data.xml",
+    ],
+    "active": False,
+    "installable": True,
+}
