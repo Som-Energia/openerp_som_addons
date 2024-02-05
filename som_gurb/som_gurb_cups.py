d9d3861310b35b89c51fab248785caeee663406a
xavier <xavier.teres@somenergia.coop>
2024-02-05 13:34:10 +0100

    ♻️ (GURB): Change beta field digits
    Co-authored-by: Daniel Estanyol i Torres <daniel.estanyol@somenergia.coop>

--- som_gurb/som_gurb_cups.py
+++ som_gurb/som_gurb_cups.py
@@ -4,28 +4,28 @@ from tools.translate import _
 import logging
 from datetime import datetime
 
-logger = logging.getLogger('openerp.{}'.format(__name__))
+logger = logging.getLogger("openerp.{}".format(__name__))
 
 
 class SomGurbCups(osv.osv):
     _name = "som.gurb.cups"
-    _description = _('CUPS en grup de generació urbana')
+    _description = _("CUPS en grup de generació urbana")
 
     def _ff_get_beta_percentage(self, cursor, uid, ids, field_name, arg, context=None):
-        gurb_obj = self.pool.get('som.gurb')
+        gurb_obj = self.pool.get("som.gurb")
         res = dict.fromkeys(ids, False)
-        for gurb_cups_vals in self.read(cursor, uid, ids, ['gurb_id', 'beta_kw']):
-            gurb_id = gurb_cups_vals.get('gurb_id', False)
+        for gurb_cups_vals in self.read(cursor, uid, ids, ["gurb_id", "beta_kw"]):
+            gurb_id = gurb_cups_vals.get("gurb_id", False)
             if gurb_id:
                 generation_power = gurb_obj.read(
-                    cursor, uid, gurb_id[0], ['generation_power']
-                )['generation_power']
+                    cursor, uid, gurb_id[0], ["generation_power"]
+                )["generation_power"]
 
                 if generation_power:
-                    beta_kw = gurb_cups_vals.get('beta_kw', 0)
-                    res[gurb_cups_vals['id']] = beta_kw * 100 / generation_power
+                    beta_kw = gurb_cups_vals.get("beta_kw", 0)
+                    res[gurb_cups_vals["id"]] = beta_kw * 100 / generation_power
                 else:
-                    res[gurb_cups_vals['id']] = 0
+                    res[gurb_cups_vals["id"]] = 0
         return res
 
     # TODO: pensar
@@ -34,10 +34,10 @@ class SomGurbCups(osv.osv):
             context = {}
         res = dict.fromkeys(ids, False)
         for cups_gurb_id in ids:
-            end_date = self.read(cursor, uid, cups_gurb_id, ['end_date'])['end_date']
+            end_date = self.read(cursor, uid, cups_gurb_id, ["end_date"])["end_date"]
             if end_date:
-                end_date_datetime = datetime.strptime(end_date, '%Y-%m-%d')
-                today_time = datetime.strptime(datetime.today().strftime('%Y-%m-%d'), '%Y-%m-%d')
+                end_date_datetime = datetime.strptime(end_date, "%Y-%m-%d")
+                today_time = datetime.strptime(datetime.today().strftime("%Y-%m-%d"), "%Y-%m-%d")
                 if today_time > end_date_datetime:
                     res[cups_gurb_id] = False
                 else:
@@ -47,23 +47,27 @@ class SomGurbCups(osv.osv):
         return res
 
     _columns = {
-        'active': fields.boolean('Actiu'),
-        'start_date': fields.date(u"Data entrada GURB", required=True),
-        'end_date': fields.date(u"Data sortida GURB",),
-        'gurb_id': fields.many2one("som.gurb", "GURB", required=True),
-        'cups_id': fields.many2one('giscedata.cups.ps', 'CUPS', required=True),
-        'beta_kw': fields.float('Beta (kW)', digits=(10, 3), required=True),
-        'beta_percentage': fields.function(
-            _ff_get_beta_percentage,
-            type='float',
+        "active": fields.boolean("Actiu"),
+        "start_date": fields.date(u"Data entrada GURB", required=True),
+        "end_date": fields.date(u"Data sortida GURB",),
+        "gurb_id": fields.many2one("som.gurb", "GURB", required=True),
+        "cups_id": fields.many2one("giscedata.cups.ps", "CUPS", required=True),
+        "beta_kw": fields.float(
+            "Beta (kW)",
             digits=(10, 3),
-            string='Beta (%)',
+            required=True,
+        ),
+        "beta_percentage": fields.function(
+            _ff_get_beta_percentage,
+            type="float",
+            string="Beta (%)",
+            digits=(12, 4),
             method=True,
         )
     }
 
     _defaults = {
-        'active': lambda *a: True,
+        "active": lambda *a: True,
     }
 
 
