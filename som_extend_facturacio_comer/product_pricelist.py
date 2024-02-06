ea5cde5e41a7423b2ef196d2b5d59bef10596b4a
pauboixsom <pau.boix@somenergia.coop>
2024-02-06 13:24:36 +0100

    Fix get generation
    Co-authored-by: Oriol Piera <oriol.piera@somenergia.coop>

--- som_extend_facturacio_comer/product_pricelist.py
+++ som_extend_facturacio_comer/product_pricelist.py
@@ -1,13 +1,16 @@
 # -*- coding: utf-8 -*-
 from osv import osv
-from som_extend_facturacio_comer.utils import get_gkwh_atr_price
+from .utils import get_gkwh_atr_price
 
 
 class ProductPricelist(osv.osv):
     """Llistes de preus.
     """
-    _name = 'product.pricelist'
     _inherit = 'product.pricelist'
 
-    def get_gkwh_atr_price(cursor, uid, polissa, period_name, context, with_taxes=False):
+    def get_gkwh_atr_price(self, cursor, uid, polissa_id, period_name, context=None, with_taxes=False):
+        pol_obj = self.pool.get('giscedata.polissa')
+        polissa = pol_obj.browse(cursor, uid, polissa_id)
         return get_gkwh_atr_price(cursor, uid, polissa, period_name, context, with_taxes)
+
+ProductPricelist()
