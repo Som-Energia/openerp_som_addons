a671e69d1db942670d45eee58eaa7f230c70ef39
xavier <xavier.teres@somenergia.coop>
2024-02-13 14:20:56 +0100

    ♻️ 🧹💄 (GURB): Make code more readable

--- som_gurb/giscedata_switching.py
+++ som_gurb/giscedata_switching.py
@@ -24,35 +24,27 @@ class GiscedataSwitching(osv.osv):
         ir_model_obj = self.pool.get("ir.model.data")
 
         sw = sw_obj.browse(cursor, uid, sw_id, context=context)
-
-        avoid_cancel = True
-        if not sw or sw.proces_id.name not in _GURB_CANCEL_CASES:
-            avoid_cancel = False
-
-        if sw.step_id.name not in _GURB_CANCEL_CASES[sw.proces_id.name]:
-            avoid_cancel = False
-
-        pol_id = sw_obj.read(
-            cursor, uid, sw_id, ["cups_polissa_id"], context=context
-        )["cups_polissa_id"][0]
-
-        # TODO: Use the real category
-        gurb_categ_id = ir_model_obj.get_object_reference(
-            cursor, uid, "som_gurb", "categ_gurb_pilot"
-        )[1]
-
+        pol_id = sw.cups_polissa_id.id
         pol_category_ids = pol_obj.read(cursor, uid, pol_id, ["category_id"])["category_id"]
 
-        if not avoid_cancel or gurb_categ_id not in pol_category_ids:
+        gurb_categ_id = ir_model_obj.get_object_reference(
+            cursor, uid, "som_gurb", "categ_gurb_pilot"  # TODO: Use the real category
+        )[1]
+
+        if (
+            sw
+            and sw.proces_id.name in _GURB_CANCEL_CASES
+            and sw.step_id.name in _GURB_CANCEL_CASES[sw.proces_id.name]
+            and gurb_categ_id in pol_category_ids
+        ):
+            msg = _("Cas cancel·lat per GURB")
+            self.historize_msg(cursor, uid, sw.id, msg, context=context)
+            sw_obj.write(cursor, uid, sw_id, {"state": "cancel"}, context=context)
+            return _("Cas importat correctament.")
+        else:
             return super(GiscedataSwitching, self).importar_xml_post_hook(
                 cursor, uid, sw_id, context=context
             )
-
-        msg = _("Cas cancel·lat per GURB")
-        self.historize_msg(cursor, uid, sw.id, msg, context=context)
-        sw_obj.write(cursor, uid, sw_id, {"state": "cancel"}, context=context)
-
-        return _("Cas importat correctament.")
 
 
 GiscedataSwitching()
