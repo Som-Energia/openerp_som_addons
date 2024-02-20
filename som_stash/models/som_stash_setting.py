5475c1db7c3edb7a2203b66236a274be7df96aed
pedropote <pere.montagud@somenergia.coop>
2024-02-20 11:19:23 +0100

    auxiliary fields as fields function
    Co-authored-by: Fran Cubero <francisco.cubero@somenergia.coop>

--- som_stash/models/som_stash_setting.py
+++ som_stash/models/som_stash_setting.py
@@ -7,17 +7,28 @@ from som_stash import SELECTABLE_MODELS_LIST, SELECTABLE_TYPES_LIST
 class SomStashSetting(osv.osv):
     _name = 'som.stash.setting'
 
+    def _ff_get_allowed(self, cursor, uid, ids, name, args, context=None):
+        res = {}
+        for val in self.browse(cursor, uid, ids, context=context):
+            res[val.id] = {
+                'allowed_models': SELECTABLE_MODELS_LIST,
+                'allowed_types': SELECTABLE_TYPES_LIST,
+            }
+        return res
+
     _columns = {
-        'allowed_models': fields.text('allowed models'),
-        'allowed_types': fields.text('allowed types'),
+        'allowed_models': fields.function(
+            _ff_get_allowed, type="char", method=True, string="Allowed models", multi=True
+        ),
+        'allowed_types': fields.function(
+            _ff_get_allowed, type="char", method=True, string="Allowed types", multi=True
+        ),
         'model': fields.many2one('ir.model', 'Model'),
         'field': fields.many2one('ir.model.fields', 'Camp'),
         'default_stashed_value': fields.text(_('Valor per defecte')),
     }
 
     _defaults = {
-        "allowed_models": lambda *a: SELECTABLE_MODELS_LIST[:],
-        "allowed_types": lambda *a: SELECTABLE_TYPES_LIST[:],
     }
 
     _sql_constraints = [
