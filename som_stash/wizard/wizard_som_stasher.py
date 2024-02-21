a6808c3b0be8ee77d693a4fd0a85d988d65830f3
Fran Cubero <francisco.cubero@somenergia.coop>
2024-02-21 13:14:13 +0100

    WIP stasher main logic
    Co-authored-by: Pere Montagud Ferragud <pere.montagud@somenergia.coop>

--- som_stash/wizard/wizard_som_stasher.py
+++ som_stash/wizard/wizard_som_stasher.py
@@ -3,6 +3,7 @@ from osv import fields, osv
 from tools.translate import _
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
+import json
 
 
 class WizardSomStasher(osv.osv_memory):
@@ -31,6 +32,7 @@ class WizardSomStasher(osv.osv_memory):
             where
             state = 'baixa'
             and data_baixa <= %s
+            and titular is not null
             and not exists
             (   select gp2.titular
                 from giscedata_polissa gp2
@@ -60,11 +62,7 @@ class WizardSomStasher(osv.osv_memory):
         date_limit = self.get_date_limit(cursor, uid, context=context)
         patrner_ids = self.get_partners_inactive_soci_before_datelimit(cursor, uid, date_limit)
         patrner_ids += self.get_partners_inactive_pol_before_datelimit(cursor, uid, date_limit)
-        res = list(set(patrner_ids))
-        return res
-
-    def get_partners(self, cursor, uid, partner_ids, context=None):
-        res = [('res.partner', partner_id) for partner_id in partner_ids]
+        res = sorted(list(set(patrner_ids)))
         return res
 
     def get_partners_address(self, cursor, uid, partner_ids, context=None):
@@ -72,47 +70,78 @@ class WizardSomStasher(osv.osv_memory):
         to_stash_ids = obj.search(cursor, uid, [
             ('partner_id', 'in', partner_ids),
         ])
-        res = [('res.partner.address', id) for id in to_stash_ids]
-        return res
+        return to_stash_ids
 
     def get_polisses(self, cursor, uid, partner_ids, context=None):
         obj = self.pool.get("giscedata.polissa")
         to_stash_ids = obj.search(cursor, uid, [
             ('titular', 'in', partner_ids),
         ])
-        res = [('giscedata.polissa', id) for id in to_stash_ids]
-        return res
+        return to_stash_ids
+
+    def do_stash_model(self, cursor, uid, ids, model, context=None):
+        # obtenim els valors a fer stash del model
+        som_stash_setting_obj = self.pool.get("som.stash.setting")
+        stash_setting_ids = som_stash_setting_obj.search(cursor, uid, [('model', '=', model)])
+
+        # montem diccionari per fer el write i llista per al read
+        dict_write = {}
+        list_fields_read = []
+        som_stash_obj = self.pool.get("som.stash")
+        for setting in som_stash_setting_obj.browse(cursor, uid, stash_setting_ids):
+            dict_write[setting.field.name] = setting.default_stashed_value
+            # {'email': 'a@a.com', 'phone': ''}
+            list_fields_read.append(setting.field.name)
+            # ['email', 'phone']
+
+        # bkp de valor actuals
+        model_obj = self.pool.get(model)
+        for id in ids:
+            # read obté -> {'email': hola@hola.com, 'phone': '666332111'}
+            dict_data = model_obj.read(cursor, uid, [id], list_fields_read)[0]
+            json_data = json.dumps(dict_data)
+            value_origin = "{},{}".format(model, str(id))
+            # origin = res.partner,3
+            # comprove si ja està el valor
+            if len(som_stash_obj.search(cursor, uid, [('origin', '=', value_origin)])) > 0:
+                som_stash_obj.read()
+                # upodatar diccionari amb els camps nous
+            else:
+                som_stash_obj.create()
+
+            print json_data
+        # fem el write
+        model_obj.write(cursor, uid, ids, dict_write)
 
     def do_stash_process(self, cursor, uid, ids, context=None):
-        # variables temporals
-        # years = 6
-        som_stash_obj = self.pool.get("som.stash")
-        # som_stash_setting_obj = self.pool.get("som.stash.setting")
-        models_to_stash = som_stash_obj._get_selectable_models_list(cursor, uid, context=context)
-
         msg = _("Resultat d'execució del wizard de backup de dades:\n")
         do_stash = self.read(
             cursor, uid, ids, ['do_stash'], context=context
         )[0]['do_stash']
 
-        # explore models to get the data to be stashed
-        if do_stash:
-            # partners_to_stash = self.get_partners_origin_to_stash(cursor, uid, context=context)
-            # # listtup [('model', id), ...]
-            # listtup_partners = self.get_partners(
-            #     cursor, uid, partners_to_stash, context=context
-            # )
-            # listtup_partners_address = self.get_partners_address(
-            #     cursor, uid, partners_to_stash, context=context
-            # )
-            # listtup_polisses = self.get_polisses(
-            #     cursor, uid, partners_to_stash, context=context
-            # )
+        partners_to_stash = self.get_partners_origin_to_stash(cursor, uid, context=context)
+        msg += _(
+            "Trobats {} partners per fer backup.\nLlista d'Ids:\n{}".format(
+                len(partners_to_stash),
+                ', '.join([str(i) for i in partners_to_stash if i])
+            )
+        )
 
-            # do the stash process
-            for model in models_to_stash:
-                pass
-                # stash_setting_fields = som_stash_setting_obj.search
+        # do not commit
+        partners_to_stash = partners_to_stash[:10]
+
+        if do_stash:
+            self.do_stash_model(cursor, uid, 'res.partner', partners_to_stash)
+
+            list_partners_address_ids = self.get_partners_address(
+                cursor, uid, partners_to_stash, context=context
+            )
+            self.do_stash_model(cursor, uid, 'res.partner.address', list_partners_address_ids)
+
+            list_pol_ids = self.get_polisses(
+                cursor, uid, partners_to_stash, context=context
+            )
+            self.do_stash_model(cursor, uid, 'giscedata.polissa', list_pol_ids)
 
         self.write(
             cursor, uid, ids, {'info': msg}
