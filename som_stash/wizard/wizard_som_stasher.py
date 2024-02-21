2d3dc47d2cd185eb7e727587ea70db6b52d2cf41
pedropote <pere.montagud@somenergia.coop>
2024-02-21 11:05:03 +0100

    fix flake8 issues
    Co-authored-by: Fran Cubero <francisco.cubero@somenergia.coop>

--- som_stash/wizard/wizard_som_stasher.py
+++ som_stash/wizard/wizard_som_stasher.py
@@ -1,11 +1,87 @@
-# -*- coding: utf-8 -*-
+# -*- encoding: utf-8 -*-
 from osv import fields, osv
 from tools.translate import _
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
 
 
 class WizardSomStasher(osv.osv_memory):
     """Wizard to automate the stashing action"""
     _name = 'wizard.som.stasher'
+
+    def _get_default_years(self, cursor, uid, context=None):
+        cfg_obj = self.pool.get('res.config')
+        return int(
+            cfg_obj.get(cursor, uid, "som_stash_years_ago", "6")
+        )
+
+    def get_date_limit(self, cursor, uid, context=None):
+        years_ago = self._get_default_years(cursor, uid, context)
+        date_limit = datetime.today() - relativedelta(years=years_ago)
+        return date_limit
+
+    def get_partners_inactive_pol_before_datelimit(self, cursor, uid, date_limit, context=None):
+        str_date_limit = datetime.strftime(date_limit, '%Y-%m-%d')
+
+        sql_array = """
+            select
+            array_agg(gp.titular) as partner_ids
+            --gp.titular
+            from giscedata_polissa gp
+            where
+            state = 'baixa'
+            and data_baixa <= %s
+            and not exists
+            (   select gp2.titular
+                from giscedata_polissa gp2
+                where (gp2.state = 'activa' or (gp2.state = 'baixa' and gp2.data_baixa > %s))
+                and gp2.titular = gp.titular
+            )
+        """
+        cursor.execute(sql_array, (str_date_limit, str_date_limit))
+        res = cursor.dictfetchone()["partner_ids"]
+        return res or []
+
+    def get_partners_inactive_soci_before_datelimit(self, cursor, uid, date_limit, context=None):
+        str_date_limit = datetime.strftime(date_limit, '%Y-%m-%d')
+
+        sql_array = """
+            select
+            array_agg(partner_id) as partner_ids
+            --partner_id
+            from somenergia_soci ss
+            where ss.baixa=true and ss.data_baixa_soci <= %s
+        """
+        cursor.execute(sql_array, (str_date_limit,))
+        res = cursor.dictfetchone()["partner_ids"]
+        return res or []
+
+    def get_partners_origin_to_stash(self, cursor, uid, context=None):
+        date_limit = self.get_date_limit(cursor, uid, context=context)
+        patrner_ids = self.get_partners_inactive_soci_before_datelimit(cursor, uid, date_limit)
+        patrner_ids += self.get_partners_inactive_pol_before_datelimit(cursor, uid, date_limit)
+        res = list(set(patrner_ids))
+        return res
+
+    def get_partners(self, cursor, uid, partner_ids, context=None):
+        res = [('res.partner', partner_id) for partner_id in partner_ids]
+        return res
+
+    def get_partners_address(self, cursor, uid, partner_ids, context=None):
+        obj = self.pool.get("res.partner.address")
+        to_stash_ids = obj.search(cursor, uid, [
+            ('partner_id', 'in', partner_ids),
+        ])
+        res = [('res.partner.address', id) for id in to_stash_ids]
+        return res
+
+    def get_polisses(self, cursor, uid, partner_ids, context=None):
+        obj = self.pool.get("giscedata.polissa")
+        to_stash_ids = obj.search(cursor, uid, [
+            ('titular', 'in', partner_ids),
+        ])
+        res = [('giscedata.polissa', id) for id in to_stash_ids]
+        return res
 
     def do_stash_process(self, cursor, uid, ids, context=None):
         # variables temporals
@@ -21,7 +97,18 @@ class WizardSomStasher(osv.osv_memory):
 
         # explore models to get the data to be stashed
         if do_stash:
-            pass
+            # partners_to_stash = self.get_partners_origin_to_stash(cursor, uid, context=context)
+            # # listtup [('model', id), ...]
+            # listtup_partners = self.get_partners(
+            #     cursor, uid, partners_to_stash, context=context
+            # )
+            # listtup_partners_address = self.get_partners_address(
+            #     cursor, uid, partners_to_stash, context=context
+            # )
+            # listtup_polisses = self.get_polisses(
+            #     cursor, uid, partners_to_stash, context=context
+            # )
+
             # do the stash process
             for model in models_to_stash:
                 pass
@@ -37,11 +124,13 @@ class WizardSomStasher(osv.osv_memory):
             'Modifica',
             help=_('Si la casella esta marcada, es realitzaran canvis als '
                    'regsitres trobats')
-        )
+        ),
+        'years': fields.integer(_("Antiguitat en anys")),
     }
 
     _defaults = {
         "do_stash": lambda *a: False,
+        "years": _get_default_years,
     }
 
 
