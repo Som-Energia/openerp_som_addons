426dfab75dce4eed1bbbb9e787e9af4b64cba26a
pedropote <pere.montagud@somenergia.coop>
2024-02-22 15:39:38 +0100

    Extend som_stash model
    Co-authored-by: Fran Cubero <francisco.cubero@somenergia.coop>

--- som_stash/models/som_stash.py
+++ som_stash/models/som_stash.py
@@ -1,7 +1,6 @@
 # -*- encoding: utf-8 -*-
 from osv import fields, osv
 from tools.translate import _
-import json
 
 
 SELECTABLE_MODELS = [
@@ -20,62 +19,20 @@ class SomStash(osv.osv):
     def _get_selectable_models_list(self, cursor, uid, context=None):
         return SELECTABLE_MODELS_LIST
 
-    def _ff_data_text(self, cursor, uid, ids, field_name, arg, context=None):
-        if not context:
-            context = {}
-
-        res = {}
-        for import_vals in self.read(cursor, uid, ids, ["data"]):
-            res[import_vals["id"]] = json.dumps(import_vals["data"], indent=4)
-        return res
-
-    def _fi_data_text(self, cursor, uid, ids, name, value, arg, context=None):
-        if not context:
-            context = {}
-
-        try:
-            parameters = json.loads(value)
-            self.write(cursor, uid, ids, {"data": parameters})
-        except ValueError:
-            pass
-
-    def check_correct_json(self, cursor, uid, ids, data):
-        try:
-            params = json.loads(data)
-        except ValueError:
-            return {
-                "warning": {
-                    "title": _(u"Atenció!"),
-                    "message": _("Els parametres entrats no tenen un format "
-                                 "correcte de JSON"),
-                }
-            }
-        if not isinstance(params, dict):
-            return {
-                "warning": {
-                    "title": _(u"Atenció"),
-                    "message": _("Els parametres han de ser un diccionari"),
-                }
-            }
-        return {}
-
     _columns = {
-        'origin': fields.reference('Origen', selection=SELECTABLE_MODELS, size=128),
-        'data': fields.json('Dades'),
-        'data_text': fields.function(
-            _ff_data_text,
-            type="text",
-            method=True,
-            string=_("Dades"),
-            fnct_inv=_fi_data_text,
-        ),
+        'origin': fields.reference(_('Origen'), selection=SELECTABLE_MODELS, size=128),
+        'res_field': fields.char(_('Camp'), size=64),
+        'res_id': fields.integer(_('ID')),
+        'value': fields.text(_('Valor')),
+        'date_stashed': fields.datetime(_('Data backup')),
     }
 
     _defaults = {
     }
 
     _sql_constraints = [
-        ('origin_uniq', 'unique(origin)', 'You cannot an origin more than once'),
+        ('origin_field_uniq', 'unique(origin, res_field)',
+         'You cannot have an origin and field more than once'),
     ]
 
 
