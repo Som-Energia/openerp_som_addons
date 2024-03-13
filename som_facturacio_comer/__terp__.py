e17822bddf31141ccbb4c01f28b80990a4e672ee
pauboixsom <pau.boix@somenergia.coop>
2024-03-13 15:20:03 +0100

    Remove demo from terp
    Co-authored-by: Daniel Estanyol i Torres <daniel.estanyol@somenergia.coop>

--- som_facturacio_comer/__terp__.py
+++ som_facturacio_comer/__terp__.py
@@ -19,9 +19,7 @@
         "giscedata_facturacio_impagat_comer",
     ],
     "init_xml": [],
-    "demo_xml": [
-        "demo/som_facturacio_comer_demo.xml"
-    ],
+    "demo_xml": [],
     "update_xml": [
         "data/res_config_data.xml",
         "giscedata_facturacio_validation_data.xml",
