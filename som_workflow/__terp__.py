0ff155558b071e5cc9906f80a233ca44548e60c5
xavier <xavier.teres@somenergia.coop>
2024-03-14 10:46:36 +0100

    Add workflow exercise

---
+++ som_workflow/__terp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "OpenERP workflow example",
+    "description": "Simple OpenERP workflow example",
+    "version": "0-dev",
+    "author": "SomEnergia",
+    "depends": [
+        "base",
+    ],
+    "init_xml": [
+    ],
+    "demo_xml": [
+    ],
+    "update_xml": [
+        "views/example_views.xml",
+        "workflow/example_model_workflow.xml",
+    ],
+    "active": False,
+    "installable": True,
+}
