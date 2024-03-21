d651695405a7e6ce897b769d5cc7be6fb8e59233
Àngel Fernández <angel.fernandez@somenergia.coop>
2024-03-21 16:36:08 +0100

    F#*k Python 2

--- som_generationkwh/res_partner.py
+++ som_generationkwh/res_partner.py
@@ -160,12 +160,15 @@ class ResPartner(osv.osv):
 
     @staticmethod
     def _prepare_datetime_value_www_response(dict_with_data):
+        # https://stackoverflow.com/questions/8777753/converting-datetime-date-to-utc-timestamp-in-python/8778548#8778548
         cet = pytz.timezone('CET')
+        utc = pytz.timezone('UTC')
         return [
             {
-                'date': int(
-                    datetime.strptime(k, '%Y-%m-%d %H:%M:%S').replace(tzinfo=cet).timestamp()
-                )*1000,  # javascript works with 3 more 0 than python
+                'date': (
+                    datetime.strptime(k, '%Y-%m-%d %H:%M:%S').replace(tzinfo=cet)
+                    - datetime(1970, 1, 1, tzinfo=utc)
+                ).total_seconds()*1000,  # javascript works with 3 more 0 than python
                 'value': dict_with_data[k]
             } for k in sorted(dict_with_data)
         ]
