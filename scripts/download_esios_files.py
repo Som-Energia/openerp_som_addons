951781fc81621bb5e85cf26945b24c94ec51c925
Oriol Piera <oriol.piera@somenergia.coop>
2024-03-26 10:05:04 +0100

    First try to find A2_liquicomun and after A1_liquicomun. No other file is downloaded

--- scripts/download_esios_files.py
+++ scripts/download_esios_files.py
@@ -32,7 +32,9 @@ import os
 from time import sleep
 
 FILE_TYPE = 'liquicomun'
+FILE_PREFERENCE = ['A2_liquicomun', 'A1_liquicomun']
 BIN_PATH = '/home/erp/ConnectionKit/bin'  # /bin folder of the ConnectionKit path
+REGEX = r'\s+([0-9]+)\s+([^ ]*)\s+[0-9]{2}-[0-9]{2}-[0-9]{4} [0-9]{2}:[0-9]{2} - [0-9]{2}-[0-9]{2}-[0-9]{4} [0-9]{2}:[0-9]{2} [0-9]{2}-[0-9]{2}-[0-9]{4} [0-9]{2}:[0-9]{2}:[0-9]{2}\s+([^ ]*)\s+([^ ]*)'  # noqa: E501
 
 
 def get_search_day():
@@ -51,27 +53,38 @@ def download_files(file_type, server, server_port):
     output = Popen(
         ['./list.sh', '-startTime', day_of_search, '-intervalType', 'Application'],
         cwd=BIN_PATH, stdout=PIPE, stderr=STDOUT)
+
+    # Search for liquicomuns
     result = []
     errors = []
     for line in output.stdout:
-        if FILE_TYPE in line:
+        if file_type in line:
             result.append(line)
         else:
             errors.append(line)
-    if len(result) == 1:
-        code, filename = re.match(r'\s+([0-9]+)\s+([^ ]*)', result[0]).groups()
-    elif len(result) == 0:
+
+    # Get preferred file
+    file_list = []
+    preferred_file = None
+    for res in result:
+        file_tuple = re.match(REGEX, res).groups()
+        if (file_tuple[2] == FILE_PREFERENCE[0] or (file_tuple[2] == FILE_PREFERENCE[1]
+                                                    and preferred_file is None)):
+            preferred_file = (file_tuple[0], file_tuple[1])
+            file_list.append(file_tuple[1])
+
+    if len(result) == 0:
         error("No files avaiables")
         print errors
         return 0
-    elif len(result) > 1:
-        step("Found multiple files...")
-        file_list = []
-        for res in result:
-            file_list.append(re.match(r'\s+([0-9]+)\s+([^ ]*)', res).groups())
-        file_list.sort(key=lambda a: a[1], reverse=True)
-        code = file_list[0][0]
-        filename = file_list[0][1]
+    elif len(file_list) == 0 or preferred_file is None:
+        error("No A2_liquicomun or A1_liquicomun files avaiables")
+        print result
+        return 0
+    elif preferred_file:
+        step("Found file...")
+        code = preferred_file[0]
+        filename = preferred_file[1]
 
     step("Downloading {}...".format(filename))
     output = Popen(
@@ -83,7 +96,7 @@ def download_files(file_type, server, server_port):
         if not timeout:
             error("Timeout downloading file {}".format(filename))
             return 0
-        sleep(5)
+        sleep(15)
         timeout -= 1
 
     step("Coping file to the server {}...".format(server))
