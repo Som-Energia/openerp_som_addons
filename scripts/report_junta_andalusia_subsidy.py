ec4dd2ec9c3f0de6ba33e6c78d2f978b4e6decbf
Oriol Piera <oriol.piera@somenergia.coop>
2024-03-28 11:48:02 +0100

    Support special characters in name and direccio

--- scripts/report_junta_andalusia_subsidy.py
+++ scripts/report_junta_andalusia_subsidy.py
@@ -24,38 +24,29 @@ from erppeek import Client
 
 csv_reader = False
 
-
-def ooop2erppeek(ooop):
-    return dict(
-        server='{uri}:{port}'.format(**ooop),
-        db=ooop['dbname'],
-        user=ooop['user'],
-        password=ooop['pwd'],
-    )
-
-
 data_dict_list = []
 output = []
 
 # Read CSV
-with open('entrada_4.csv', mode='r') as file:
+with open('input_vat.csv', mode='r') as file:
     csv_reader = csv.DictReader(file)
 
     for row in csv_reader:
         data_dict_list.append(row)
 
 # Connect to ERP
-c = Client(**ooop2erppeek(configdb.erppeek))
+c = Client(**configdb.erppeek)
 
 pol_obj = c.model('giscedata.polissa')
 fact_obj = c.model('giscedata.facturacio.factura')
 
 for data_dict in data_dict_list:
+    vat = data_dict['vat']
     pol_ids = pol_obj.search(
-        [('titular_nif', 'like', '%' + data_dict['nif'])],
+        [('titular_nif', 'like', '%' + vat)],
         context={'active_test': False},
     )
-    print("NIF {}: {} contracts".format(data_dict['nif'], len(pol_ids)))
+    print("NIF {}: {} contracts".format(vat, len(pol_ids)))
     polisses = pol_obj.browse(pol_ids)
     for pol in polisses:
         fact_ids = fact_obj.search([
@@ -64,16 +55,16 @@ for data_dict in data_dict_list:
             ('data_final', '>=', '2021-01-01'),
             ('data_inici', '<=', '2022-12-31'),
         ])
-        print("    NIF {}: POL {}: {} factures".format(data_dict['nif'], pol.name, len(fact_ids)))
+        print("    NIF {}: POL {}: {} factures".format(vat, pol.name, len(fact_ids)))
         factures = fact_obj.browse(fact_ids)
         for fact in factures:
             fact_line = OrderedDict()
             fact_line['tipo_registro_2'] = '2'
-            fact_line['nif'] = data_dict['nif']
-            fact_line['nombre'] = data_dict['nombre']
+            fact_line['nif'] = vat
+            fact_line['nombre'] = data_dict['name'].encode('utf-8')
             fact_line['contrato'] = pol.name
             fact_line['tipo_via'] = ''
-            fact_line['direccio'] = pol.cups_direccio
+            fact_line['direccio'] = pol.cups_direccio.encode('utf-8')
             fact_line['tipo_numeracion'] = 'NUM'
             fact_line['numero'] = ''
             fact_line['calificador_numero'] = ''
@@ -99,7 +90,6 @@ for data_dict in data_dict_list:
             fact_line['dias'] = fact.dies
             fact_line['pagado'] = fact.state
             fact_line['fecha_pago'] = fact.date_due
-            fact_line['consumo_facturado'] = fact.energia_kwh
             if not fact.lectures_energia_ids:
                 fact_line['tipo_lectura'] = ''
             else:
@@ -110,6 +100,7 @@ for data_dict in data_dict_list:
             signe = 1
             if fact.type == 'out_refund':
                 signe = -1
+            fact_line['consumo_facturado'] = fact.energia_kwh * signe
             fact_line['importe_energia'] = (fact.amount_total - (
                 fact.total_altres + fact.total_lloguers + fact.amount_tax)) * signe
             fact_line['importe_servicios'] = (fact.total_altres + fact.total_lloguers) * signe
@@ -117,13 +108,14 @@ for data_dict in data_dict_list:
             fact_line['total_factura'] = fact.amount_total * signe
 
             fact_line['tipo_registro_4'] = '4'
-            fact_line['nif_sol'] = data_dict['nif']
-            fact_line['nombre_sol'] = data_dict['nombre']
+            fact_line['nif_sol'] = vat
+            fact_line['nombre_sol'] = data_dict['name'].encode('utf-8')
             fact_line['codigo_error'] = ''
             fact_line['motivo_error'] = ''
             output.append(fact_line)
+    # break
 
-with open('output_4_fix.csv', 'w') as csv_file:
+with open('output_data.csv', 'w') as csv_file:
     escritor_csv = csv.DictWriter(csv_file, fieldnames=output[0].keys(), delimiter=';')
     escritor_csv.writeheader()
 
