9a32372a8c8c90f6ff2a569ef7809de897f1e454
Oriol Piera <oriol.piera@somenergia.coop>
2024-04-10 17:09:38 +0200

    New module som_consulta_pobresa

---
+++ som_consulta_pobresa/README.md
@@ -0,0 +1,3 @@
+# no_som_pobresa
+
+Mòdul per gestionar les peticions de pobresa energètica dels contractes amb factures impades, als ajuntaments
---
+++ som_consulta_pobresa/models/som_consulta_pobresa.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+from osv import osv, fields
+from tools.translate import _
+
+RESOLUTION_STATES = [
+    ('positiva', 'Positiva'),
+    ('negaviva', 'Negativa'),
+]
+
+
+class SomConsultaPobresa(osv.osv):
+    _name = 'som.consulta.pobresa'
+    _inherits = {"crm.case": "crm_id"}
+    _description = 'Model per gestionar les consultes de pobresa energètica'
+
+    def _ff_get_titular(self, cr, uid, ids, field, arg, context=None):
+        """ Anem a buscar el titular de la pólissa assignada (si en té) """
+        res = {}
+        if not context:
+            context = {}
+        scp_obj = self.pool.get('som.consulta.pobresa')
+        consultes = scp_obj.browse(cr, uid, ids)
+        for c in consultes:
+            res[c.id] = (c.polissa_id and c.polissa_id.titular.name
+                         or False)
+        return res
+
+    _columns = {
+        'crm_id': fields.many2one('crm.case', required=True),
+        'polissa_id': fields.many2one('giscedata.polissa',
+                                      _('Contracte'), required=True),
+        'partner_id': fields.function(_ff_get_titular, method=True, string='Titular'),
+        'municipi': fields.char("Número de registre", size=128),  # _ff ?
+        'numero_registre': fields.char("Número de registre", size=128),
+        'agrupacio_supramunicipal': fields.char("Número de registre", size=128),
+        'direccio_cups': fields.char("Direcció CUPS", size=128),  # _ff ?
+        'email_partner': fields.char("Email titular", size=128),  # _ff ?
+        'resolucio': fields.selection(RESOLUTION_STATES, 'Resolució', size=16),
+    }
+    _order = "id desc"
+
+
+SomConsultaPobresa()
