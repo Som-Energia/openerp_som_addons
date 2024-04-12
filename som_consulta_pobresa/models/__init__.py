c45ec49102597bfd6e4c75ad9bface65c2c5440f
Oriol Piera <oriol.piera@somenergia.coop>
2024-04-12 07:18:16 +0200

    🧹📝 Clean unused folders. Fix py3 imports. Add documentation.

--- som_consulta_pobresa/models/__init__.py
+++ som_consulta_pobresa/models/__init__.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
-
+from __future__ import absolute_import
 from . import agrupacio_supramunicipal
 from . import som_consulta_pobresa
--- som_consulta_pobresa/tests/__init__.py
+++ som_consulta_pobresa/tests/__init__.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
-
-from test_som_consulta_pobresa import *
-from test_wizard_create_som_consulta_pobresa import *
+from __future__ import absolute_import
+from .test_som_consulta_pobresa import *
+from .test_wizard_create_som_consulta_pobresa import *
