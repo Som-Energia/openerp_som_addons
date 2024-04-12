fbde7ed37e7065fd2e978ab614d40cafeee9c823
Oriol Piera <oriol.piera@somenergia.coop>
2024-04-12 07:02:20 +0200

    Improve coverage

--- som_consulta_pobresa/tests/test_wizard_create_som_consulta_pobresa.py
+++ som_consulta_pobresa/tests/test_wizard_create_som_consulta_pobresa.py
@@ -19,14 +19,20 @@ class TestWizardCrearConsultaPobresa(testing.OOTestCase):
         cons_obj = self.openerp.pool.get("som.consulta.pobresa")
         wiz_obj = self.openerp.pool.get("wizard.crear.consulta.pobresa")
         imd_obj = self.openerp.pool.get('ir.model.data')
+        pol_obj = self.openerp.pool.get('giscedata.polissa')
         pol_id = imd_obj.get_object_reference(
             cursor, uid, "giscedata_polissa", "polissa_0002"
         )[1]
         context = {"active_ids": [pol_id], "active_id": pol_id}
         wiz_id = wiz_obj.create(cursor, uid, {}, context=context)
-        wiz_obj.crear_consulta_pobresa(cursor, uid, wiz_id, context=context)
+
+        result = wiz_obj.crear_consulta_pobresa(cursor, uid, wiz_id, context=context)
 
         cons_list = cons_obj.search(cursor, uid, [('polissa_id', '=', pol_id)])
-
         cons_data = cons_obj.browse(cursor, uid, cons_list[0])
-        self.assertEqual(cons_data['name'], u'[0002] Camptocamp (Alegr\xeda-Dulantzi)')
+        pol = pol_obj.browse(cursor, uid, pol_id)
+        self.assertTrue(str(cons_list) in result['domain'])
+        self.assertEqual(cons_data.name, u'[0002] Camptocamp (Alegr\xeda-Dulantzi)')
+        self.assertEqual(cons_data.titular_id, u'Camptocamp')
+        self.assertEqual(cons_data.direccio_cups, pol.cups.direccio)
+        # self.assertEqual(cons_data.email_partner, polissa_id.direccio_notificacio.email) # no es pot testejar perque direccio_notificacio depen de giscedata_facturacio # noqa: 501
--- som_consulta_pobresa/wizard/wizard_crear_consulta_pobresa.py
+++ som_consulta_pobresa/wizard/wizard_crear_consulta_pobresa.py
@@ -19,19 +19,21 @@ class WizardCrearConsultaPobresa(osv.osv_memory):
         pol_obj = self.pool.get('giscedata.polissa')
 
         active_ids = context.get("active_ids")
+        scp_creades = []
         sec_pobresa = sec_obj.search(cursor, uid, [('code', '=', 'BSCPE')])[0]
 
         for _id in active_ids:
             pol = pol_obj.browse(cursor, uid, _id)
-            scp_obj.create(cursor, uid, {
+            scp_id = scp_obj.create(cursor, uid, {
                 'polissa_id': _id,
                 'section_id': sec_pobresa,
                 'name': '[{}] {} ({})'.format(
                     pol.name, pol.titular.name, pol.cups.id_municipi.name),
             })
+            scp_creades.append(scp_id)
 
         return {
-            "domain": "[('id','in', %s)]" % str(active_ids),
+            "domain": "[('id','in', %s)]" % str(scp_creades),
             "name": "Consultes pobresa creades",
             "view_type": "form",
             "view_mode": "tree,form",
