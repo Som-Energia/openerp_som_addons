02becd77ce2a4b027747262e3f2fb2949f616e2a
destanyol <daniel.estanyol@somenergia.coop>
2024-04-16 15:13:17 +0200

    🎨 Precommit

--- som_facturacio_comer/migrations/5.0.23.6.0/post-0004_update_view_to_status_tree.py
+++ som_facturacio_comer/migrations/5.0.23.6.0/post-0004_update_view_to_status_tree.py
@@ -8,17 +8,18 @@ def up(cursor, installed_version):
     if not installed_version:
         return
 
-    logger = logging.getLogger('openerp.migration')
+    logger = logging.getLogger("openerp.migration")
 
     logger.info("Creating pooler")
-    pool = pooler.get_pool(cursor.dbname)
+    pooler.get_pool(cursor.dbname)
 
     logger.info("Updating XML giscedata_facturacio_contracte_lot_view.xml")
     load_data_records(
-        cursor, 'som_facturacio_comer', "giscedata_facturacio_contracte_lot_view.xml", ["view_giscedata_facturacio_contracte_lot_som_tree"], mode='update'
+        cursor, "som_facturacio_comer",
+        "giscedata_facturacio_contracte_lot_view.xml",
+        ["view_giscedata_facturacio_contracte_lot_som_tree"], mode="update"
     )
     logger.info("XMLs succesfully updatd.")
-
 
 
 def down(cursor, installed_version):
