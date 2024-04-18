b6afee3c5568b84cc40f1347c0d8985bf5bf9458
Oriol Piera <oriol.piera@somenergia.coop>
2024-04-18 13:48:13 +0200

    Use reusable workflow in PR GA

--- account_invoice_som/__init__.py
+++ account_invoice_som/__init__.py
@@ -1,4 +1,6 @@
-import report
-import account_invoice
-import account_move_line
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+from . import report
+from . import account_invoice
+from . import account_move_line
 from . import wizard
