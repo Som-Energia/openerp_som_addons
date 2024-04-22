f8625c876a5ce33eadef980c64edf520a02eeaec
Marite Guerrieri <marite.guerrieri@somenergia.coop>
2024-04-22 13:18:48 +0200

    🐛 Precommit fix

--- www_som/tests/tests_indexada_helpers.py
+++ www_som/tests/tests_indexada_helpers.py
@@ -185,9 +185,10 @@ class TestIndexadaHelpers(TestChangeToIndexada):
                 "maturity": ["C3", "C3", "C3", "C3", "C3", None, None, None, None, None,
                              None, None, None, None, None, None, None, None, None, None,
                              None, None, None, None],
-                "compensation_euros_kwh": [0.0012, 0.0013, 0.0014, 0.0015, 0.0016, None, None, None, None,
-                                           None, None, None, None, None, None, None, None,
-                                           None, None, None, None, None, None, None]
+                "compensation_euros_kwh": [0.0012, 0.0013, 0.0014, 0.0015, 0.0016,
+					   None, None, None, None, None, None, None,
+                                           None, None, None, None, None, None, None,
+                                           None, None, None, None, None]
             }
         }
         self.assertDictEqual(json.loads(result), expected)
