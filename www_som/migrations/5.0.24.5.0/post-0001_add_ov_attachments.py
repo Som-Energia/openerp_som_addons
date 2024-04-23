7b2700a9f29bedb9265ebccb0a9dcc65480a7d58
Àngel Fernández <angel.fernandez@somenergia.coop>
2024-04-23 10:19:38 +0200

    Add ov attachments mvp code
    Fix migration
    
    Add fiscal ov category data
    
    Fix fiscal ov category data
    
    Fix fiscal ov category data (eval to True)
    
    Fix fiscal ov category data (pre to post)
    
    Improve get ov attachments with poweremail mailbox
    
    Fix format :)
    
    Refactor www_fields to base module + add test
    
    Fix demo data

--- www_som/migrations/5.0.24.5.0/post-0001_add_ov_attachments.py
+++ www_som/migrations/5.0.24.5.0/post-0001_add_ov_attachments.py
@@ -1,7 +1,6 @@
 # -*- encoding: utf-8 -*-
 import logging
 from oopgrade.oopgrade import load_data, load_data_records, add_columns
-import pooler
 
 
 def up(cursor, installed_version):
@@ -9,10 +8,6 @@ def up(cursor, installed_version):
         return
 
     logger = logging.getLogger('openerp.migration')
-    pool = pooler.get_pool(cursor.dbname)
-
-    wiz_obj = pool.get('wizard.import.ref.cadastral.from.csv')
-    wiz_obj._auto_init(cursor, context={'module': 'www_som'})
 
     logger.info("Adding column ov_available to ir_attachment_category")
     add_columns(
