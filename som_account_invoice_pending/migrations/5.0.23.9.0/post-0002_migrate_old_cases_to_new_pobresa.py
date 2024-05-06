e5ca0678275b21fa0a97cdd2540a4197a54d8fd7
Oriol Piera <oriol.piera@somenergia.coop>
2024-05-06 14:16:36 +0200

    Modify migration script to historize migration

--- som_account_invoice_pending/migrations/5.0.23.9.0/post-0002_migrate_old_cases_to_new_pobresa.py
+++ som_account_invoice_pending/migrations/5.0.23.9.0/post-0002_migrate_old_cases_to_new_pobresa.py
@@ -5,13 +5,14 @@ logger = logging.getLogger('openerp.' + __name__)
 
 
 def up(cursor, installed_version):
-
     if not installed_version:
         return
     uid = 1
+
     pool = pooler.get_pool(cursor.dbname)
     scp_obj = pool.get('som.consulta.pobresa')
     crm_obj = pool.get('crm.case')
+    crmh_obj = pool.get('crm.case.history')
     imd_obj = pool.get('ir.model.data')
 
     section_id = imd_obj.get_object_reference(
@@ -19,28 +20,36 @@ def up(cursor, installed_version):
         'crm_section_bono_social_consulta_pobresa'
     )[1]
     search_vals = [
-        ("section", "=", section_id),
+        ("section_id", "=", section_id),
     ]
     crm_list = crm_obj.search(cursor, uid, search_vals)
     logger.info('Trobats {} crm.case de pobresa energètica'.format(len(crm_list)))
 
     for crm_id in crm_list:
-        crm = crm_obj.read(cursor, uid, crm_id,
-                           ['name', 'polissa_id', 'partner_id', 'description',
-                            'state', 'date'])
+        crm = crm_obj.browse(cursor, uid, crm_id)
         create_vals = {
-            "name": crm['name'],
-            "polissa_id": crm['polissa_id'],
-            "partner_id": crm['partner_id'],
-            "description": crm['description'],
-            "state": crm['state'],
-            "date": crm['date'],
+            "name": crm.name,
+            "polissa_id": crm.polissa_id.id,
+            "partner_id": crm.partner_id.id,
+            "description": crm.description,
+            "state": crm.state,
+            "date": crm.date,
             'section_id': section_id,
         }
         if crm['state'] == 'done':
             create_vals['state'] = 'open'
 
-        scp_obj.create(cursor, uid, create_vals)
+        scp_id = scp_obj.create(cursor, uid, create_vals)
+
+        nou_scp = scp_obj.browse(cursor, uid, scp_id)
+        write_vals = {
+            "user_id": uid,
+            "case_id": nou_scp.crm_id.id,
+            "date": crm.date,
+            "description": "Consulta creada per migració dels antics "
+            "CRM cases a partir del cas (crm.case) amb id {}".format(crm_id)
+        }
+        crmh_obj.create(cursor, uid, write_vals)
 
     logger.info('S\'han creat les consultes pobresa')
 
