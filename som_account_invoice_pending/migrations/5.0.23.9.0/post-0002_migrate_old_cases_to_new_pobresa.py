e1d2583ee040f5f37373b03cfc46ff865aa82340
Oriol Piera <oriol.piera@somenergia.coop>
2024-05-07 08:45:39 +0200

    Add original history log

--- som_account_invoice_pending/migrations/5.0.23.9.0/post-0002_migrate_old_cases_to_new_pobresa.py
+++ som_account_invoice_pending/migrations/5.0.23.9.0/post-0002_migrate_old_cases_to_new_pobresa.py
@@ -1,6 +1,7 @@
 # coding=utf-8
 import logging
 import pooler
+import netsvc
 logger = logging.getLogger('openerp.' + __name__)
 
 
@@ -42,14 +43,31 @@ def up(cursor, installed_version):
         scp_id = scp_obj.create(cursor, uid, create_vals)
 
         nou_scp = scp_obj.browse(cursor, uid, scp_id)
+        message = "Consulta creada per migració dels antics CRM (crm.cases) amb id {}".format(
+            crm_id)
+        for crm_history in crm.history_line:
+            message += """
+            ===========
+            {} / ({})
+            {}
+            """.format(crm_history.user_id.name, crm_history.date, crm_history.description)
+
         write_vals = {
             "user_id": uid,
             "case_id": nou_scp.crm_id.id,
             "date": crm.date,
-            "description": "Consulta creada per migració dels antics "
-            "CRM cases a partir del cas (crm.case) amb id {}".format(crm_id)
+            "description": message
         }
         crmh_obj.create(cursor, uid, write_vals)
+
+        # Creem el workflow si no està creat
+        wf_service = netsvc.LocalService("workflow")
+        cursor.execute("select id from wkf_instance where "
+                       "res_type = 'crm.case' and res_id = %s",
+                       (nou_scp.crm_id.id,))
+        res = cursor.fetchall()
+        if not res:
+            wf_service.trg_create(uid, 'crm.case', nou_scp.crm_id.id, cursor)
 
     logger.info('S\'han creat les consultes pobresa')
 
