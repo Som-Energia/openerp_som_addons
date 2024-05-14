4e5e818b0bdafede71269d40a3e27798dc785520
xavier <xavier.teres@somenergia.coop>
2024-05-14 14:25:01 +0200

    ♻️ (estalvi): Remove context

--- som_estalvi/models/report_backend_som_estalvi.py
+++ som_estalvi/models/report_backend_som_estalvi.py
@@ -182,7 +182,7 @@ class ReportBackendSomEstalvi(ReportBackend):
 
         wiz_id = informe_dades_obj.create(cursor, uid, {}, context=context)
         dades_factures = informe_dades_obj.find_invoices(
-            cursor, uid, [wiz_id], [pol.id], start_date, end_date, context=context
+            cursor, uid, [wiz_id], [pol.id], start_date, end_date
         )[pol.id]
 
         data["energia"] = dades_factures['Energia activa'] + dades_factures['MAG']
