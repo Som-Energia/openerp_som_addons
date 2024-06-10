95b6b573a8ef50e7a46d3f843803cdf7a46f1d5e
destanyol <daniel.estanyol@somenergia.coop>
2024-06-10 09:29:41 +0200

    🚧 (condicions): gurb

--- som_gurb/reports/condicions_particulars.py
+++ som_gurb/reports/condicions_particulars.py
@@ -16,14 +16,11 @@ class CondicionsGurbReport(report_int):
         gurb_cups_o = pool.get("som.gurb.cups")
 
         for gurb_cups_id in ids:
-
-            pol_id = gurb_cups_o.get_polissa_gurb_cups(cursor, uid, gurb_cups_id, context=None)
+            pol_id = gurb_cups_o.get_polissa_gurb_cups(cursor, uid, gurb_cups_id, context=context)
 
             report = netsvc.LocalService('report.giscedata.polissa')
 
-            result, result_format = report.create(
-                cursor, uid, [pol_id], {}, context=context
-            )
+            result, result_format = report.create(cursor, uid, [pol_id], {}, context=context)
 
     def join_pdfs(self, plain_pdf_files):
         """
