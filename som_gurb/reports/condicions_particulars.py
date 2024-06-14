11a35a2878e6914e52a2916ee57ee9b1de69cb97
destanyol <daniel.estanyol@somenergia.coop>
2024-06-14 11:01:03 +0200

    🐛 (gurb): Better GurbReportInt
    Co-authored-by: Xavier Terés <xavier.teres@somenergia.coop>

--- som_gurb/reports/condicions_particulars.py
+++ som_gurb/reports/condicions_particulars.py
@@ -18,29 +18,31 @@ class ConditionsGurbReport(report_int):
         gurb_cups_o = pool.get("som.gurb.cups")
 
         for gurb_cups_id in ids:
-            pol_id = gurb_cups_o.get_polissa_gurb_cups(cursor, uid, gurb_cups_id, context=context)
+            pol_id = gurb_cups_o.get_polissa_gurb_cups(
+                cursor, uid, gurb_cups_id, context=context
+            )
 
             report = netsvc.LocalService('report.giscedata.polissa')
 
             res = []
-            contract_pdf, _ = report.create(cursor, uid, [pol_id], {}, context=context)
+            contract_pdf, doc_type = report.create(cursor, uid, [pol_id], {}, context=context)
+            if doc_type == 'pdf':
+                condicions_generals_pdf = gurb_cups_o.browse(
+                    cursor, uid, gurb_cups_id).general_conditions_id.attachment_id.datas
+                pdfs = [
+                    contract_pdf, b64decode(condicions_generals_pdf)
+                ]
+                tmp_dir = tempfile.gettempdir()
+                for pdf in pdfs:
+                    pdf_file_name = '{}.pdf'.format(uuid.uuid4())
+                    pdf_file_path = os.path.join(tmp_dir, pdf_file_name.replace(' ', ''))
+                    with open(pdf_file_path, 'w') as pdf_file:
+                        pdf_file.write(pdf)
+                    res.append(pdf_file_path)
 
-            pool = pooler.get_pool(cursor.dbname)
-            pdfs = [
-                contract_pdf, b64decode(gurb_cups_o.browse(
-                    cursor, uid, gurb_cups_id).general_conditions_id.attachment_id.datas)
-            ]
-            tmp_dir = tempfile.gettempdir()
-            for pdf in pdfs:
-                pdf_file_name = '{}.pdf'.format(uuid.uuid4())
-                pdf_file_path = os.path.join(tmp_dir, pdf_file_name.replace(' ', ''))
-                with open(pdf_file_path, 'w') as pdf_file:
-                    pdf_file.write(pdf)
-                res.append(pdf_file_path)
+                result = self.join_pdfs(res)
 
-            result = self.join_pdfs(res)
-
-            return result, 'pdf'
+                return result, 'pdf'
 
     def join_pdfs(self, pdf_paths):
         """
