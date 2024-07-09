52d00986ef5ab99c0d1119dcaf0e4e2c2233d681
Àngel Fernández <angel.fernandez@somenergia.coop>
2024-07-09 10:00:16 +0200

    Get generation montlhy and yearly historical use

--- som_generationkwh/tests/__init__.py
+++ som_generationkwh/tests/__init__.py
@@ -1,5 +1,6 @@
 from emission_tests import *
 from investment_tests import *
+from assignment_tests import *
 from partner_tests import *
 from somenergia_soci_tests import *
 from test_wizard_baixa_soci import *
