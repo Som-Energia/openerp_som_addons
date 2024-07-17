1286edded5c8dfdceeb2a2dad2f74aa56160b4a6
pauboixsom <pau.boix@somenergia.coop>
2024-07-17 10:36:15 +0200

    modify migration script

--- som_indexada/migrations/5.0.2.108/pre-0002_pricelist_indexada_2024_semantic_id_creation.py
+++ som_indexada/migrations/5.0.2.108/pre-0002_pricelist_indexada_2024_semantic_id_creation.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import logging
-from oopgrade.oopgrade import load_data_records
+from datetime import datetime
+import pooler
 
 
 def up(cursor, installed_version):
@@ -10,28 +11,47 @@ def up(cursor, installed_version):
     logger = logging.getLogger('openerp.migration')
     logger.info("Updating XMLs")
 
-    list_of_records = ["pricelist_indexada_20td_peninsula_2024",
-                       "pricelist_indexada_20td_balears_2024",
-                       "pricelist_indexada_20td_canaries_2024",
-                       "pricelist_indexada_30td_peninsula_2024",
-                       "pricelist_indexada_30td_balears_2024",
-                       "pricelist_indexada_30td_canaries_2024",
-                       "pricelist_indexada_61td_peninsula_2024",
-                       "pricelist_indexada_61td_balears_2024",
-                       "pricelist_indexada_61td_canaries_2024",
-                       "pricelist_indexada_30tdve_peninsula_2024",
-                       "pricelist_indexada_empresa_peninsula_non_standard_2024",
-                       "pricelist_indexada_empresa_balears_non_standard_2024",
-                       "pricelist_indexada_empresa_canaries_non_standard_2024"]
+    list_of_xml_name = [("pricelist_indexada_20td_peninsula_2024", "Indexada 2.0TD Península 2024")
+                        ("pricelist_indexada_20td_balears_2024", "Indexada 2.0TD Balears 2024")
+                        ("pricelist_indexada_20td_canaries_2024", "Indexada 2.0TD Canàries 2024")
+                        ("pricelist_indexada_30td_peninsula_2024", "Indexada 3.0TD Península 2024")
+                        ("pricelist_indexada_30td_balears_2024", "Indexada 3.0TD Balears 2024")
+                        ("pricelist_indexada_30td_canaries_2024", "Indexada 3.0TD Canàries 2024")
+                        ("pricelist_indexada_61td_peninsula_2024", "Indexada 6.1TD Península 2024")
+                        ("pricelist_indexada_61td_balears_2024", "Indexada 6.1TD Balears 2024")
+                        ("pricelist_indexada_61td_canaries_2024", "Indexada 6.1TD Canàries 2024")
+                        ("pricelist_indexada_30tdve_peninsula_2024",
+                        "Indexada 3.0TVE Península 2024")
+                        ("pricelist_indexada_empresa_peninsula_non_standard_2024",
+                        "Indexada Empresa Peninsula 2024")
+                        ("pricelist_indexada_empresa_balears_non_standard_2024",
+                        "Indexada Empresa Balears 2024")
+                        ("pricelist_indexada_empresa_canaries_non_standard_2024",
+                        "Indexada Empresa Canaries 2024")]
 
-    load_data_records(
-        cursor,
-        'som_indexada',
-        'data/product_pricelist_data.xml',
-        list_of_records,
-        mode='update'
-    )
-    logger.info("XMLs succesfully updated.")
+    logger.info("Starting Pricelist Indexada 2024 semantic id creation migration script")
+    pool = pooler.get_pool(cursor.dbname)
+
+    model_data_obj = pool.get("ir.model.data")
+    pricelist_obj = pool.get("product.pricelist")
+
+    for pricelist in list_of_xml_name:
+        pricelist_ids = pricelist_obj.search(cursor, 1, [("name", "=", pricelist[1])])
+        if len(pricelist_ids) == 1:
+            pricelist_id = pricelist_ids[0]
+            today = datetime.today().strftime("%Y-%m-%d")
+
+            model_data_vals = {
+                "noupdate": True,
+                "name": pricelist[0],
+                "module": "som_indexada",
+                "model": "product.pricelist",
+                "res_id": pricelist_id,
+                "date_init": today,
+                "date_update": today,
+            }
+
+            model_data_obj.create(cursor, 1, model_data_vals)
 
 
 def down(cursor, installed_version):
