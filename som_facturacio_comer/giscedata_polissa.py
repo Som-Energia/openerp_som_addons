eb6fe007db296ce1b0af5418a6255a28d73191b6
Iker Paz Bustins <ikertaliga@gmail.com>
2024-07-18 12:44:22 +0200

    Partir facturació per modificació de fòrmula indexada

--- som_facturacio_comer/giscedata_polissa.py
+++ som_facturacio_comer/giscedata_polissa.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 from osv import osv, fields
 from tools.translate import _
+from giscegas_facturacio.giscegas_polissa import INTERVAL_INVOICING_FIELDS
 
+INTERVAL_INVOICING_FIELDS += ['indexed_formula']
 
 class GiscedataPolissa(osv.osv):
     """Pòlissa per afegir el camp teoric_maximum_consume_gc."""
@@ -67,6 +69,53 @@ class GiscedataPolissa(osv.osv):
                 res[pol_id] = fact_date[0].get("date_invoice")
 
         return res
+
+    def get_modcontractual_intervals(self, cursor, uid, polissa_id, data_inici, data_final, context=None):
+        if context is None:
+            context = {}
+
+        modcon_o = self.pool.get('giscedata.polissa.modcontractual')
+        price_o = self.pool.get('product.pricelist')
+        dates_de_tall = super(GiscedataPolissa, self).get_modcontractual_intervals(cursor, uid, polissa_id, data_inici,
+                                                                                   data_final, context)
+
+        llista_preu_dades = []
+        indexed_formula_old = ""
+        changes = {}
+        dates = {}
+
+        for mod_data in sorted(dates_de_tall.keys()):
+            values = ['llista_preu', 'data_inici', 'data_final']
+            modcon_dades = modcon_o.read(cursor, uid, dates_de_tall[mod_data]['id'], values, context=context)
+
+            llista_preu_dades.append((
+                dates_de_tall[mod_data]['id'],
+                mod_data,
+                modcon_dades['llista_preu_id'][0],
+                modcon_dades['data_inici'],
+                modcon_dades['data_final']
+            ))
+
+        if len(set(llista_preu_dades)) > 1:
+
+            for modcon_id, mod_data, llista_preu_id, data_inici, data_final in llista_preu_dades:
+                indexed_formula = price_o.read(cursor, uid, llista_preu_id, ['indexed_formula'],
+                                               context=context)['indexed_formula']
+
+                if indexed_formula != indexed_formula_old and indexed_formula_old != "":
+                    changes[modcon_id] = ['indexed_formula']
+                    dates[modcon_id] = (data_inici, data_final)
+
+                    # crear nou registre
+                    dates_de_tall[mod_data].append({
+                        'id': modcon_id,
+                        'changes': changes,
+                        'dates': dates
+                    })
+
+                indexed_formula_old = indexed_formula
+
+        return dates_de_tall
 
     _columns = {
         "teoric_maximum_consume_gc": fields.float(
