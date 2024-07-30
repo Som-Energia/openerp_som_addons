a203cefb5dcbad42556ec22739c9c202d7aa4c15
Oriol Piera <oriol.piera@somenergia.coop>
2024-07-30 14:11:41 +0200

    Add more info, sql query
    Co-authored-by: Fran Cubero <francisco.cubero@somenergia.coop>

--- scripts/informe_fue.py
+++ scripts/informe_fue.py
@@ -1,14 +1,18 @@
 # -*- coding: utf-8 -*-
-from datetime import datetime
+from datetime import datetime, timedelta
 import dbconfig
-from consolemsg import step, success
+from consolemsg import step, success, warn
 from erppeek import Client
 from tqdm import tqdm
+import psycopg2
 
 
 """_summary_
 Dades per informe FUE sobre les refacturacions
-
+1.- Hem tingut en compte les factures amb lectures Estimades (estmació distri codi 40) i estimació comer 'ES'.  # noqa: E501
+2.- A efectes de refacturació, considerem només les factures tipus "R"
+3.- Per obentir el marge d'error en les estimacions, només sumem les refacturacions on hem hagut de tornar dines (no fem res amb les infraestimacions)  # noqa: E501
+4.- Considerem que la distri no ens esta enviant lectures quan han passat 2 mesos des de la darrera lectura  # noqa: E501
 
 SQL per trobar factures en aquest estat:
 
@@ -42,7 +46,31 @@ ESTIMACIO_SOM = 'ES'  # Estimada Som Energia
 
 START_DATE = '2021-06-01'
 
+DATA_FALTEN_LECTURES = (datetime.today() - timedelta(days=62)).strftime('%Y-%m-%d')
+DATA_FALTEN_LECTURES = '2023-01-01'
+
 TOTS_CONTRACTES = 'True'  # En cas de canvi de titular, comptem 2 contractes
+
+
+def get_db_cursor():
+    try:
+        dbconn = psycopg2.connect(**dbconfig.psycopg)
+    except Exception, ex:
+        warn("Unable to connect to database {}", str(dbconfig.psycopg))
+        raise ex
+
+    return dbconn.cursor()
+
+
+def execute_sql(dbcur, sql_query):
+    try:
+        dbcur.execute(sql_query)
+    except Exception, ex:
+        warn('Failed executing query')
+        warn(sql_query)
+        raise ex
+
+    return [record[0] for record in dbcur.fetchall()]
 
 
 def obtenir_llista_factures_estimades_som(c):
@@ -122,8 +150,70 @@ def obtenir_factures_refacturades_sobrestimades(c, fact_ids):
     return sobrestimades_ids, total_sobrestimat
 
 
+def contractes_estimats_no_refactures(cur):
+    sql = """
+        select distinct gp.id
+        from
+        giscedata_polissa gp ,
+        giscedata_facturacio_factura gff ,
+        account_invoice ai,
+        giscedata_lectures_lectura gll ,
+        giscedata_lectures_comptador glc
+        where
+        gp.id = gff.polissa_id and
+        ai.id = gff.invoice_id and
+        glc.polissa = gp.id and
+        gll.comptador = glc.id and
+        gll.name = gff.data_final and
+        gp.active = true and
+        gp.state = 'activa' and
+        gll.origen_id in (7,10,11) and
+        gll.origen_comer_id = 6 and
+        gff.id =
+            (select max(gff2.id)
+            from giscedata_facturacio_factura gff2
+            where gff2.polissa_id = gp.id
+            ) and
+        ai."type" = 'out_invoice' and
+        ai.rectifying_id is null
+    """
+    pol_ids = execute_sql(cur, sql)
+    """
+    erppeek
+    pol_ids = []
+    for pol_id in c.GiscedataPolissa.search([]):
+        pol = c.GiscedataPolissa.read(pol_id, ['data_ultima_factura'])
+        f_ids = c.GiscedataFacturacioFactura.search(
+            [('polissa_id','=', pol_id),
+             ('date_invoice','=', pol['data_ultima_factura']),
+             ('type','=','out_invoice'),
+             ('rectifying_id', "=", False)])
+        if not f_ids:
+            continue
+        fact = c.GiscedataFacturacioFactura.read(f_ids[0], ['comptadors', 'data_final'])
+        l_ids = c.GiscedataLecturesLectura.search(
+            [('comptador', '=', fact['comptadors'][0]),
+             ('name','=',fact['data_final']),
+             ('origen_id','in', [7, 10, 11]),
+             ('origen_comer_id','=', 6)])
+        if not l_ids:
+            continue
+        import pudb;pu.db
+        pol_ids.append(pol_id)"""
+
+    return len(pol_ids)
+
+
+def contractes_sense_lectures_distri(c):
+    return len(c.GiscedataPolissa.search(
+        [('data_ultima_lectura', '<', DATA_FALTEN_LECTURES)]))
+
+
 def main():
     c = Client(**dbconfig.erppeek)
+    cur = get_db_cursor()
+
+    contractes_estimats_no_refactures(cur)
     step(u"Obtenim factures")
     llista_factures = obtenir_llista_factures_estimades_som(c)
     step(u"Nombre de factures amb lectures estimades:  {}".format(len(llista_factures)))
@@ -142,7 +232,8 @@ def main():
     step(u"Nombre de factures refacturades sobrestimades: {}".format(
         len(llista_factures_refacturades_sobrestimades)))
     step(u"Total kwh sobrestimats: {}".format(total_sobrestimat))
-
+    ncontractes_sense_lectures = contractes_sense_lectures_distri(c)
+    step(u"Nombre de contractes sense lectures distri (2 mesos): {}".format(ncontractes_sense_lectures))  # noqa: E501
     success(u"Feina feta.")
 
 
