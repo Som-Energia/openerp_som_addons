58854ad4e5251cbe18ca52e39b816d6b5d04d342
Emili García <egarcia@gisce.net>
2024-09-24 11:19:39 +0200

    Adds cron activation logic to check auvidi change

--- som_auvidi/giscedata_polissa.py
+++ som_auvidi/giscedata_polissa.py
@@ -26,9 +26,23 @@ class GiscedataPolissaModcontractual(osv.osv):
     _name = "giscedata.polissa.modcontractual"
     _inherit = "giscedata.polissa.modcontractual"
 
-    _columns = {
-        'te_auvidi': fields.boolean('Té AUVIDI')
-    }
+    def _do_previous_actions_on_activation(self, cursor, uid, mc_id, context=None):
+        res = super(GiscedataPolissaModcontractual, self)._do_previous_actions_on_activation(
+            cursor, uid, mc_id, context
+        )
+        if res == 'OK':
+            modcon = self.browse(cursor, uid, mc_id, context={'prefetch': False})
+            te_auvidi_ara = modcon.te_auvidi
+            tenia_auvidi_abans = modcon.modcontractual_ant.te_auvidi
 
+            if modcon.modcontractual_ant and te_auvidi_ara and te_auvidi_ara != tenia_auvidi_abans:
+                # Let's check if last F1 reading is prev to modcon creation date (without time)
+                if not self._apply_modcon_date_last_f1_plus_1(cursor, uid, mc_id, context=None):
+                    return 'error'
+        return res
+
+    _columns = {
+        'te_auvidi': fields.boolean('Té AUVIDI'),
+    }
 
 GiscedataPolissaModcontractual()
