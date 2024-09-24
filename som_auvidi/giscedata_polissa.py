5f50d78c23f48e9a5063f96153e8b61ed731ba53
Emili García <egarcia@gisce.net>
2024-09-24 12:59:16 +0200

    Assings category to polissa when activate modcon

--- som_auvidi/giscedata_polissa.py
+++ som_auvidi/giscedata_polissa.py
@@ -41,6 +41,34 @@ class GiscedataPolissaModcontractual(osv.osv):
                     return 'error'
         return res
 
+    def aplicar_modificacio(self, cursor, uid, mod_id, polissa_id=None):
+        """Aplica els canvis d'una modificació contractual a una pòlissa."""
+        res = super(GiscedataPolissaModcontractual, self).aplicar_modificacio(
+            cursor, uid, mod_id, polissa_id=polissa_id
+        )
+        polissa_obj = self.pool.get('giscedata.polissa')
+        servei_gen_obj = self.pool.get('giscedata.servei.generacio')
+        sg_polissa_obj = self.pool.get('giscedata.servei.generacio.polissa')
+
+        if not polissa_id:
+            polissa_id = self.browse(cursor, uid, mod_id).polissa_id.id
+
+        current_te_auvidi = self.read(cursor, uid, mod_id, ['te_auvidi'])['te_auvidi']
+
+        if current_te_auvidi:
+            sg_pol_id = sg_polissa_obj.search(cursor, uid, [('polissa_id', '=', polissa_id)])
+            if len(sg_pol_id):
+                sg_pol_data = sg_polissa_obj.read(cursor, uid, sg_pol_id[0], ['servei_generacio_id'])
+                if sg_pol_data.get('servei_generacio_id'):
+                    servei_gen_id = sg_pol_data.get('servei_generacio_id', [False])[0]
+                    if servei_gen_id:
+                        categoria_polissa = servei_gen_obj.read(cursor, uid, servei_gen_id, ['categoria_polissa']).get('categoria_polissa')
+                        polissa_current_categories = polissa_obj.read(cursor, uid, polissa_id, ['category_id']).get('category_id', [])
+                        if categoria_polissa and categoria_polissa[0] not in polissa_current_categories:
+                            new_categories = (list(set(polissa_current_categories)) or []) + [categoria_polissa[0]]
+                            polissa_obj.write(cursor, uid, polissa_id, {'category_id': [(6, 0, new_categories)]})
+        return res
+
     _columns = {
         'te_auvidi': fields.boolean('Té AUVIDI'),
     }
