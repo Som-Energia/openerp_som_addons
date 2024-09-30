3f917d924ef212ae43e9b74f33c316af622ba0c0
Emili García <egarcia@gisce.net>
2024-09-30 10:48:23 +0200

    Cancel auvidi removes category

--- som_auvidi/giscedata_polissa.py
+++ som_auvidi/giscedata_polissa.py
@@ -53,19 +53,36 @@ class GiscedataPolissaModcontractual(osv.osv):
         if not polissa_id:
             polissa_id = self.browse(cursor, uid, mod_id).polissa_id.id
 
-        current_te_auvidi = self.read(cursor, uid, mod_id, ['te_auvidi'])['te_auvidi']
+        modcon = self.browse(cursor, uid, mod_id)
+        current_te_auvidi = modcon.te_auvidi
+        cancels_auvidi = modcon.modcontractual_ant \
+                         and modcon.modcontractual_ant.te_auvidi \
+                         and not modcon.te_auvidi
 
-        if current_te_auvidi:
+        if current_te_auvidi or cancels_auvidi:
             sg_pol_id = sg_polissa_obj.search(cursor, uid, [('polissa_id', '=', polissa_id)])
             if len(sg_pol_id):
                 sg_pol_data = sg_polissa_obj.read(cursor, uid, sg_pol_id[0], ['servei_generacio_id'])
                 if sg_pol_data.get('servei_generacio_id'):
                     servei_gen_id = sg_pol_data.get('servei_generacio_id', [False])[0]
                     if servei_gen_id:
-                        categoria_polissa = servei_gen_obj.read(cursor, uid, servei_gen_id, ['categoria_polissa']).get('categoria_polissa')
-                        polissa_current_categories = polissa_obj.read(cursor, uid, polissa_id, ['category_id']).get('category_id', [])
-                        if categoria_polissa and categoria_polissa[0] not in polissa_current_categories:
-                            new_categories = (list(set(polissa_current_categories)) or []) + [categoria_polissa[0]]
+                        categoria_polissa = servei_gen_obj.read(
+                            cursor, uid, servei_gen_id, ['categoria_polissa']
+                        ).get('categoria_polissa')
+                        polissa_current_categories = polissa_obj.read(
+                            cursor, uid, polissa_id, ['category_id']
+                        ).get('category_id', [])
+                        polissa_current_categories = list(set(polissa_current_categories))
+                        new_categories = []
+                        # If AUVIDI is cancelled we must remove category
+                        if cancels_auvidi and categoria_polissa and categoria_polissa[0] in polissa_current_categories:
+                            polissa_current_categories.remove(categoria_polissa[0])
+                            new_categories = polissa_current_categories
+                        # If AUVIDI and there's no category we must add it
+                        elif current_te_auvidi and categoria_polissa and categoria_polissa[0] not in polissa_current_categories:
+                            new_categories = polissa_current_categories + [categoria_polissa[0]]
+
+                        if new_categories:
                             polissa_obj.write(cursor, uid, polissa_id, {'category_id': [(6, 0, new_categories)]})
         return res
 
