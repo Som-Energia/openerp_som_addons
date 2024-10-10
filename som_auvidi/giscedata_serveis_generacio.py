c27c05801aeb243cf094410e7a701545f7234176
alex <aivan@gisce.net>
2024-10-10 17:11:25 +0200

    new poder personalizar missatge de factura repetida

--- som_auvidi/giscedata_serveis_generacio.py
+++ som_auvidi/giscedata_serveis_generacio.py
@@ -1,11 +1,16 @@
 # -*- coding: utf-8 -*-
 from osv import osv, fields
 from datetime import datetime, timedelta
+from tools.translate import _
 
 
 class GiscedataServeiGeneracio(osv.osv):
     _name = "giscedata.servei.generacio"
     _inherit = "giscedata.servei.generacio"
+
+    def get_missatge_factura_duplicada(self, cursor, uid, context=None):
+        return _("Aquest contracte té auvidi i seria millor fer la factura per lot, si no saps com procedir"
+                 " busca ajuda")
 
     def create(self, cursor, uid, vals, context=None):
         if context is None:
