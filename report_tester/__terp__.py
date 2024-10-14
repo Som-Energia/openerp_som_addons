ed30c423f561803b9b91119794111400c6b1f9b9
Fran Cubero <francisco.cubero@somenergia.coop>
2024-10-14 16:45:07 +0200

    initial module commit

---
+++ report_tester/__terp__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "report tester",
+    "description": """
+    This module provide :
+        * menus models u wizards per poder testejar reports de manera integrada dins del erp
+    """,
+    "version": "0-dev",
+    "author": "GISCE",
+    "category": "SomEnergia",
+    "depends": [
+        "base",
+    ],
+    "init_xml": [],
+    "demo_xml": [],
+    "update_xml": [
+        "security/ir.model.access.csv",
+    ],
+    "active": False,
+    "installable": True,
+}
