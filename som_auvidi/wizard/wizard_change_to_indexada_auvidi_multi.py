b3beae1ca43463025ad664ce268eddd473fbcb5e
Fran Cubero <francisco.cubero@somenergia.coop>
2024-10-16 16:14:57 +0200

    simplification, refactor and error handling

--- som_auvidi/wizard/wizard_change_to_indexada_auvidi_multi.py
+++ som_auvidi/wizard/wizard_change_to_indexada_auvidi_multi.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from osv import osv, fields
+from datetime import date, timedelta
 
 
 class WizardChangeToIndexadaAuvidiMulti(osv.osv_memory):
@@ -13,8 +14,14 @@ class WizardChangeToIndexadaAuvidiMulti(osv.osv_memory):
         wiz_og = self.browse(cursor, uid, ids[0], context=context)
 
         pol_ids = context.get("active_ids")
-        failed_polisses = []
-        error_polisses = []
+        faileds = []
+        from_wainting_periodes = []
+        from_periodes = []
+        from_waiting_indexed = []
+        from_already_there = []
+        from_indexed = []
+        msg = []
+
         for pol_id in pol_ids:
 
             mode = self.get_current_mode(cursor, uid, pol_id)
@@ -24,36 +31,68 @@ class WizardChangeToIndexadaAuvidiMulti(osv.osv_memory):
                 if not md:
                     # Periods withe MODCON pending to indexed --> modcon pending Indexed + auvidi
                     # Create the MODCON to indexed
-                    if not self.change_to_indexada_auvidi(
-                        cursor, uid, pol_id, wiz_og.pricelist.id, wiz_og.coeficient_k
-                    ):
-                        failed_polisses.append(pol_id)
-                        continue
-
-                md = self.get_last_pending_modcon(cursor, uid, pol_id)
-                # Periods with MODCON pending to indexed and auvidi --> don't do anything
-                # Periods with MODCON pending to indexed --> modify the MODCON to add the auvdi
-                md.te_auvidi = True
-
+                    if not self.change_to_indexada(cursor, uid, pol_id):
+                        faileds.append(pol_id)
+                    else:
+                        md = self.get_last_pending_modcon(cursor, uid, pol_id)
+                        md.te_auvidi = True
+                        from_periodes.append(pol_id)
+                else:
+                    if md.te_auvidi:
+                        # Periods with MODCON pending to indexed and auvidi --> don't do anything
+                        from_already_there.append(pol_id)
+                    else:
+                        # Periods with MODCON pending to indexed --> modify the MODCON to add auvdi
+                        md.te_auvidi = True
+                        from_waiting_indexed.append(pol_id)
             else:  # in indexed
                 if not md:
                     # Indexed with out MODCON to periods --> create MODCON to auvidi
-                    pass
+                    if not self.create_auvidi_pending_modcon(cursor, uid, pol_id):
+                        faileds.append(pol_id)
+                    else:
+                        from_indexed.append(pol_id)
                 else:
                     # Indexed with MODCON to periods --> error, cannot be done!
-                    error_polisses.append(pol_id)
+                    from_wainting_periodes.append(pol_id)
 
-        info = ""
-        if failed_polisses:
-            failed_str = self.get_list_polissa_names(cursor, uid, failed_polisses)
-            info += "\nLes pòlisses següents han fallat: {}".format(failed_str)
-        if error_polisses:
-            error_str = self.get_list_polissa_names(cursor, uid, error_polisses)
-            info += "\nLes pòlisses següents no es pot fer el canvi: {}".format(error_str)
-        if not failed_polisses and not error_polisses:
-            info = "Procés acabat correctament!"
+        if from_periodes:
+            pols = self.get_list_polissa_names(cursor, uid, from_periodes)
+            msg.append("Pòlisses que estan a periodes:")
+            msg.append(" - Creada modcon a indexada + auvidi: {}".format(pols))
 
-        wiz_og.write({"state": "end", "info": info})
+        if from_waiting_indexed:
+            pols = self.get_list_polissa_names(cursor, uid, from_waiting_indexed)
+            msg.append("Pòlisses que estan a periodes i tenen modcon a indexada:")
+            msg.append(" - Modcon modificada, afegit auvidi: {}".format(pols))
+
+        if from_already_there:
+            pols = self.get_list_polissa_names(cursor, uid, from_already_there)
+            msg.append("Pòlisses que estan a periodes i tenen modcon a indexada amb auvidi:")
+            msg.append(" - No fem res: {}".format(pols))
+
+        if from_indexed:
+            pols = self.get_list_polissa_names(cursor, uid, from_indexed)
+            msg.append("Pòlisses que estan a indexada:")
+            msg.append(" - Creada modcon a auvidi: {}".format(pols))
+
+        if from_wainting_periodes:
+            pols = self.get_list_polissa_names(cursor, uid, from_wainting_periodes)
+            msg.append("ERROR Pòlisses que estan a indexada amb modcon pendent a periodes:")
+            msg.append(" - {}".format(pols))
+
+        if faileds:
+            pols = self.get_list_polissa_names(cursor, uid, faileds)
+            msg.append("ERROR Pòlisses que han fallat al crear modcon:")
+            msg.append(" - {}".format(pols))
+
+        if not faileds and not from_wainting_periodes:
+            msg.append("Procés acabat correctament!")
+
+        wiz_og.write({"state": "end", "info": "\n".join(msg)})
+
+    def quit_auvidi_multi(self, cursor, uid, ids, context=None):
+        pass
 
     def get_last_pending_modcon(self, cursor, uid, pol_id):
         pol_obj = self.pool.get("giscedata.polissa")
@@ -75,13 +114,9 @@ class WizardChangeToIndexadaAuvidiMulti(osv.osv_memory):
         pol_data = pol_obj.read(cursor, uid, pol_ids, ["name"])
         return ', '.join([pol['name'] for pol in pol_data])
 
-    def change_to_indexada_auvidi(self, cursor, uid, pol_id, pricelist_id, coeficient_k):
+    def change_to_indexada(self, cursor, uid, pol_id):
         wz_chng_to_indx_obj = self.pool.get("wizard.change.to.indexada")
-        ctx = {
-            "active_id": pol_id,
-            "business_pricelist": pricelist_id,
-            "coeficient_k": coeficient_k,
-        }
+        ctx = {"active_id": pol_id}
         params = {"change_type": "from_period_to_index"}
         wiz_id = wz_chng_to_indx_obj.create(cursor, uid, params, context=ctx)
         wiz = wz_chng_to_indx_obj.browse(cursor, uid, [wiz_id])[0]
@@ -91,11 +126,21 @@ class WizardChangeToIndexadaAuvidiMulti(osv.osv_memory):
         except Exception:
             return False
 
+    def create_auvidi_pending_modcon(self, cursor, uid, pol_id):
+        pol_obj = self.pool.get("giscedata.polissa")
+        tomorrow = (date.today() + timedelta(days=1)).strftime("%Y-%m-%d")
+        auvidi_flag = {
+            'te_auvidi': True,
+        }
+        try:
+            pol_obj.create_contracte(cursor, uid, pol_id, tomorrow, auvidi_flag)
+            return True
+        except Exception:
+            return False
+
     _columns = {
         "state": fields.selection([("init", "Init"), ("end", "End")], "State"),
         "info": fields.text("Description"),
-        "pricelist": fields.many2one("product.pricelist", "Tarifa"),
-        "coeficient_k": fields.float("Coeficient K", digits=(9, 3)),
     }
 
     _defaults = {
