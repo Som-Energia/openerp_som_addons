cdb3ba02f31ab283a04cb905c73e53e5a297008f
Oriol Piera <oriol.piera@somenergia.coop>
2024-11-05 15:15:51 +0100

    Rerun tests

--- som_auvidi/giscedata_serveis_generacio.py
+++ som_auvidi/giscedata_serveis_generacio.py
@@ -16,7 +16,7 @@ class GiscedataServeiGeneracio(osv.osv):
             imd_obj = self.pool.get('ir.model.data')
             categ_obj = self.pool.get('giscedata.polissa.category')
 
-            new_name = "[AUVI] " + vals.get('name')
+            new_name = " [AUVI] " + vals.get('name')
             new_code = "AUVI" + ''.join([x[0] for x in vals.get('name').split()]).upper()
             auvidi_categ_id = imd_obj.get_object_reference(
                 cursor, uid, 'som_auvidi', 'polissa_category_auvidi_base'
