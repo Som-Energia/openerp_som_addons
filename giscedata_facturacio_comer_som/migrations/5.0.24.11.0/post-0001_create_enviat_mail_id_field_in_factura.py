ce3d1d03951e69f5dfa33163dbed697076ce90b0
Fran Cubero <francisco.cubero@somenergia.coop>
2024-11-15 16:57:50 +0100

    🔧 simplified migration and external migration script

--- giscedata_facturacio_comer_som/migrations/5.0.24.11.0/post-0001_create_enviat_mail_id_field_in_factura.py
+++ giscedata_facturacio_comer_som/migrations/5.0.24.11.0/post-0001_create_enviat_mail_id_field_in_factura.py
@@ -3,67 +3,6 @@ import logging
 import pooler
 from tools import config
 from oopgrade.oopgrade import add_columns_fk, column_exists, load_data, load_data_records
-from tqdm import tqdm
-
-
-def search_email(pool, cursor, uid, fact_id, fact_number, sent_date):
-    mail_obj = pool.get("poweremail.mailbox")
-
-    query = [
-        ('folder', '=', 'sent'),
-        ('reference', '=', 'giscedata.facturacio.factura,{}'.format(fact_id)),
-        ('date_mail', '=', sent_date),
-    ]
-
-    subjects = [
-        u'Factura XX',
-        u'Som Energia: Factura XX',
-        u'Factura pagada XX',
-        u'Factura electricitat XX',
-        u'Factura electricidad XX',
-        u'Reenviament XX (adjunt correcte) / Reenvío XX (adjunto correcto)',
-        u'Reenvío de factura XX',
-        u'Reenviament Factura XX',
-        u'Reenvío Factura XX',
-    ]
-
-    # Let's try the fastest ones
-    for subject in subjects:
-        full_subject = subject.replace("XX", fact_number)
-        mail_ids = mail_obj.search(
-            cursor, uid,
-            [('pem_subject', '=', full_subject)] + query
-        )
-        if mail_ids:
-            return mail_ids
-
-    # The slowest one
-    return mail_obj.search(
-        cursor, uid,
-        [('pem_subject', 'like', '%{}%'.format(fact_number))] + query
-    )
-
-
-def update_field(pool, cursor, uid, fact_id):
-    fact_obj = pool.get("giscedata.facturacio.factura")
-
-    fact_data = fact_obj.read(cursor, uid, ['number', 'enviat_data'])
-
-    mail_ids = search_email(pool, cursor, uid, fact_id,
-                            fact_data['number'], fact_data['enviat_data'])
-    if mail_ids:
-        fact_obj.write(cursor, uid, fact_id, {'enviat_mail_id': mail_ids[0]})
-
-
-def get_fact_to_update(pool, cursor, uid):
-    fact_obj = pool.get("giscedata.facturacio.factura")
-    fact_ids = fact_obj.search(cursor, uid, [
-        ('number', '!=', None),
-        ('enviat', '=', True),
-        ('enviat_carpeta', '=', 'sent'),
-        ('type', 'in', ['out_invoice', 'out_refund']),
-    ])
-    return fact_ids
 
 
 def up(cursor, installed_version):
@@ -73,7 +12,7 @@ def up(cursor, installed_version):
     logger = logging.getLogger('openerp.migration')
 
     logger.info("Creating pooler")
-    pool = pooler.get_pool(cursor.dbname)
+    pooler.get_pool(cursor.dbname)
 
     logger.info("Create enviat_mail_id column  in giscedata_facturacio_factura")
     if not column_exists(cursor, 'giscedata_facturacio_factura', 'enviat_mail_id'):
@@ -100,12 +39,7 @@ def up(cursor, installed_version):
     )
     logger.info("XMLs succesfully updated.")
 
-    logger.info("Starting the migration: Fill the column")
-    uid = 1
-    fact_ids = get_fact_to_update(pool, cursor, uid)
-    for fact_id in tqdm(fact_ids):
-        update_field(pool, cursor, uid, fact_id)
-    logger.info("Column succesfuly filled")
+    # Run the script: manual_migration.py
 
 
 def down(cursor, installed_version):
