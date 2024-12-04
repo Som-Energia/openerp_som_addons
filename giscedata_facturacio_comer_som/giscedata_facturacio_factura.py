f855685c78044054cda619f478f0a6b10bf1a2d5
Àngel Fernández <angel.fernandez@somenergia.coop>
2024-12-04 11:39:34 +0100

    Add script to store unsent pdf invoices

--- giscedata_facturacio_comer_som/giscedata_facturacio_factura.py
+++ giscedata_facturacio_comer_som/giscedata_facturacio_factura.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
+import netsvc
+from datetime import datetime, timedelta
 from osv import osv, fields
+from addons import get_module_resource
 
 
 class GiscedataFacturacioFactura(osv.osv):
@@ -44,6 +47,37 @@ class GiscedataFacturacioFactura(osv.osv):
                     self.write(cursor, uid, fact_id, {'enviat_mail_id': new_mail_id})
 
         return res
+
+    def store_unsent_pdf_invoices(self, cursor, uid, context=None):
+        conf_obj = self.pool.get("res.config")
+        unsent_store_days = conf_obj.get(cursor, uid, "factura_pdf_unsent_store_days", 60)
+        date_search = datetime.today() - timedelta(days=unsent_store_days)
+
+        query_file = get_module_resource(
+            'giscedata_facturacio_comer_som', 'sql', "query_fact_to_pdf_store.sql")
+        query = open(query_file).read()
+
+        cursor.execute(query, (date_search.strftime('%Y-%m-%d'), ))
+        unstored_fact_ids = [x[0] for x in cursor.fetchall()]
+
+        error_ids = []
+        for fact_id in unstored_fact_ids:
+            try:
+                report = netsvc.service_exist("report.giscedata.facturacio.factura")
+                values = {
+                    "model": "giscedata.facturacio.factura",
+                    "id": [fact_id],
+                    "report_type": "pdf",
+                }
+                report.create(
+                    cursor, uid, [fact_id], values,
+                    {'save_pdf_in_invoice_attachments': True}
+                )[0]
+            except Exception:
+                error_ids.append(fact_id)
+
+        total_successful = len(unstored_fact_ids) - len(error_ids)
+        return total_successful, error_ids
 
     _columns = {
         'enviat_mail_id': fields.many2one(
