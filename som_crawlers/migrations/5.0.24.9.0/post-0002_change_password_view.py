cebe6865b9f5a3669280cf421e292f8539f78214
destanyol <daniel.estanyol@somenergia.coop>
2024-12-19 12:29:04 +0100

    🏗️ (crawler): Migration script for password change

---
+++ som_crawlers/migrations/5.0.24.9.0/post-0002_change_password_view.py
@@ -0,0 +1,23 @@
+# -*- encoding: utf-8 -*-
+import logging
+from oopgrade.oopgrade import load_data
+
+
+def up(cursor, installed_version):
+    if not installed_version:
+        return
+
+    logger = logging.getLogger('openerp.migration')
+
+    logger.info("Updating XML")
+    load_data(
+        cursor, 'som_crawlers', 'view/som_crawlers_config_view.xml', mode='update'
+    )
+    logger.info("XML succesfully updated.")
+
+
+def down(cursor, installed_version):
+    pass
+
+
+migrate = up
