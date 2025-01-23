db22bd6e9d263a529e8cc479a2b175606ef5db44
Fran Cubero <francisco.cubero@somenergia.coop>
2025-01-23 17:33:36 +0100

    ✅ test code for the wizard
    Co-authored-by: Marité Guerrieri <marite.guerrieri@somenergia.coop>

--- somre_ov_module/tests/__init__.py
+++ somre_ov_module/tests/__init__.py
@@ -4,3 +4,4 @@ from test_wizard_create_change_password import *
 from test_somre_ov_installations import *
 from test_somre_ov_invoices import *
 from test_somre_ov_production_data import *
+from test_wizard_create_somre_ov_users import *
