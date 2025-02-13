4cdeecb13eb296c7a9dd7680c192e46ccf50351d
Oriol Piera <oriol.piera@somenergia.coop>
2025-02-13 13:34:21 +0100

    🗃️ Fix migration script with new values in new fields. Add field in form
    Co-authored-by: Pau Boix <pau.boix@somenergia.coop>

--- som_gurb/migrations/5.0.24.5/post-0001_add_gurb_wizard.py
+++ som_gurb/migrations/5.0.24.5/post-0001_add_gurb_wizard.py
@@ -9,16 +9,24 @@ def up(cursor, installed_version):
         return
 
     logger = logging.getLogger('openerp.migration')
+    uid = 1
 
     logger.info("Creating pooler")
     pooler.get_pool(cursor.dbname)
+    pooler.get('som.gurb.cups')._auto_init(cursor, context={'module': 'som_gurb'})
+
+    # Update states of all GurbCups to active
+    sgc_obj = pooler.get('som.gurb.cups')
+    list_to_update = sgc_obj.search(cursor, uid, [('state', '=', False)])
+    for _id in list_to_update[:1]:
+        activation_date = sgc_obj.read(cursor, uid, _id, ['start_date'])['start_date']
+        sgc_obj.write(cursor, uid, _id, {'state': 'active', 'state_date': activation_date})
 
     views = [
         'wizard/wizard_deactivate_gurb_cups.xml',
         'workflow/som_gurb_cups_workflow.xml',
         'views/som_gurb_cups_view.xml',
     ]
-
     for view in views:
         # Actualitza els diferents records i vistes
         logger.info("Updating XML {}".format(view))
