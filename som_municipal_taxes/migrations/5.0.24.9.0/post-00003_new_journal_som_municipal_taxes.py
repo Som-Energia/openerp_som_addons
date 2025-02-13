30e4506f2e397ddb7dd831325c9b781ea4eedb0e
Oriol Piera <oriol.piera@somenergia.coop>
2025-02-13 09:30:47 +0100

    🗃️   Add migration script

---
+++ som_municipal_taxes/migrations/5.0.24.9.0/post-00003_new_journal_som_municipal_taxes.py
@@ -0,0 +1,31 @@
+# -*- encoding: utf-8 -*-
+import logging
+import pooler
+from oopgrade.oopgrade import load_data
+
+
+def up(cursor, installed_version):
+    if not installed_version:
+        return
+
+    logger = logging.getLogger('openerp.migration')
+
+    logger.info("Creating pooler")
+    pooler.get_pool(cursor.dbname)
+
+    views = [
+        'data/som_municipal_taxes_data.xml',
+    ]
+
+    for view in views:
+        # Crear les diferents vistes
+        logger.info("Updating XML {}".format(view))
+        load_data(cursor, 'som_municipal_taxes', view, idref=None, mode='update')
+        logger.info("XMLs succesfully updatd.")
+
+
+def down(cursor, installed_version):
+    pass
+
+
+migrate = up
