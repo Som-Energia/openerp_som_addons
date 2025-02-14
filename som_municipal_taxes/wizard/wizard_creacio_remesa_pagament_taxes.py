8ff3c2e935ca86aa4ff81a34677ed0761b056880
Oriol Piera <oriol.piera@somenergia.coop>
2025-02-14 08:15:10 +0100

    💄🗃️  Improme error message. Fix incomplete data for invoices.

--- som_municipal_taxes/wizard/wizard_creacio_remesa_pagament_taxes.py
+++ som_municipal_taxes/wizard/wizard_creacio_remesa_pagament_taxes.py
@@ -126,7 +126,9 @@ class WizardCreacioRemesaPagamentTaxes(osv.osv_memory):
         ))
 
         linia_creada = []
-        linia_no_creada = []
+        linia_falta_partner = []
+        linia_ja_creada = []
+        linia_falta_partner_address = []
         invoice_ids = []
         for city in totals_by_city:
             city_name = city[0]
@@ -136,19 +138,26 @@ class WizardCreacioRemesaPagamentTaxes(osv.osv_memory):
             quarter_name = dict(self._columns['quarter'].selection)[quarter_number]
             origin_name = '{}/{}{}'.format(city_ine, year, quarter_name)
             if invoice_obj.search(cursor, uid, [('origin', '=', origin_name)]):
-                linia_no_creada.append(city_name)
+                linia_ja_creada.append(city_name)
                 continue
 
             municipi_id = mun_obj.search(cursor, uid, [('ine', '=', city_ine)])[0]
             config_id = config_obj.search(cursor, uid, [('municipi_id', '=', municipi_id)])[0]
             config_data = config_obj.read(cursor, uid, config_id, ['partner_id', 'bank_id'])
             if not config_data['partner_id']:
-                linia_no_creada.append(config_data['name'])
-                continue
+                if config_data.get('name', False):
+                    linia_falta_partner.append(config_data['name'])
+                else:
+                    linia_falta_partner.append(str(config_data))
+                    continue
 
             # Get partner_address_id from partner_id
             partner_address_id = self.pool.get('res.partner').address_get(
                 cursor, uid, [config_data['partner_id'][0]], ['invoice'])['invoice']
+            if not partner_address_id:
+                linia_falta_partner_address.append(city_name)
+                continue
+
             # Create account.invoice objects for each city
             invoice_id = invoice_obj.create(cursor, uid, dict(
                 partner_id=config_data['partner_id'][0],
@@ -183,9 +192,16 @@ class WizardCreacioRemesaPagamentTaxes(osv.osv_memory):
 
         invoice_obj.afegeix_a_remesa(cursor, uid, invoice_ids, order_id)
         info = "S'ha creat la remesa amb {} línies\n\n".format(len(linia_creada))
-        if linia_no_creada:
-            info += """Atenció. Els següents ajuntaments no tenen un compte corrent informat
-              i per tant no s'ha pogut crear el pagament: {}""".format(", ".join(linia_no_creada))
+        if linia_falta_partner:
+            info += """Atenció. Els següents ajuntaments no tenen un partner \
+            creat: \n {}\n\n""".format(", ".join(linia_falta_partner))
+        if linia_ja_creada:
+            info += """Atenció. Els següents ajuntaments ja tenen una factura creada \
+              per aquest període:\n {}\n\n""".format(", ".join(linia_ja_creada))
+        if linia_falta_partner_address:
+            info += """Atenció. Els següents ajuntaments no tenen una adreça creada a la \
+              fitxa del partner i per tant no s'ha pogut crear la factura:\n {}\n\n""".format(
+                ", ".join(linia_falta_partner_address))
 
         return order_id, info
 
