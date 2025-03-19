8d5a7daffbfb3fc9b94fcef310e96df51ec25e2d
Oriol Piera <oriol.piera@somenergia.coop>
2025-03-19 10:07:15 +0100

    🧹🎨 (open_invoices) Clean an pre-commit

--- scripts/open_invoices.py
+++ scripts/open_invoices.py
@@ -1,71 +1,58 @@
 # -*- coding: utf-8 -*-
+from __future__ import absolute_import, division, print_function, \
+    unicode_literals
 from erppeek import Client
-from datetime import timedelta,datetime
-import tqdm
-
-now=datetime.now()
-print "========================"
-print now
-
-def last_month_invoices(now,limit=None):
-    end_date=datetime(now.year,now.month,1)-timedelta(days=1)
-    start_date=datetime(end_date.year,end_date.month,1)
-    ids = O.GiscedataFacturacioFactura.search([('type','in',['in_invoice']),
-                                               ('state','=','draft'),
-                                               ('date_invoice','>=',start_date.strftime("%Y-%m-%d")),
-                                               ('date_invoice','<=',end_date.strftime("%Y-%m-%d"))])
-    return ids[:limit]
+from datetime import timedelta, datetime
+import dbconfig
 
 
-def current_month_invoices(now,limit=None):
-    end_date=datetime(now.year,now.month,now.day)
-    start_date=datetime(end_date.year,end_date.month,1)
-    ids = O.GiscedataFacturacioFactura.search([('type','in',['in_invoice']),
-                                               ('state','=','draft'),
-                                               ('date_invoice','>=',start_date.strftime("%Y-%m-%d")),
-                                               ('date_invoice','<=',end_date.strftime("%Y-%m-%d"))])
-    return ids[:limit]
-
-
-#Canviat el 18/06/2019
-def before_3days_invoices(now, limit=None):
-    end_date=datetime(now.year,now.month,now.day)-timedelta(days=3)
-    # start_date=datetime(end_date.year,end_date.month,end_date.day)-timedelta(days=120) Canviat el 25/07/2022
-    start_date=datetime(end_date.year,end_date.month,end_date.day)-timedelta(days=240)
-    ids = O.GiscedataFacturacioFactura.search([('type','in',['in_invoice']),
-                                               ('state','=','draft'),
-                                               ('date_invoice','>=',start_date.strftime("%Y-%m-%d")),
-                                               ('date_invoice','<=',end_date.strftime("%Y-%m-%d"))])
-    #Afegit el dia 25/07/2022
-    ids_ab = O.GiscedataFacturacioFactura.search([('type','=','in_refund'), ('tipo_rectificadora','!=','BRA'),
-                                               ('state','=','draft'),
-                                               ('date_invoice','>=',start_date.strftime("%Y-%m-%d")),
-                                               ('date_invoice','<=',end_date.strftime("%Y-%m-%d"))])
+# Canviat el 18/06/2019
+def before_3days_invoices(conn, now, limit=None):
+    end_date = datetime(now.year, now.month, now.day) - timedelta(days=3)
+    # Canviat el 25/07/2022
+    # start_date=datetime(end_date.year,end_date.month,end_date.day)-timedelta(days=120)
+    start_date = datetime(end_date.year, end_date.month, end_date.day) - timedelta(days=240)
+    ids = conn.GiscedataFacturacioFactura.search(
+        [('type', 'in', ['in_invoice']),
+         ('state', '=', 'draft'),
+         ('date_invoice', '>=', start_date.strftime("%Y-%m-%d")),
+         ('date_invoice', '<=', end_date.strftime("%Y-%m-%d"))])
+    # Afegit el dia 25/07/2022
+    ids_ab = conn.GiscedataFacturacioFactura.search(
+        [('type', '=', 'in_refund'), ('tipo_rectificadora', '!=', 'BRA'),
+         ('state', '=', 'draft'),
+         ('date_invoice', '>=', start_date.strftime("%Y-%m-%d")),
+         ('date_invoice', '<=', end_date.strftime("%Y-%m-%d"))])
     ids += ids_ab
     return ids[:limit]
 
 
 def open_invoices():
-    O = Client(**dbconfig.erppeek)
-    ids = before_3days_invoices(datetime.now(),limit=10000)
+    now = datetime.now()
+    print("========================")
+    print(now)
+    conn = Client(**dbconfig.erppeek)
+    ids = before_3days_invoices(conn, datetime.now(), limit=10000)
     n_invoices = 0
     invoice_err = []
-    for id_ in tqdm.tqdm(ids):
+    for id_ in ids:
         try:
-           ctx = {'active_ids': [id_]}
-           wiz_o = O.GiscedataFacturacioSwitchingAprovarFactura
-           wizard_id = wiz_o.create({'open_1ct_diff_invoices': True, 'open_negative_invoices': False}, ctx)
-           wiz_o.action_aprovar_factures([wizard_id],ctx)
-           n_invoices = n_invoices + 1
-        except Exception, e:
-           print e
-           invoice_err.append(id_)
-           pass
+            ctx = {'active_ids': [id_]}
+            wiz_o = conn.GiscedataFacturacioSwitchingAprovarFactura
+            wizard_id = wiz_o.create({'open_1ct_diff_invoices': True,
+                                     'open_negative_invoices': False}, ctx)
+            wiz_o.action_aprovar_factures([wizard_id], ctx)
+            n_invoices = n_invoices + 1
+        except Exception as e:
+            print(e)
+            invoice_err.append(id_)
 
-    print "S'han obert un total de " + str(n_invoices) + " factures."
-    print "Factures que no s'han pogut obrir:"
-    print invoice_err
+    print("S'han obert un total de {0} factures.".format(str(n_invoices)))
+    print("Factures que no s'han pogut obrir: {0}".format(invoice_err))
+    now = datetime.now()
+    print("========================")
+    print(now)
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     open_invoices()
