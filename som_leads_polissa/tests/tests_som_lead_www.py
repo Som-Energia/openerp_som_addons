a39a96815f41ba1beabcaca981fc8a8b0ab4637e
destanyol <daniel.estanyol@somenergia.coop>
2025-04-03 16:28:34 +0200

    ✅🚧 (leads) Create selfcons preliminary test
    Co-authored-by: Àngel Fernández Ibáñez <angel.fernandez@somenergia.coop>

--- som_leads_polissa/tests/tests_som_lead_www.py
+++ som_leads_polissa/tests/tests_som_lead_www.py
@@ -392,6 +392,8 @@ class TestsSomLeadWww(testing.OOTestCase):
     def test_create_self_consumption_lead(self):
         www_lead_o = self.get_model("som.lead.www")
         lead_o = self.get_model("giscedata.crm.lead")
+        self_consumption_o = self.get_model("giscedata.autoconsum")
+        generator_o = self.get_model("giscedata.autoconsum.generador")
 
         values = self._basic_values
         values["self_consumption"] = {
@@ -409,10 +411,27 @@ class TestsSomLeadWww(testing.OOTestCase):
 
         lead = lead_o.browse(self.cursor, self.uid, lead_id)
 
+        # Buscar el CAU
+        self_consumption_ids = self_consumption_o.search(
+            self.cursor, self.uid, [("cau", '=', values["self_consumption"]["cau"])]
+        )
+        self.assertEqual(len(self_consumption_ids), 1)
+
+        # Look if there is our CUPS in CAU
+        self_consumption_cups = self_consumption_o.browse(
+            self.cursor, self.uid, self_consumption_ids[0]
+        )
+        self.assertEqual(self_consumption_cups.cups_id[0].name[:-2], values["cups"])
+
+        # A Generator for our CAU exists
+        generator_ids = generator_o.search(
+            self.cursor, self.uid, [("autoconsum_id", "=", self_consumption_ids[0])]
+        )
+        self.assertEqual(len(generator_ids), 1)
+
+        # Check Contract fields
+        self.assertNotEqual(lead.polissa_id.tipus_subseccio, "00")
         self.assertEqual(lead.polissa_id.autoconsumo, '41')
 
-        # Buscar el CAU i mirar els CUPS adherits
-
-        # Mirar que el tipus_subsecció de la polissa no és 00
-
         # Que surt al ATR que és un contracte amb autoconsum
+        # TODO
