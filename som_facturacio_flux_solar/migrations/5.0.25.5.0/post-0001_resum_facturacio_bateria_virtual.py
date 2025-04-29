7d3d8f592797725db1d4e303626989d1effa2dd5
destanyol <daniel.estanyol@somenergia.coop>
2025-04-29 15:16:11 +0200

    🐛 PRE-Commit

--- som_facturacio_flux_solar/migrations/5.0.25.5.0/post-0001_resum_facturacio_bateria_virtual.py
+++ som_facturacio_flux_solar/migrations/5.0.25.5.0/post-0001_resum_facturacio_bateria_virtual.py
@@ -13,9 +13,9 @@ def up(cursor, installed_version):
 
     logger.info("Creating pooler")
     pool = pooler.get_pool(cursor.dbname)
-_
+
     pool.get("giscedata.bateria.virtual.resum.facturacio").init(cursor)
-    ##UPDATAR UN MODUL NOU AL CREAR-LO O AFEGIR UNA COLUMNA##
+    # UPDATAR UN MODUL NOU AL CREAR-LO O AFEGIR UNA COLUMNA##
     logger.info("Creating table: giscedata.bateria.virtual.resum.facturacio")
     pool.get("giscedata.bateria.virtual.resum.facturacio")._auto_init(cursor, context={
         'module': 'som_facturacio_flux_solar'})
