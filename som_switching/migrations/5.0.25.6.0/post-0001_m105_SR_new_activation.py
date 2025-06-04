f88f40f5cbd3e1fa10aa712693ab59fdcb6ad9c2
pedropote <pere.montagud@somenergia.coop>
2025-06-04 12:13:18 +0200

    🔨 Migration script to add new ATR activation

---
+++ som_switching/migrations/5.0.25.6.0/post-0001_m105_SR_new_activation.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+import logging
+from oopgrade.oopgrade import load_data_records
+
+
+def up(cursor, installed_version):
+    if not installed_version:
+        return
+
+    logger = logging.getLogger('openerp.migration')
+
+    logger.info("Updating XMLs")
+    list_of_records = [
+        "sw_act_m105_acord_repartiment_autoconsum",
+    ]
+    load_data_records(
+        cursor,
+        'som_switching',
+        'giscedata_switching_activation_data.xml',
+        list_of_records,
+        mode='update',
+    )
+
+
+def down(cursor, installed_version):
+    pass
+
+
+migrate = up
