0008476f2190cd2ee7fa2111bf5913bb026b74bd
Oriol Piera <oriol.piera@somenergia.coop>
2025-06-06 12:40:06 +0200

    ♻️  Move classes to models/ folder

--- som_switching/models/giscedata_switching.py
+++ som_switching/models/giscedata_switching.py
@@ -150,46 +150,6 @@ class GiscedataSwitchingAparell(osv.osv):
 GiscedataSwitchingAparell()
 
 
-class GiscedataSwitchingM1_05(osv.osv):
-    """Classe per gestionar el canvi de comercialitzador"""
-
-    _name = "giscedata.switching.m1.05"
-    _inherit = "giscedata.switching.m1.05"
-
-    def create(self, cursor, uid, values, context=None):
-        res_id = super(GiscedataSwitchingM1_05, self).create(cursor, uid, values, context=context)
-        # Forcem el recomput del camp function
-        obj = self.browse(cursor, uid, res_id, context=context)
-        if obj.dades_cau:
-            self.pool.get('giscedata.switching')._store_set_values(
-                cursor, uid, [obj.sw_id.id], ['collectiu_atr'], context)
-
-        return res_id
-
-
-GiscedataSwitchingM1_05()
-
-
-class GiscedataSwitchingD1_01(osv.osv):
-    """Classe per gestionar el canvi de comercialitzador"""
-
-    _name = "giscedata.switching.d1.01"
-    _inherit = "giscedata.switching.d1.01"
-
-    def create(self, cursor, uid, values, context=None):
-        res_id = super(GiscedataSwitchingD1_01, self).create(cursor, uid, values, context=context)
-        # Forcem el recomput del camp function
-        obj = self.browse(cursor, uid, res_id, context=context)
-        if obj.dades_cau:
-            self.pool.get('giscedata.switching')._store_set_values(
-                cursor, uid, [obj.sw_id.id], ['collectiu_atr'], context)
-
-        return res_id
-
-
-GiscedataSwitchingD1_01()
-
-
 class GiscedataSwitching(osv.osv):
     """Classe per gestionar el canvi de comercialitzador"""
 
