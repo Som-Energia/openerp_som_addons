5674dede8a519d7422618dd2a088acdee275cff7
pauboixsom <pau.boix@somenergia.coop>
2025-06-12 12:14:32 +0200

    🗃️ auto  migration script

---
+++ som_crawlers/migrations/5.0.24.5.0/post-0003_NEW_COELCA_crawler_update_views_and_fields.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+import logging
+from oopgrade.oopgrade import load_data
+
+
+def up(cursor, installed_version):
+    if not installed_version:
+        return
+
+    logger = logging.getLogger('openerp.migration')
+
+    logger.info("Updating XML files")
+    data_files = [
+        'data/som_crawlers_config_data.xml',
+        'data/som_crawlers_step_data.xml',
+        'data/som_crawlers_task_data.xml',
+    ]
+    for data_file in data_files:
+        load_data(
+            cursor, 'som_crawlers', data_file,
+            idref=None, mode='update'
+        )
+
+    logger.info("Migration completed successfully.")
+
+
+def down(cursor, installed_version):
+    pass
+
+
+migrate = up
