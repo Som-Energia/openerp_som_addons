b9a6c5b1b4e29a7422618080f42fe7a976767fc1
Fran Cubero <francisco.cubero@somenergia.coop>
2025-07-14 16:03:55 +0200

    🔨 migration script

---
+++ som_autoreclama/migrations/5.0.24.5.0/post-0003_FIX_aom_autoreclama_f1c_automation_crontab_miss_migrate_data.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+import logging
+from oopgrade.oopgrade import load_data_records
+
+
+def up(cursor, installed_version):
+    if not installed_version:
+        return
+
+    logger = logging.getLogger('openerp.migration')
+
+    logger.info("Updating XML files")
+    data_files = [
+
+    ]
+    for data_file in data_files:
+        load_data_records(
+            cursor,
+            'som_autoreclama',
+            'som_autoreclama_state_data.xml',
+            ["ir_cron_autoreclama_ifc_automation"],
+            mode='update'
+        )
+
+    logger.info("Migration completed successfully.")
+
+
+def down(cursor, installed_version):
+    pass
+
+
+migrate = up
