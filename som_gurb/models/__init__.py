59083e77c1275ff0607606a307e3afd0adae4f9c
destanyol <daniel.estanyol@somenergia.coop>
2025-07-22 11:34:29 +0200

    🐛 (lead) Delete import of future migration

--- som_gurb/models/__init__.py
+++ som_gurb/models/__init__.py
@@ -3,7 +3,6 @@ from __future__ import absolute_import
 
 from . import som_gurb_cau
 from . import som_gurb_cups
-from . import som_gurb_group
 from . import giscedata_switching
 from . import giscedata_facturacio
 from . import report_backend_som_gurb_documents
