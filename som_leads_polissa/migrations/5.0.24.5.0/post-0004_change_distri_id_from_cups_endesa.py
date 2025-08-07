e790e39673d52201320bec275173eb48e4d22cd6
Àngel Fernández <angel.fernandez@somenergia.coop>
2025-08-07 15:04:33 +0200

    🐛 Fix migration script

--- som_leads_polissa/migrations/5.0.24.5.0/post-0004_change_distri_id_from_cups_endesa.py
+++ som_leads_polissa/migrations/5.0.24.5.0/post-0004_change_distri_id_from_cups_endesa.py
@@ -7,17 +7,20 @@ def up(cursor, installed_version):
     if not installed_version:
         return
 
+    uid = 1
+
     logger = logging.getLogger('openerp.migration')
     pool = pooler.get_pool(cursor.dbname)
 
     logger.info("Initializing new fields")
-    endesa_id = pool.get("res.partner").search([('ref', '=', '0031')])
-    llista_cups_a_canviar = pool.get("giscedata.cups.ps").search([('name', 'ilike', 'ES0031')])
+    endesa_id = pool.get("res.partner").search(cursor, uid, [('ref', '=', '0031')])[0]
+    llista_cups_a_canviar = pool.get("giscedata.cups.ps").search(
+        cursor, uid, [('name', 'ilike', 'ES0031')])
     for cups_distri_id in pool.get("giscedata.cups.ps").read(
-            llista_cups_a_canviar, ['distribuidora_id']):
+            cursor, uid, llista_cups_a_canviar, ['distribuidora_id']):
         if cups_distri_id['distribuidora_id'][0] != endesa_id:
             pool.get("giscedata.cups.ps").write(
-                cups_distri_id['id'], {'distribuidora_id': endesa_id})
+                cursor, uid, cups_distri_id['id'], {'distribuidora_id': endesa_id})
     logger.info("Migration completed successfully.")
 
 
