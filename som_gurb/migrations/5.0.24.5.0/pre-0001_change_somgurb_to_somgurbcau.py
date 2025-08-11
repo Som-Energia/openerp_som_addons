448c41bf979c4081f7b0c282c764fc0430954bd8
pauboixsom <pau.boix@somenergia.coop>
2025-08-11 15:50:18 +0200

    🐛 Fix migration script
    Co-authored-by: Daniel Estanyol i Torres <daniel.estanyol@somenergia.coop>

--- som_gurb/migrations/5.0.24.5.0/pre-0001_change_somgurb_to_somgurbcau.py
+++ som_gurb/migrations/5.0.24.5.0/pre-0001_change_somgurb_to_somgurbcau.py
@@ -16,7 +16,7 @@ def up(cursor, installed_version):
                                    ('som_gurb', 'first_opening_days'),
                                    ('som_gurb', 'max_power'),
                                    ('som_gurb', 'min_power')])
-    oopgrade.rename_tables(cursor, ['som_gurb', 'som_gurb_cau'])
+    oopgrade.rename_tables(cursor, [('som_gurb', 'som_gurb_cau')])
 
     logger.info("NICE!! SERVERS UP!!")
 
