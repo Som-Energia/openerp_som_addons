2f02c0f3325ac87c051d9ef58c18c33541b75442
destanyol <daniel.estanyol@somenergia.coop>
2025-10-22 16:55:10 +0200

    👔 (GURB) Support F1s after april 2025
    Co-authored-by: Pau Boix <pau.boix@somenergia.coop>

--- som_gurb/wizard/wizard_calculate_gurb_savings.py
+++ som_gurb/wizard/wizard_calculate_gurb_savings.py
@@ -7,7 +7,7 @@ class WizardCalculateGurbSavings(osv.osv_memory):
     _name = "wizard.calculate.gurb.savings"
     _description = "Wizard per calcular l'estalvi d'un Gurb Cups"
 
-    def calculate_gurb_savings(self, cursor, uid, ids, context=None):
+    def calculate_gurb_savings(self, cursor, uid, ids, context=None):  # noqa: C901
         if context is None:
             context = {}
 
@@ -52,6 +52,8 @@ class WizardCalculateGurbSavings(osv.osv_memory):
             # del f1
             linies_autoconsum_ids = gffl_obj.search(cursor, uid, ([("factura_id", "=", f1_id),
                                                                    ("tipus", "=", "autoconsum")]))
+            linies_genneta_ids = gffl_obj.search(cursor, uid, ([("factura_id", "=", f1_id),
+                                                                ("tipus", "=", "generacio_neta")]))
 
             f1 = gff_obj.browse(cursor, uid, f1_id)
             # de la gff
@@ -77,15 +79,26 @@ class WizardCalculateGurbSavings(osv.osv_memory):
                                                                   ("tipus", "=", "generacio")]))
 
             # buscar la gff a partir del date to i from del f1?
-
-            linies_autoconsum = gffl_obj.browse(cursor, uid, linies_autoconsum_ids)
             linies_generacio = gffl_obj.browse(cursor, uid, linies_generacio_ids)
+            if linies_autoconsum_ids:
+                linies_autoconsum = gffl_obj.browse(cursor, uid, linies_autoconsum_ids)
+            else:
+                linies_genneta = gffl_obj.browse(cursor, uid, linies_genneta_ids)
+                linies_gen_dict = {'P1': 0, 'P2': 0, 'P3': 0, 'P4': 0, 'P5': 0, 'P6': 0}
+                for linia_gen in linies_generacio:
+                    linies_gen_dict[linia_gen.name] = linia_gen.quantity
             linies_gurb = gffl_obj.browse(cursor, uid, linies_gurb_ids)
             linies_energia = gffl_obj.browse(cursor, uid, linies_energia_ids)
 
             total_auto = {'P1': 0, 'P2': 0, 'P3': 0, 'P4': 0, 'P5': 0, 'P6': 0}
-            for linia_autoconsum in linies_autoconsum:
-                total_auto[linia_autoconsum.name] += linia_autoconsum.quantity
+            if linies_autoconsum_ids:
+                for linia_autoconsum in linies_autoconsum:
+                    total_auto[linia_autoconsum.name] += linia_autoconsum.quantity
+            else:
+                for linia_genneta in linies_genneta:
+                    total_auto[linia_genneta.name] += (
+                        linia_genneta.quantity - linies_gen_dict[linia_genneta.name]
+                    )
 
             auto_kwh = sum(total_auto.values())
             total_energia = {'P1': 0, 'P2': 0, 'P3': 0, 'P4': 0, 'P5': 0, 'P6': 0}
