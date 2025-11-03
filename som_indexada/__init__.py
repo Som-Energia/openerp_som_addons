89196d104ff284d3c7538af34b64898dad9a4be5
Oriol Piera <oriol.piera@somenergia.coop>
2025-11-03 12:30:01 +0100

    🐛 som_indexada: exceptions disapear long time ago

--- som_indexada/__init__.py
+++ som_indexada/__init__.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import
 from . import wizard
-from . import exceptions
 from . import giscedata_polissa
 from . import giscedata_polissa_modcontractual
 from . import som_polissa_k_change
