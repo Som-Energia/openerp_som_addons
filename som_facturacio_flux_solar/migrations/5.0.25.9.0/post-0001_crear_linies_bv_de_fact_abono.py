19bda209b7983f56c7589bdd0293ad981a45ae21
Emili García <egarcia@gisce.net>
2025-11-11 11:17:45 +0100

    Fixes pre-commit

--- som_facturacio_flux_solar/migrations/5.0.25.9.0/post-0001_crear_linies_bv_de_fact_abono.py
+++ som_facturacio_flux_solar/migrations/5.0.25.9.0/post-0001_crear_linies_bv_de_fact_abono.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 import logging
 import pooler
-from tools import config
 
 
 def up(cursor, installed_version):
@@ -15,19 +14,21 @@ def up(cursor, installed_version):
     fact_obj = pool.get("giscedata.facturacio.factura")
     uid = 1
 
-    n_factures_procesades, factures_no_processades = fact_obj.crear_descomptes_bv_factures_anulladores(cursor, uid)
+    n_facts, facts_no_processades = fact_obj.crear_descomptes_bv_factures_anulladores(cursor, uid)
 
-    if factures_no_processades:
+    if facts_no_processades:
         msg_process = "Proces finalizat amb avisos"
     else:
         msg_process = "Proces finalizat"
-    n_factures_no_processades = len(factures_no_processades)
-    n_total_factures = n_factures_procesades + n_factures_no_processades
+    n_facts_no_processades = len(facts_no_processades)
+    n_total_factures = n_facts + n_facts_no_processades
     logger.info("{}. S'ha processat {} factures anulladores"
-                " de {}.".format(msg_process, n_factures_procesades, n_total_factures))
-    if n_factures_no_processades:
-        logger.info("Per {} factures anulladores no s'ha pogut crear les linies de descompte de bateteria virtual,"
-                    " els ids son: {}".format(n_factures_no_processades, factures_no_processades))
+                " de {}.".format(msg_process, n_facts, n_total_factures))
+    if n_facts_no_processades:
+        logger.info("Per {} factures anulladores no s'ha pogut crear les linies de descompte de "
+                    "bateteria virtual, els ids son: {}".format(
+            n_facts_no_processades, facts_no_processades
+        ))
 
 
 def down(cursor, installed_version):
