cb3a3607aab40716fc874e547abf5acdfc9541f5
pedropote <pere.montagud@somenergia.coop>
2025-11-12 14:32:22 +0100

    ♻️  Refactor multipunt contract simulation calculation

--- som_indexada/som_polissa_k_change.py
+++ som_indexada/som_polissa_k_change.py
@@ -2,7 +2,7 @@
 from osv import osv, fields
 from tools.translate import _
 
-from . import utils
+from utils import calculate_new_indexed_prices
 
 import logging
 
@@ -13,21 +13,12 @@ class SomPolissaKChange(osv.osv):
     _name = "som.polissa.k.change"
     _description = _("Canvi de K de pòlissa")
 
-    def calculate_multipunt_values(self, cursor, uid, context=None):
-        if context is None:
-            context = {}
-
+    def compute_simulation(self, cursor, uid, ids, context=None):
         pol_obj = self.pool.get('giscedata.polissa')
-
-        search_params = [('partner_id', '!=', False)]
-
-        k_change_ids = self.search(cursor, uid, search_params, context=context)
-
-        for k_change in k_change_ids:
-            partner_id = self.read(
-                cursor, uid, k_change, ['partner_id'], context=context
-            )['partner_id'][0]
-
+        for record in self.browse(cursor, uid, ids, context):
+            if not record.partner_id:
+                continue
+            partner_id = record.partner_id.id
             import_total_anual_nova_amb_impost = 0
             import_total_anual_antiga_amb_impost = 0
             pol_ids = pol_obj.search(cursor, uid, [
@@ -36,11 +27,10 @@ class SomPolissaKChange(osv.osv):
             ])
             for pol in pol_ids:
                 context['partner_id'] = partner_id
-                pol_browse = pol_obj.browse(cursor, uid, pol, context=context)
-                data = utils.calculate_new_eie_indexed_prices(
-                    cursor, uid, pol_browse, context=context
+                pol = pol_obj.browse(cursor, uid, pol, context=context)
+                data = calculate_new_indexed_prices(
+                    cursor, uid, pol, is_eie=True, context=context
                 )
-
                 import_total_anual_nova_amb_impost += data['import_total_anual_nova_amb_impost']
                 import_total_anual_antiga_amb_impost += data['import_total_anual_antiga_amb_impost']
 
@@ -48,7 +38,16 @@ class SomPolissaKChange(osv.osv):
                 'import_total_anual_nova_amb_impost': import_total_anual_nova_amb_impost,
                 'import_total_anual_antiga_amb_impost': import_total_anual_antiga_amb_impost
             }
-            self.write(cursor, uid, [k_change], write_vals, context=context)
+            self.write(cursor, uid, [record.id], write_vals, context=context)
+
+    def calculate_multipunt_values(self, cursor, uid, context=None):
+        if context is None:
+            context = {}
+
+        search_params = [('partner_id', '!=', False)]
+        k_change_ids = self.search(cursor, uid, search_params, context=context)
+
+        self.compute_simulation(cursor, uid, k_change_ids, context=context)
 
     _columns = {
         "polissa_id": fields.many2one("giscedata.polissa", "Pòlissa"),
