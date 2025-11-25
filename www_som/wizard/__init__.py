702089e9a3e310710a211621bd97ac7dfb3fc23d
Oriol Piera <oriol.piera@somenergia.coop>
2025-11-25 13:48:02 +0100

    ✨ add wizard to change to social and to regular

--- www_som/wizard/__init__.py
+++ www_som/wizard/__init__.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import
 from . import wizard_model_list_from_file
-from . import wizard_change_tariff
+from . import wizard_change_tariff_social
