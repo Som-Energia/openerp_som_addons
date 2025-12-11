fedd4062938777ce9a13760289b6ca5e3a05836a
Oriol Piera <oriol.piera@somenergia.coop>
2025-12-11 11:59:33 +0100

    🗃️ migration script

---
+++ www_som/migrations/5.0.24.5.0/post-0004_ADD_wizard_change_tariff_migrate_fields_data_security.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+import logging
+from oopgrade.oopgrade import load_data
+
+
+def up(cursor, installed_version):
+    if not installed_version:
+        return
+
+    logger = logging.getLogger('openerp.migration')
+
+    logger.info("Updating XML files")
+    data_files = [
+        'wizard/wizard_change_tariff_social_view.xml',
+    ]
+    for data_file in data_files:
+        load_data(
+            cursor, 'www_som', data_file,
+            idref=None, mode='update'
+        )
+
+    logger.info("Updating CSV security files")
+    security_files = [
+        'security/ir.model.access.csv',
+    ]
+    for security_file in security_files:
+        load_data(
+            cursor, 'www_som', security_file,
+            idref=None, mode='update'
+        )
+    logger.info("Migration completed successfully.")
+
+
+def down(cursor, installed_version):
+    pass
+
+
+migrate = up
