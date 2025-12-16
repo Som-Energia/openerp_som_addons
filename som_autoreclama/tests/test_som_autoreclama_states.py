b4d6cb3b0346ceda73a8c0f9750a71e72c369515
Fran Cubero <francisco.cubero@somenergia.coop>
2025-12-16 16:58:49 +0100

    🚧 testing

--- som_autoreclama/tests/test_som_autoreclama_states.py
+++ som_autoreclama/tests/test_som_autoreclama_states.py
@@ -4,6 +4,7 @@ from destral.transaction import Transaction
 from datetime import date, timedelta, datetime
 from addons import get_module_resource
 import mock
+import unittest
 from ..models import giscedata_atc, giscedata_polissa, som_autoreclama_state_history
 
 
@@ -3101,6 +3102,7 @@ class SomAutoreclama009AutomationTest(SomAutoreclamaEzATC_Test):
         msg = u"Error en la creació del CAC R1 009, ja n'hi ha un CAC  009 en estat obert o pendent amb id {}!!!".format(atc_id)  # noqa: E501
         self.assertTrue(msg in context.exception.message)
 
+    """
     def test_create_ATC_R1_009_from_polissa_via_wizard__previous_009(self):
         atc_obj = self.get_model("giscedata.atc")
         pol_obj = self.get_model("giscedata.polissa")
@@ -3249,6 +3251,7 @@ class SomAutoreclama009AutomationTest(SomAutoreclamaEzATC_Test):
         self.assertEqual(atc.name, u"AUTOCAC 009")
 
     """
+
     def test_create_ATC_R1_009_from_polissa_via_wizard__tag_one_indexed(self):
         atc_obj = self.get_model("giscedata.atc")
         pol_obj = self.get_model("giscedata.polissa")
@@ -3483,4 +3486,3 @@ class SomAutoreclama009AutomationTest(SomAutoreclamaEzATC_Test):
         self.assertEqual(atc.tag.id, tag_id)
 
     # TODO: test NF readings
-    """
