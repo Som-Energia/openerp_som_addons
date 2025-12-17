3480362da5ba12fb1ffc11738637e659dba00775
Fran Cubero <francisco.cubero@somenergia.coop>
2025-12-17 10:31:36 +0100

    🚧 testing

--- som_autoreclama/tests/test_som_autoreclama_states.py
+++ som_autoreclama/tests/test_som_autoreclama_states.py
@@ -3094,7 +3094,6 @@ class SomAutoreclama009AutomationTest(SomAutoreclamaEzATC_Test):
         msg = u"Error en la creació del CAC R1 009, ja n'hi ha un CAC  009 en estat obert o pendent amb id {}!!!".format(atc_id)  # noqa: E501
         self.assertTrue(msg in context.exception.message)
 
-    """
     def test_create_ATC_R1_009_from_polissa_via_wizard__previous_009(self):
         atc_obj = self.get_model("giscedata.atc")
         pol_obj = self.get_model("giscedata.polissa")
@@ -3144,6 +3143,8 @@ class SomAutoreclama009AutomationTest(SomAutoreclamaEzATC_Test):
         msg = u"Error en la creació del CAC R1 009, ja n'hi ha un CAC  009 en estat obert o pendent amb id {}!!!".format(atc_id)  # noqa: E501
         self.assertTrue(msg in context.exception.message)
 
+
+"""
     def test_create_ATC_R1_009_from_polissa_via_wizard__previous_036(self):
         atc_obj = self.get_model("giscedata.atc")
         pol_obj = self.get_model("giscedata.polissa")
@@ -3476,4 +3477,4 @@ class SomAutoreclama009AutomationTest(SomAutoreclamaEzATC_Test):
         self.assertEqual(atc.tag.id, tag_id)
 
     # TODO: test NF readings
-    """
+"""
