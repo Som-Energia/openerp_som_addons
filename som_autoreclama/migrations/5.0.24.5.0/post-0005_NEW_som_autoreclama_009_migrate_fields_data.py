e3b6539e8f91d4fa400ab742c8c7c71c1deb3c8f
Fran Cubero <francisco.cubero@somenergia.coop>
2025-12-22 17:24:22 +0100

    🔨 updated migration script

--- som_autoreclama/migrations/5.0.24.5.0/post-0005_NEW_som_autoreclama_009_migrate_fields_data.py
+++ som_autoreclama/migrations/5.0.24.5.0/post-0005_NEW_som_autoreclama_009_migrate_fields_data.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import logging
 from oopgrade.oopgrade import load_data
+import pooler
 
 
 def up(cursor, installed_version):
@@ -11,13 +12,54 @@ def up(cursor, installed_version):
 
     logger.info("Initializing new fields")
 
+    pool = pooler.get_pool(cursor.dbname)
+    pool.get("giscedata.atc")._auto_init(
+        cursor, context={'module': 'som_autoreclama'}
+    )
+    pool.get("giscedata.polissa")._auto_init(
+        cursor, context={'module': 'som_autoreclama'}
+    )
+    pool.get("som.autoreclama.state")._auto_init(
+        cursor, context={'module': 'som_autoreclama'}
+    )
+    pool.get("som.autoreclama.state.history.atc")._auto_init(
+        cursor, context={'module': 'som_autoreclama'}
+    )
+    pool.get("som.autoreclama.state.history.polissa")._auto_init(
+        cursor, context={'module': 'som_autoreclama'}
+    )
+    pool.get("som.autoreclama.state.history.polissa009")._auto_init(
+        cursor, context={'module': 'som_autoreclama'}
+    )
+    pool.get("som.autoreclama.state.workflow")._auto_init(
+        cursor, context={'module': 'som_autoreclama'}
+    )
+    pool.get("wizard.som.autoreclama.set.correct.state")._auto_init(
+        cursor, context={'module': 'som_autoreclama'}
+    )
+
     logger.info("Updating XML files")
     data_files = [
         'data/giscedata_atc_tag_data.xml',
+        'data/som_autoreclama_state_data.xml',
+        'views/giscedata_atc_view.xml',
+        'views/giscedata_polissa_view.xml',
+        'views/som_autoreclama_state_view.xml',
+        'wizard/wizard_som_autoreclama_set_correct_state_view.xml',
     ]
     for data_file in data_files:
         load_data(
             cursor, 'som_autoreclama', data_file,
+            idref=None, mode='update'
+        )
+
+    logger.info("Updating CSV security files")
+    security_files = [
+        'security/ir.model.access.csv',
+    ]
+    for security_file in security_files:
+        load_data(
+            cursor, 'som_autoreclama', security_file,
             idref=None, mode='update'
         )
     logger.info("Migration completed successfully.")
