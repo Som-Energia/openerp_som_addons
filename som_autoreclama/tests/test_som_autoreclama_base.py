34754c04a3faf62a5531ece546870623fd38891e
Fran Cubero <francisco.cubero@somenergia.coop>
2026-01-09 17:27:01 +0100

    ✅ new test code

--- som_autoreclama/tests/test_som_autoreclama_base.py
+++ som_autoreclama/tests/test_som_autoreclama_base.py
@@ -134,6 +134,7 @@ class SomAutoreclamaBaseTests(testing.OOTestCase):
         initial_state=None,
         data_baixa=None,
         data_baixa_from_today=None,
+        pool_readings=None
     ):
         f1i_obj = self.get_model("giscedata.polissa.f1.info")
         h_obj = self.get_model("som.autoreclama.state.history.polissa")
@@ -189,29 +190,87 @@ class SomAutoreclamaBaseTests(testing.OOTestCase):
 
             pol_obj.write(self.cursor, self.uid, polissa_id, vals)
 
+        if pool_readings:
+            meter_obj = self.get_model("giscedata.lectures.comptador")
+            meter_id = meter_obj.create(self.cursor, self.uid, {
+                'polissa': polissa_id,
+                'name': 'Comptador prova',
+                'data_alta': '1977-08-26',
+            })
+            pl_obj = self.get_model("giscedata.lectures.lectura.pool")
+            for pool_reading in pool_readings:
+                pl_obj.create(self.cursor, self.uid, {
+                    'comptador': meter_id,
+                    'name': self.get_lectura_date(pool_reading),
+                    'lectura': self.get_lectura(pool_reading),
+                    'origen_id': self.get_origen_id(pool_reading),
+                    'periode': self.get_periode_id(pool_reading),
+                    'tipus': 'A',
+                })
+
         return polissa_id
+
+    def get_lectura_date(self, pool_reading):
+        if 'date' in pool_reading:
+            return pool_reading['date']
+
+        if 'days_ago' in pool_reading:
+            return today_minus_str(pool_reading['days_ago'])
+
+        return today_str()
+
+    def get_lectura(self, pool_reading):
+        return pool_reading.get('lectura', 1000)
+
+    def get_origen_id(self, pool_reading):
+        origen = pool_reading.get('origen', '10')
+        if origen == 'real':
+            origen = '10'
+        elif origen in ['est', 'estimada']:
+            origen = '40'
+
+        return self.search_in("giscedata.lectures.origen", [("codi", "=", origen)])
+
+    def get_periode_id(self, pool_reading):
+        periode = pool_reading.get('periode', 'P1')
+        return self.search_in("giscedata.polissa.tarifa.periodes", [("name", "=", periode)])
 
     def add_done_006_to_polissa(self, pol_id, days):
         dat = today_minus_str(days)
         return self.build_atc(subtype="006", state='done', date_closed=dat, date=dat, r1=False)
 
-    def add_correct_to_history(self, pol_id, days, atc_id=None):
+    def add_done_009_to_polissa(self, pol_id, days):
+        dat = today_minus_str(days)
+        return self.build_atc(subtype="009", state='done', date_closed=dat, date=dat, r1=False)
+
+    def add_to_history_base(self, pol_id, days, atc_id, model_name, state_semantic_id):
         imd_obj = self.get_model("ir.model.data")
-        polh_obj = self.get_model("som.autoreclama.state.history.polissa")
+        polh_obj = self.get_model(model_name)
         correct_state_id = imd_obj.get_object_reference(
-            self.cursor, self.uid, "som_autoreclama", "correct_state_workflow_polissa"
+            self.cursor, self.uid, "som_autoreclama", state_semantic_id
         )[1]
         polh_obj.historize(
             self.cursor, self.uid, pol_id, correct_state_id, today_minus_str(days), atc_id)
 
+    def add_correct_to_history(self, pol_id, days, atc_id=None):
+        self.add_to_history_base(pol_id, days, atc_id,
+                                 "som.autoreclama.state.history.polissa",
+                                 "correct_state_workflow_polissa")
+
+    def add_correct_to_history009(self, pol_id, days, atc_id=None):
+        self.add_to_history_base(pol_id, days, atc_id,
+                                 "som.autoreclama.state.history.polissa009",
+                                 "correct_state_workflow_polissa009")
+
     def add_review_to_history(self, pol_id, days, atc_id=None):
-        imd_obj = self.get_model("ir.model.data")
-        polh_obj = self.get_model("som.autoreclama.state.history.polissa")
-        review_state_id = imd_obj.get_object_reference(
-            self.cursor, self.uid, "som_autoreclama", "review_state_workflow_polissa"
-        )[1]
-        polh_obj.historize(
-            self.cursor, self.uid, pol_id, review_state_id, today_minus_str(days), atc_id)
+        self.add_to_history_base(pol_id, days, atc_id,
+                                 "som.autoreclama.state.history.polissa",
+                                 "review_state_workflow_polissa")
+
+    def add_review_to_history009(self, pol_id, days, atc_id=None):
+        self.add_to_history_base(pol_id, days, atc_id,
+                                 "som.autoreclama.state.history.polissa009",
+                                 "review_state_workflow_polissa009")
 
     def get_number_of_db_connections(self):
         sql = "SELECT count(*) FROM pg_stat_activity;"
