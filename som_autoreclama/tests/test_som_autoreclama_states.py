cfb6176a7d40d3ed6b28684aac1c447351edc877
Fran Cubero <francisco.cubero@somenergia.coop>
2026-03-23 11:40:16 +0100

    ✅🐛 typo

--- som_autoreclama/tests/test_som_autoreclama_states.py
+++ som_autoreclama/tests/test_som_autoreclama_states.py
@@ -1095,7 +1095,6 @@ class SomAutoreclamaConditionsTest(SomAutoreclamaBaseTests):
             if cond.subtype_id.name in ["006", "009", "036"]:  # unsuported
                 continue
 
-
             # test more
             atc_id = self.build_atc(subtype=cond.subtype_id.name, log_days=cond.days * 2, r1=True)
             atc_data = atc_obj.get_autoreclama_data(self.cursor, self.uid, atc_id, "atc", {})
