39e370f4ce72c4fcf5bddea74b8c8595b5762312
Àngel Fernández <angel.fernandez@somenergia.coop>
2026-04-28 16:11:22 +0200

    ♻ Remove hardcoded PRICE_CHANGE_DATE + fix eie

--- som_polissa_condicions_generals/report_backend_mailcanvipreus_eie.py
+++ som_polissa_condicions_generals/report_backend_mailcanvipreus_eie.py
@@ -53,27 +53,23 @@ class ReportBackendMailcanvipreusEiE(ReportBackend):
 
         return data
 
-    def getImpostos(self, fiscal_position, context=False):
-        imp_str = "IVA del 10%" if context and context.get('iva10') else "IVA del 21%"
-        imp_value = 21
-        if fiscal_position:
-            if fiscal_position.id in [33, 47, 56, 52, 61, 38, 21, 19, 87, 89, 94]:
-                imp_str = "IGIC del 3%"
-                imp_value = 3
-            if fiscal_position.id in [34, 48, 53, 57, 53, 62, 39, 25, 88, 90]:
-                imp_str = "IGIC del 0%"
-                imp_value = 0
-        return imp_str, float(imp_value)
+    def get_iva_text(self, context=None):
+        if 'IGIC' in self.simplified_taxes:
+            return 'IGIC del {}%'.format(int(self.simplified_taxes['IGIC'] * 100))
+        else:
+            return 'IVA del {}%'.format(int(self.simplified_taxes['IVA'] * 100))
 
     @report_browsify
     def get_data(self, cursor, uid, env, context=None):
         imd_obj = self.pool.get('ir.model.data')
+        pol_obj = self.pool.get("giscedata.polissa")
         if context is None:
             context = {}
 
-        impostos_str, impostos_value = self.getImpostos(env.polissa_id.fiscal_position_id, context)
+        self.simplified_taxes = pol_obj.get_simplified_taxes(
+            cursor, uid, env.polissa_id.id, context=context)
 
-        if impostos_str == 'IVA del 10%':
+        if 'IVA' in self.simplified_taxes and self.simplified_taxes['IVA'] < 0.21:
             fp_id = imd_obj.get_object_reference(
                 cursor, uid, 'som_polissa_condicions_generals', 'fp_iva_reduit')[1]
             context.update({'force_fiscal_position': fp_id})
