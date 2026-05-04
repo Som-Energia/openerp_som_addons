14bf6cf09151f23a2c7ab713a5f6bf7982fad306
Pau Boix <pau.boix@somenergia.coop>
2026-05-04 15:47:02 +0200

    🗃️ auto migration script

---
+++ som_crawlers/migrations/5.0.25.5.0/post-0002_NEW_cide_api_migrate_fields_data.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+import logging
+from oopgrade.oopgrade import load_data
+import pooler
+
+
+def up(cursor, installed_version):
+    if not installed_version:
+        return
+
+    logger = logging.getLogger('openerp.migration')
+
+    logger.info("Initializing new fields")
+
+    pool = pooler.get_pool(cursor.dbname)
+    pool.get("som.crawlers.task.step")._auto_init(
+        cursor, context={'module': 'som_crawlers'}
+    )
+
+    logger.info("Updating XML files")
+    data_files = [
+        'data/som_crawlers_config_data.xml',
+        'data/som_crawlers_step_data.xml',
+        'data/som_crawlers_task_data.xml',
+    ]
+    for data_file in data_files:
+        load_data(
+            cursor, 'som_crawlers', data_file,
+            idref=None, mode='update'
+        )
+    logger.info("Migration completed successfully.")
+
+
+def down(cursor, installed_version):
+    pass
+
+
+migrate = up
