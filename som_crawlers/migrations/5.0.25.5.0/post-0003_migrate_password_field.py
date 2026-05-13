d6622a2c375ea6b932b751064b5e4a3abba96137
Pau Boix <pau.boix@somenergia.coop>
2026-05-13 10:45:56 +0200

    🗃️ migrate password field

---
+++ som_crawlers/migrations/5.0.25.5.0/post-0003_migrate_password_field.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+import logging
+from oopgrade.oopgrade import column_exists, change_column_type
+
+
+def up(cursor, installed_version):
+    if not installed_version:
+        return
+
+    logger = logging.getLogger('openerp.migration')
+
+    logger.info(
+        "Migrating password from VARCHAR(30) to VARCHAR(100)..."
+    )
+    if column_exists(cursor, 'som_crawlers_config', 'contrasenya'):
+        change_column_type(
+            cursor, {
+                'som_crawlers_config': [
+                    ('contrasenya', 'VARCHAR(100)')
+                ]
+            }
+        )
+    logger.info("Column type changed to VARCHAR(100) successfully!")
+
+
+def down(cursor, installed_version):
+    pass
+
+
+migrate = up
