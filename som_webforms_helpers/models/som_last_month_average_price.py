509ec141bd5db7287d458ede307bb3107825f8c4
Pau Boix <pau.boix@somenergia.coop>
2026-05-28 16:12:55 +0200

    🐛 fix: stabilize latest average price ordering

--- som_webforms_helpers/models/som_last_month_average_price.py
+++ som_webforms_helpers/models/som_last_month_average_price.py
@@ -20,7 +20,8 @@ class SomLastMonthAveragePrice(osv.osv):
 
     def get_current_price(self, cursor, uid, tariff, type, context=None):
         ids = self.search(cursor, uid, [("tariff", "=", tariff),
-                          ("type", "=", type)], limit=1, order="date desc", context=context)
+                          ("type", "=", type)], limit=1,
+                          order="date desc nulls last, id desc", context=context)
         if not ids:
             return False
         return self.read(
