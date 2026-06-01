26ed4934420542039f2368dfd160d03de93601a2
Francisco Jose Cubero Sanchez <francisco.cubero@somenergia.coop>
2026-06-01 13:53:48 +0200

    Update giscedata_facturacio_comer_som/migrations/5.0.25.5.0/post-0006_NEW_update_gdo_to_2025_migrate_data.py
    Co-authored-by: greptile-apps[bot] <165735046+greptile-apps[bot]@users.noreply.github.com>

--- giscedata_facturacio_comer_som/migrations/5.0.25.5.0/post-0006_NEW_update_gdo_to_2025_migrate_data.py
+++ giscedata_facturacio_comer_som/migrations/5.0.25.5.0/post-0006_NEW_update_gdo_to_2025_migrate_data.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 import logging
-from oopgrade.oopgrade import load_data
+from oopgrade.oopgrade import load_data_records
 
 
 def up(cursor, installed_version):
@@ -10,14 +10,18 @@ def up(cursor, installed_version):
     logger = logging.getLogger('openerp.migration')
 
     logger.info("Updating XML files")
-    data_files = [
-        'giscedata_facturacio_comer_data.xml',
+    list_of_records = [
+        "som_environmental_impact_data",
+        "mitjana_environmental_impact_data",
+        "component_gdo_data",
     ]
-    for data_file in data_files:
-        load_data(
-            cursor, 'giscedata_facturacio_comer_som', data_file,
-            idref=None, mode='update'
-        )
+    load_data_records(
+        cursor,
+        'giscedata_facturacio_comer_som',
+        'giscedata_facturacio_comer_data.xml',
+        list_of_records,
+        mode='update'
+    )
     logger.info("Migration completed successfully.")
 
 
