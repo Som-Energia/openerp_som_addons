8a3e1485484622de66af61326f42a65817462c60
PauBoix <87970784+PauBoix@users.noreply.github.com>
2026-06-04 12:45:58 +0200

    ✨ feat: add lead payment fields to view

---
+++ som_leads_polissa/migrations/5.0.25.5.0/post-0003_update_lead_payment_view.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+import logging
+
+from oopgrade.oopgrade import load_data_records
+
+
+def up(cursor, installed_version):
+    if not installed_version:
+        return
+
+    logger = logging.getLogger('openerp.migration')
+
+    logger.info("Updating lead payment view fields")
+
+    list_of_records = [
+        "giscedata_crm_leads_som_view",
+    ]
+
+    load_data_records(
+        cursor,
+        'som_leads_polissa',
+        'views/giscedata_crm_lead_view.xml',
+        list_of_records,
+        mode='update'
+    )
+
+    logger.info("Lead payment view fields updated")
+
+
+def down(cursor, installed_version):
+    pass
+
+
+migrate = up
