9922bc6faa459b6cd77bcaaa74ccb40cdc181938
Pau Boix <pau.boix@somenergia.coop>
2026-06-29 16:26:42 +0200

    💄 add contract summary button on polissa

--- som_polissa_condicions_generals/__terp__.py
+++ som_polissa_condicions_generals/__terp__.py
@@ -30,6 +30,7 @@
         "report/giscedata_polissa_condicions_generals_m101_report.xml",
         "report/giscedata_polissa_condicions_generals_ignore_modcon_report.xml",
         "report/giscedata_polissa_contract_summary_report.xml",
+        "views/giscedata_polissa_view.xml",
         "security/ir.model.access.csv",
     ],
     "active": False,
