f251db41480d7926f92d88af7e2c9013832e5e00
Fran Cubero <francisco.cubero@somenergia.coop>
2026-07-15 12:09:38 +0200

    👔adding wardrails

--- scripts/change_fiscal_positions.py
+++ scripts/change_fiscal_positions.py
@@ -6,6 +6,8 @@ from tqdm import tqdm
 
 O = Client(**dbconfig.erppeek)  # noqa: E741
 
+write_vals = False
+
 fp_change_map = [
     (98, 88),
     (97, 87),
@@ -28,14 +30,17 @@ for fp_old, fp_new in tqdm(fp_change_map):
     """ slow version
     for p_id in tqdm(pol_ids):
         pol = pol_obj.browse(p_id)
-        pol.fiscal_position_id = fp_new
-        pol.modcontractual_activa.fiscal_position_id = fp_new
+        if write_vals:
+            pol.fiscal_position_id = fp_new
+            pol.modcontractual_activa.fiscal_position_id = fp_new
     """
 
     pol_data = pol_obj.read(pol_ids, ['modcontractual_activa'])
     mod_ids = [p['modcontractual_activa'][0] for p in pol_data if p['modcontractual_activa']]
 
-    pol_obj.write(pol_ids, {'fiscal_position_id': fp_new})
-    mod_obj.write(mod_ids, {'fiscal_position_id': fp_new})
-    success("FP {} -> {}: {} polisses actualitzades {} modcons actialitzades".format(fp_old,
-            fp_new, len(pol_ids), len(mod_ids)))
+    if write_vals:
+        pol_obj.write(pol_ids, {'fiscal_position_id': fp_new})
+        mod_obj.write(mod_ids, {'fiscal_position_id': fp_new})
+
+    success("FP {} -> {}: {} polisses actualitzades {} modcons actualitzades {}".format(
+            fp_old, fp_new, len(pol_ids), len(mod_ids), "Si" if write_vals else "No"))
