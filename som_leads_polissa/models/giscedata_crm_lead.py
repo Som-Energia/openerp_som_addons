c2c8fa4a4ce4701c7a8447e68125eedf607c5846
destanyol <daniel.estanyol@somenergia.coop>
2026-07-23 12:15:25 +0200

    🩹(lead) Fix report summary to summary full
    Co-authored-by: Fran Cubero <francisco.cubero@somenergia.coop>

--- som_leads_polissa/models/giscedata_crm_lead.py
+++ som_leads_polissa/models/giscedata_crm_lead.py
@@ -675,7 +675,7 @@ class GiscedataCrmLead(osv.OsvInherits):
             context = {}
         imd_obj = self.pool.get('ir.model.data')
         report_id = imd_obj.get_object_reference(
-            cursor, uid, 'som_polissa_condicions_generals', 'lead_contract_summary_report'
+            cursor, uid, 'som_polissa_condicions_generals', 'lead_contract_summary_full_report'
         )[1]
         return report_id
 
