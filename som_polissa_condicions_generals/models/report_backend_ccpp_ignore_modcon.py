11529356d53c53f9c02aae3c2afa86ff7ba98976
Àngel Fernández <angel.fernandez@somenergia.coop>
2026-07-23 15:25:23 +0200

    🦺 py3 compatibility

--- som_polissa_condicions_generals/models/report_backend_ccpp_ignore_modcon.py
+++ som_polissa_condicions_generals/models/report_backend_ccpp_ignore_modcon.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
 from report_backend.report_backend import ReportBackend, report_browsify
 from report_puppeteer.report_puppeteer import PuppeteerParser
 
