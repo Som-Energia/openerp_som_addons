0d22592fe666ce030216cb2afa7b234ecff20d9a
Pau Boix <pau.boix@somenergia.coop>
2026-08-20 09:03:40 +0200

    ✨ load card payment invoice view migration

--- som_card_payment/migrations/5.0.0-dev/post-0001_load_polissa_view.py
+++ som_card_payment/migrations/5.0.0-dev/post-0001_load_polissa_view.py
@@ -1,13 +1,27 @@
 # -*- coding: utf-8 -*-
 from oopgrade.oopgrade import load_data
+import pooler
 
 
 def up(cursor, installed_version):
     if not installed_version:
         return
 
+    pool = pooler.get_pool(cursor.dbname)
+    for model_name in (
+        'res.partner.creditcard',
+        'giscedata.polissa',
+        'giscedata.polissa.modcontractual',
+        'giscedata.facturacio.factura',
+    ):
+        pool.get(model_name)._auto_init(cursor, {})
+
     load_data(
         cursor, 'som_card_payment', 'views/giscedata_polissa_view.xml',
+        idref=None, mode='update'
+    )
+    load_data(
+        cursor, 'som_card_payment', 'views/giscedata_facturacio_factura_view.xml',
         idref=None, mode='update'
     )
 
