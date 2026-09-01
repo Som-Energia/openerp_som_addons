26d09d99c08dbe5e35bcb1d4e6ebf80864d448b3
Àngel Fernández <angel.fernandez@somenergia.coop>
2026-09-01 14:35:06 +0200

    🧹 Remove custom som emmagatzamatge and use GISCE logic (#1428)
    * 🧹 Remove custom som emmagatzamatge and use GISCE logic
    
    * Use empty models and views for migration
    
    * 🩹 More migration hacks

--- som_polissa/models/giscedata_autoconsum.py
+++ som_polissa/models/giscedata_autoconsum.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from osv import osv, fields
+from osv import osv
 
 
 class GiscedataAutoconsumGenerador(osv.osv):
@@ -8,17 +8,7 @@ class GiscedataAutoconsumGenerador(osv.osv):
     _name = 'giscedata.autoconsum.generador'
     _inherit = 'giscedata.autoconsum.generador'
 
-    _columns = {
-        'emmagatzematge': fields.boolean('Te emmagatzematge'),
-        'potencia_sortida': fields.float(u'Potència de sortida (KW)'),
-        'energia_acumulable': fields.float(u'Energía acumulable'),
-    }
-
-    _defaults = {
-        'emmagatzematge': lambda *a: False,
-        'potencia_sortida': lambda *a: 0.0,
-        'energia_acumulable': lambda *a: 0.0,
-    }
+    # FIXME: Delete me in the future, this is only for migration purposes
 
 
 GiscedataAutoconsumGenerador()
