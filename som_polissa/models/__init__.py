cd11cd21877cbd9c6cb2ac61cc0ec91bc0625bfe
Àngel Fernández <angel.fernandez@somenergia.coop>
2026-09-01 11:39:56 +0200

    🧹 Remove custom som emmagatzamatge and use GISCE logic

--- som_polissa/models/__init__.py
+++ som_polissa/models/__init__.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import
-from . import giscedata_autoconsum
 from . import giscedata_cups
 from . import giscedata_polissa
 from . import giscedata_switching_helpers
