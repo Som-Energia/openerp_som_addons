7d9128ffa46cdb3ec761547f6e2c3a111934c083
destanyol <daniel.estanyol@somenergia.coop>
2026-09-04 15:25:27 +0200

    ✅ Fix more tests

--- som_leads_polissa/tests/base_som_lead_www.py
+++ som_leads_polissa/tests/base_som_lead_www.py
@@ -108,7 +108,14 @@ class BaseSomLeadWwwTest(testing.OOTestCase):
         )
         self.patch_signature_allows_activation.start()
 
+        self.patch_send_activation_mail_async = mock.patch.object(
+            self.get_model("som.lead.www"),
+            "_send_activation_mail_async",
+        )
+        self.patch_send_activation_mail_async.start()
+
     def tearDown(self):
+        self.patch_send_activation_mail_async.stop()
         self.patch_signature_allows_activation.stop()
         self.patch_subscribe_member.stop()
         self.patch_unsubscribe_customer.stop()
