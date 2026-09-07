58fb1e99ecbb26124025623de1563ba761ec9578
Àngel Fernández <angel.fernandez@somenergia.coop>
2026-09-07 11:47:22 +0200

    ✨ Add IA generated draft code

---
+++ som_holder_change/__terp__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Som Energia Holder Change",
+    "description": """
+    Holder change request management for existing contracts.
+    """,
+    "version": "0-dev",
+    "author": "SomEnergia",
+    "category": "SomEnergia",
+    "depends": [
+        "som_polissa",
+        "som_switching",
+        "som_polissa_condicions_generals",
+        "giscedata_signatura_documents_signaturit",
+        "som_card_payment",
+    ],
+    "init_xml": [],
+    "demo_xml": [],
+    "update_xml": [
+        "security/ir.model.access.csv",
+    ],
+    "active": False,
+    "installable": True,
+}
---
+++ som_holder_change/tests/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+
+from .test_holder_change_request import *
+from .test_holder_change_www import *
+from .test_holder_change_m1 import *
