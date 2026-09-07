c5eb2d0a2d8b1e1673cba949ef948c01a8a69b0a
Oriol Piera <oriol.piera@somenergia.coop>
2026-09-07 08:41:58 +0200

    🐛 (som_dashboard) task 1 ATR show cases with attachments correctly

---
+++ som_dashboard/migrations/5.0.0-dev/post-0004_som_dashboard_update_tasca1_attachment_filters.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+import logging
+from oopgrade.oopgrade import load_data_records
+
+
+def up(cursor, installed_version):
+    if not installed_version:
+        return
+
+    logger = logging.getLogger('openerp.migration')
+    logger.info("Updating Task 1 M attachment dashboard filters")
+    load_data_records(
+        cursor,
+        'som_dashboard',
+        'som_dashboard_gc_tasca_1.xml',
+        [
+            'action_ms_with_attachments_dashboard',
+            'action_ms_without_attachments_dashboard',
+        ],
+        mode='update'
+    )
+    logger.info("Migration completed successfully.")
+
+
+def down(cursor, installed_version):
+    pass
+
+
+migrate = up
