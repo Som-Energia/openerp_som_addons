4e9201c1982a5c5770be5df75ed72c8796f908c4
Oriol Piera <oriol.piera@somenergia.coop>
2026-09-14 11:59:19 +0000

    🐛 (som_polissa_condicions_generals) fix k number i report (#1455)
    * 🐛 (som_polissa_condicions_generals) fix k number i report
    
    * 🩹 Verify Greptile feedback before changing code; do not commit or destroy local data without explicit authorization

--- som_polissa_condicions_generals/tests/tests_report_backend_ccpp.py
+++ som_polissa_condicions_generals/tests/tests_report_backend_ccpp.py
@@ -3,6 +3,7 @@ from __future__ import absolute_import, unicode_literals, division
 import unittest
 import os
 from datetime import datetime
+import mock
 from destral import testing
 from destral.transaction import Transaction
 from mako.template import Template
@@ -303,6 +304,56 @@ class TestReportBackendCCPP(testing.OOTestCase):
         self.assertEqual(result['mostra_indexada'], True)
         self.assertTrue('coeficient_k' in result['pricelists'][0])
         self.assertTrue('coeficient_k_untaxed' in result['pricelists'][0])
+
+    def test_get_prices_data_uses_contract_coefficient_over_pricelist(self):
+        self.pol_obj.send_signal(
+            self.cursor, self.uid, [self.contract_20TD_id], ["validar", "contracte"])
+        context = {"active_id": self.contract_20TD_id, "change_type": "from_period_to_index"}
+        wiz_id = self.wiz_change_to_index_obj.create(self.cursor, self.uid, {}, context=context)
+        self.wiz_change_to_index_obj.change_to_indexada(
+            self.cursor, self.uid, [wiz_id], context=context)
+        self.pol_obj.write(
+            self.cursor, self.uid, [self.contract_20TD_id], {'coeficient_k': 3.478}
+        )
+        self.k_change_obj.unlink(
+            self.cursor, self.uid, self.k_change_obj.search(self.cursor, self.uid, [])
+        )
+
+        pol_20td = self.pol_obj.browse(self.cursor, self.uid, self.contract_20TD_id)
+        result = self.backend_obj.get_prices_data(self.cursor, self.uid, pol_20td, context={})
+
+        self.assertEqual(result['coeficient_k_untaxed'], 0.003478)
+        for pricelist in result['pricelists']:
+            self.assertEqual(pricelist['coeficient_k_untaxed'], 0.003478)
+
+    def test_get_prices_data_uses_contract_fallback_without_pricelist_coefficient(self):
+        self.pol_obj.send_signal(
+            self.cursor, self.uid, [self.contract_20TD_id], ["validar", "contracte"])
+        context = {"active_id": self.contract_20TD_id, "change_type": "from_period_to_index"}
+        wiz_id = self.wiz_change_to_index_obj.create(self.cursor, self.uid, {}, context=context)
+        self.wiz_change_to_index_obj.change_to_indexada(
+            self.cursor, self.uid, [wiz_id], context=context)
+        self.pol_obj.write(
+            self.cursor, self.uid, [self.contract_20TD_id], {
+                'coeficient_k': 0.0,
+                'coeficient_d': 3.478,
+            }
+        )
+        self.k_change_obj.unlink(
+            self.cursor, self.uid, self.k_change_obj.search(self.cursor, self.uid, [])
+        )
+
+        pol_20td = self.pol_obj.browse(self.cursor, self.uid, self.contract_20TD_id)
+        with mock.patch.object(
+            self.backend_obj, '_get_coeficient_k_from_pricelist', return_value=False
+        ):
+            result = self.backend_obj.get_prices_data(
+                self.cursor, self.uid, pol_20td, context={}
+            )
+
+        self.assertEqual(result['coeficient_k_untaxed'], 0.003478)
+        for pricelist in result['pricelists']:
+            self.assertEqual(pricelist['coeficient_k_untaxed'], 0.003478)
 
     def test_get_coeficient_k_for_pricelist_uses_k_old_for_current_block(self):
         fs_data = {'k_old': 12.0, 'k_new': 19.0}
